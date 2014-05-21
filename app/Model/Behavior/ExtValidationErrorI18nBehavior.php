6f98c8d6f587da5486e9bcb41c36d27a1645d665
bigplants <hirakatad@isao.co.jp>
2014-05-22 01:25:27 +0900

    バリデーション系の処理をリファクタリング(クラスを分離)

---
+++ app/Model/Behavior/ExtValidationErrorI18nBehavior.php
@@ -0,0 +1,28 @@
+<?php
+App::uses('ValidationErrorI18nBehavior', 'Cakeplus.Model/Behavior');
+
+/**
+ * Created by PhpStorm.
+ * User: daikihirakata
+ * Date: 2014/05/22
+ * Time: 1:01
+ */
+class ExtValidationErrorI18nBehavior extends ValidationErrorI18nBehavior
+{
+    function beforeValidate(Model $model, $options = [])
+    {
+        //ここに全てのエラーメッセージを記載する
+        $error_messages = array(
+            'notEmpty'          => __d('validate', "入力必須項目です。"),
+            'isAlphabetOnly'    => __d('validate', "アルファベットのみで入力してください。"),
+            'email'             => __d('validate', "メールアドレスが正しくありません。"),
+            'notBlankCheckbox'  => __d('validate', "利用規約に同意してください。"),
+            'passwordSameCheck' => __d('validate', "パスワードが一致しません。"),
+            'minLength'         => __d('validate', '%2$d文字以上で入力してください。'),
+        );
+        $this->setErrorMessageI18n($model, $error_messages, false);
+        $this->replaceValidationErrorMessagesI18n($model);
+        parent::beforeValidate($model, $options);
+        return true;
+    }
+}
---
+++ app/Model/Behavior/ExtValidationPatternsBehavior.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('ValidationPatternsBehavior', 'Cakeplus.Model/Behavior');
+
+/**
+ * Created by PhpStorm.
+ * User: daikihirakata
+ * Date: 2014/05/22
+ * Time: 1:01
+ */
+class ExtValidationPatternsBehavior extends ValidationPatternsBehavior
+{
+}
