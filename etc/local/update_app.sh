907e110aa1705fe3b95b7a354211ca03f0a0489a
bigplants <hirakatad@isao.co.jp>
2014-05-27 23:43:39 +0900

    ローカルにてアプリケーションのアップデートを行うスクリプトを追加 #60

---
+++ etc/local/update_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Update Application"
+
+cd /vagrant;git submodule update --init --recursive
+cd /vagrant;git fetch
+cd /vagrant/app;./Console/cake remove_cache
+cd /vagrant/app;./Console/cake migrations.migration run all
+cd /vagrant/app;./Console/cake test app AllTests --stderr
