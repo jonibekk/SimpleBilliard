b736797b7fbdf1dc2aa4527184a33b6acdce5843
bigplants <hirakatad@isao.co.jp>
2014-05-28 01:27:54 +0900

    AppControllerの不要なプロパティを削除

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -15,9 +15,8 @@ App::uses('Controller', 'Controller');
  * Application Controller
  * Add your application-wide methods in the class below, your controllers
  * will inherit them.
-
  *
-*@package        app.Controller
+ * @package        app.Controller
  * @link           http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  * @property LangComponent         $Lang
  * @property TimezoneComponent     $Timezone
@@ -55,18 +54,6 @@ class AppController extends Controller
     public $uses = [
         'User',
     ];
-
-    /**
-     * @var null
-     */
-    public $top_lang = null;
-
-    /**
-     * Mixpanel
-     *
-     * @var MixPanel
-     */
-    public $Mp;
 
     public function beforeFilter()
     {
