9548f1971017a6b3bc405753c4b9c2fcb2509a49
bigplants <hirakatad@isao.co.jp>
2014-06-26 04:00:14 +0900

    100万件のダミーデータの挿入に成功

--- app/Console/Command/DummyDataShell.php
+++ app/Console/Command/DummyDataShell.php
@@ -24,103 +24,163 @@ class DummyDataShell extends AppShell
      */
     public $Schema;
 
+    public $records = [];
+
+    public $start_time;
+
     function startup()
     {
-        Configure::write('debug', 0);
+        Configure::write('debug', 2);
         ini_set('memory_limit', '2024M');
         Configure::write('shell_mode', true);
+        $this->_setTableAndRecords();
+        $this->start_time = microtime(true);
     }
 
     function main()
     {
-
-//        $this->Schema = new CakeSchema;
-//        $this->Schema->read();
         $this->User->cacheQueries = false;
-        $start_time = microtime(true);
-        $this->insertMultiMusic($this->records, "users", 1000000);
+        $this->User->useDbConfig = "bench";
+        foreach ($this->records as $table_name => $records) {
+            $this->insertInitData($records, $table_name);
+        }
+        foreach ($this->records as $table_name => $records) {
+            $this->insertMulti($records, $table_name);
+        }
         $end_time = microtime(true);
-        $total_time = $end_time - $start_time;
+        $total_time = $end_time - $this->start_time;
 
-//        $data = [];
-//        for ($i = 0; $i < 10000; $i++) {
-//            $this->user_record['id'] = "dummy_" . $i;
-//            $data[] = $this->user_record;
-//        }
-//        $this->User->saveAll($data, ['validate' => false, 'atomic' => false]);
         $this->out("ダミーデータを登録しました。");
         $this->out("実行時間:{$total_time}sec");
     }
-
-    public $records = [
-        'id'                => '537ce224-f96c-4c97-89ea-433dac11b50b',
-        'first_name'        => 'Lorem ipsum dolor sit amet',
-        'last_name'         => 'Lorem ipsum dolor sit amet',
-        'middle_name'       => 'Lorem ipsum dolor sit amet',
-        'gender_type'       => 3,
-        'birth_day'         => '2014-05-22',
-        'hide_year_flg'     => 1,
-        'hometown'          => 'Lorem ipsum dolor sit amet',
-        'comment'           => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-        'password'          => 'Lorem ipsum dolor sit amet',
-        'password_token'    => 'Lorem ipsum dolor sit amet',
-        'password_modified' => '2014-05-22 02:28:04',
-        'no_pass_flg'       => 1,
-        'photo_file_name'   => 'Lorem ipsum dolor sit amet',
-        'primary_email_id'  => 'Lorem ipsum dolor sit amet',
-        'active_flg'        => 1,
-        'last_login'        => '2014-05-22 02:28:04',
-        'admin_flg'         => 1,
-        'default_team_id'   => 'Lorem ipsum dolor sit amet',
-        'timezone'          => 3,
-        'auto_timezone_flg' => 1,
-        'language'          => 'Lorem ipsum dolor sit amet',
-        'auto_language_flg' => 1,
-        'romanize_flg'      => 1,
-        'update_email_flg'  => 1,
-        'del_flg'           => 0,
-        'deleted'           => '2014-05-22 02:28:04',
-        'created'           => '2014-05-22 02:28:04',
-        'modified'          => '2014-05-22 02:28:04'
-    ];
 
     public function deleteAllDummy()
     {
         $this->User->deleteAll(['User.id Like' => "dummy_%"]);
     }
 
-    public function insertMultiMusic($default_data, $table_name, $row_count = 100000)
+    public function insertInitData($default_data, $table_name)
     {
         $fields = array_keys($default_data);
         $holder = '(' . implode(',', array_fill(0, count($fields), '?')) . ')';
-        $once_rows = 10000;
-        $query_count = floor($row_count / $once_rows);
+        $once_rows = 10;
         $holders = implode(',', array_fill(0, $once_rows, $holder));
-        $current_no = 0;
+        $current_no = 1;
         $data = [];
         for ($i = 0; $i < $once_rows; $i++) {
             $data[] = $default_data;
         }
-        for ($i = 0; $i < $query_count; $i++) {
-            $params = array();
-            foreach ($data as $val) {
-                foreach ($fields as $field) {
-                    if ($field == 'created' || $field == 'modified') {
-                        $params[] = date('Y-m-d H:i:s');
+        $params = [];
+        foreach ($data as $val) {
+            foreach ($fields as $field) {
+                if ($field == "id") {
+                    $params[] = $current_no;
+                }
+                else {
+                    $params[] = $val[$field];
+                }
+            }
+            $current_no++;
+        }
+        $fields_imploded = implode(',', $fields);
+        $sql = "INSERT INTO {$table_name} ({$fields_imploded}) VALUES {$holders}";
+        $this->User->query($sql, $params);
+    }
+
+    public function insertMulti($default_data, $table_name)
+    {
+        $schema = $this->Schema->read();
+        $table_schema = $schema['tables'][$table_name];
+        $current_start_time = microtime(true);
+
+        $fields = array_keys($default_data);
+        $fields_imploded = implode(',', $fields);
+
+        $from = "";
+        for ($i = 1; $i < 7; $i++) {
+            if ($i != 1) {
+                $from .= ",";
+            }
+            $from .= " {$table_name} t{$i}";
+        }
+        $id = "t6.id * 100000 + t5.id * 10000 + t4.id * 1000 + t3.id * 100 + t2.id * 10 + t1.id";
+        $select_fields = "";
+        foreach ($default_data as $key => $val) {
+            if ($key === "id") {
+                continue;
+            }
+            if ($table_schema[$key]['type'] == "string" || $table_schema[$key]['type'] == "text") {
+                $select_fields .= ", CONCAT(t1.{$key},({$id}))";
+            }
+            else {
+                $select_fields .= ", t1.{$key}";
+            }
+        }
+        $sql = "INSERT INTO {$table_name} ({$fields_imploded}) SELECT {$id}{$select_fields} FROM {$from}";
+        $this->User->query($sql);
+        $end_time = microtime(true);
+        $current_time = $end_time - $current_start_time;
+        $total_time = $end_time - $this->start_time;
+        $this->out("****************************");
+        $this->out("完了 : {$table_name}");
+        $this->out("実行時間:{$current_time}sec");
+        $this->out("経過時間:{$total_time}sec");
+
+    }
+
+    function _setTableAndRecords()
+    {
+        $this->Schema = new CakeSchema();
+        $schema = $this->Schema->read();
+        $records = [];
+        foreach ($schema['tables'] as $table_name => $fields) {
+            if ($table_name == "missing") {
+                continue;
+            }
+            foreach ($fields as $field_name => $field) {
+                if ($field_name == "indexes") {
+                    continue;
+                }
+                if ($field_name == "tableParameters") {
+                    continue;
+                }
+                if (!isset($field['type'])) {
+                    continue;
+                }
+
+                if ($field_name == "del_flg") {
+                    $records[$table_name][$field_name] = false;
+                }
+                elseif ($field_name == "deleted") {
+                    $records[$table_name][$field_name] = null;
+                }
+                else {
+
+                    if ($field['type'] == "string") {
+                        $records[$table_name][$field_name] = "test string";
                     }
-                    elseif ($field == "id") {
-                        $params[] = "dummy_" . $current_no;
+                    elseif ($field['type'] == "integer") {
+                        $records[$table_name][$field_name] = true;
+
+                    }
+                    elseif ($field['type'] == "text") {
+                        $records[$table_name][$field_name] = "test text";
+                    }
+                    elseif ($field['type'] == "boolean") {
+                        $records[$table_name][$field_name] = true;
+                    }
+                    elseif ($field['type'] == "datetime") {
+                        $records[$table_name][$field_name] = date('Y-m-d H:i:s');
+                    }
+                    elseif ($field['type'] == "date") {
+                        $records[$table_name][$field_name] = date('Y-m-d');
                     }
                     else {
-                        $params[] = $val[$field];
+                        $records[$table_name][$field_name] = false;
                     }
                 }
-                $current_no++;
             }
-            $fields_imploded = implode(',', $fields);
-            $sql = "INSERT INTO {$table_name} ({$fields_imploded}) VALUES {$holders}";
-            unset($fields_imploded);
-            $this->User->query($sql, $params);
         }
+        $this->records = $records;
     }
 }
