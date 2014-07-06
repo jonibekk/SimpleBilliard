0056ee55d21e62ba197298ea205bae08ec927f12
bigplants <hirakatad@isao.co.jp>
2014-07-06 19:00:51 +0900

    postsのmodifiedにインデックスを張った

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -400,12 +400,13 @@ class AppSchema extends CakeSchema
         'del_flg'         => array('type' => 'boolean', 'null' => false, 'default' => '0', 'comment' => '削除フラグ'),
         'deleted'         => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '投稿を削除した日付時刻'),
         'created'         => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '投稿を追加した日付時刻'),
-        'modified'        => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '投稿を更新した日付時刻'),
+        'modified'        => array('type' => 'datetime', 'null' => true, 'default' => null, 'key' => 'index', 'comment' => '投稿を更新した日付時刻'),
         'indexes'         => array(
-            'PRIMARY' => array('column' => 'id', 'unique' => 1),
-            'user_id' => array('column' => 'user_id', 'unique' => 0),
-            'team_id' => array('column' => 'team_id', 'unique' => 0),
-            'goal_id' => array('column' => 'goal_id', 'unique' => 0)
+            'PRIMARY'  => array('column' => 'id', 'unique' => 1),
+            'user_id'  => array('column' => 'user_id', 'unique' => 0),
+            'team_id'  => array('column' => 'team_id', 'unique' => 0),
+            'goal_id'  => array('column' => 'goal_id', 'unique' => 0),
+            'modified' => array('column' => 'modified', 'unique' => 0)
         ),
         'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
     );
