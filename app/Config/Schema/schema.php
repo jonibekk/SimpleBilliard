f23ac871092caa0ceee75dadc3ccf7639d0085a7
bigplants <hirakatad@isao.co.jp>
2014-07-15 01:22:42 +0900

    migrationファイルをプライマリキー変更に対応

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -117,9 +117,9 @@ class AppSchema extends CakeSchema
         'del_flg'            => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index', 'comment' => '削除フラグ'),
         'deleted'            => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '投稿を削除した日付時刻'),
         'created'            => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => '投稿を追加した日付時刻'),
-        'modified'           => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => true, 'key' => 'primary', 'comment' => '投稿を更新した日付時刻'),
+        'modified'           => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '投稿を更新した日付時刻'),
         'indexes'            => array(
-            'PRIMARY' => array('column' => array('id', 'modified'), 'unique' => 1),
+            'PRIMARY' => array('column' => 'id', 'unique' => 1),
             'post_id' => array('column' => 'post_id', 'unique' => 0),
             'user_id' => array('column' => 'user_id', 'unique' => 0),
             'team_id' => array('column' => 'team_id', 'unique' => 0),
@@ -428,9 +428,9 @@ class AppSchema extends CakeSchema
         'del_flg'         => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index', 'comment' => '削除フラグ'),
         'deleted'         => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '投稿を削除した日付時刻'),
         'created'         => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '投稿を追加した日付時刻'),
-        'modified'        => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => true, 'key' => 'primary', 'comment' => '投稿を更新した日付時刻'),
+        'modified'        => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => '投稿を更新した日付時刻'),
         'indexes'         => array(
-            'PRIMARY'       => array('column' => array('id', 'modified'), 'unique' => 1),
+            'PRIMARY'       => array('column' => 'id', 'unique' => 1),
             'user_id'       => array('column' => 'user_id', 'unique' => 0),
             'team_id'       => array('column' => 'team_id', 'unique' => 0),
             'goal_id'       => array('column' => 'goal_id', 'unique' => 0),
