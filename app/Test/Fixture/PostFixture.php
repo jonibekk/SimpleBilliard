49258489cb5c105667a08a1291c82ead64145d30
bigplants <hirakatad@isao.co.jp>
2014-08-10 03:38:39 +0900

    PagesControllerのテストを修正 #181

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -56,7 +56,7 @@ class PostFixture extends CakeTestFixture
     public $records = array(
         array(
             'id'            => 1,
-            'user_id'       => 1,
+            'user_id' => 2,
             'team_id'       => 1,
             'body'            => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
             'type'            => 1,
