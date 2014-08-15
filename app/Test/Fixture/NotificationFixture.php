0258d0b3583b70581a75cd7e779dd9fa332aaf4c
Daiki Hirakata <hirakatad@isao.co.jp>
2014-08-15 16:35:55 +0900

    カラム追加に伴うNotificationFixtureの変更 #42

--- app/Test/Fixture/NotificationFixture.php
+++ app/Test/Fixture/NotificationFixture.php
@@ -19,18 +19,22 @@ class NotificationFixture extends CakeTestFixture
         'type'            => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 3, 'unsigned' => true, 'comment' => 'タイプ(1:ゴール,2:投稿,3:etc ...)'),
         'from_user_id'    => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => '通知元ユーザID(belongsToでUserモデルに関連)'),
         'body'            => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '通知本文', 'charset' => 'utf8'),
+        'model_id'        => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => 'モデルID(feedならpost_id,circleならcircle_id)'),
+        'url_data'        => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'URLデータ(json)', 'charset' => 'utf8'),
+        'count_num'       => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => 'メッセージ内で利用する件数'),
         'unread_flg'      => array('type' => 'boolean', 'null' => false, 'default' => '1', 'key' => 'index', 'comment' => '未読フラグ(通知を開いたらOff)'),
         'del_flg'         => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index', 'comment' => '削除フラグ'),
         'deleted'         => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '通知を削除した日付時刻'),
         'created'         => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '通知を追加した日付時刻'),
-        'modified'        => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '通知を更新した日付時刻'),
+        'modified'        => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => '通知を更新した日付時刻'),
         'indexes'         => array(
             'PRIMARY'      => array('column' => 'id', 'unique' => 1),
             'user_id'      => array('column' => 'user_id', 'unique' => 0),
             'team_id'      => array('column' => 'team_id', 'unique' => 0),
             'from_user_id' => array('column' => 'from_user_id', 'unique' => 0),
             'del_flg'      => array('column' => 'del_flg', 'unique' => 0),
-            'unread_flg'   => array('column' => 'unread_flg', 'unique' => 0)
+            'unread_flg' => array('column' => 'unread_flg', 'unique' => 0),
+            'modified'   => array('column' => 'modified', 'unique' => 0)
         ),
         'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
     );
