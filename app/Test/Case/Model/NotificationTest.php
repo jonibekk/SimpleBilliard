5249d97c456c034df379b13b275d710a4ed3679a
Daiki Hirakata <hirakatad@isao.co.jp>
2014-08-20 17:21:54 +0900

    Notificationモデルのテストを修正 #42

--- app/Test/Case/Model/NotificationTest.php
+++ app/Test/Case/Model/NotificationTest.php
@@ -4,7 +4,7 @@ App::uses('Notification', 'Model');
 /**
  * Notification Test Case
  *
- * @property mixed Notification
+ * @property  Notification $Notification
  */
 class NotificationTest extends CakeTestCase
 {
@@ -43,9 +43,67 @@ class NotificationTest extends CakeTestCase
         parent::tearDown();
     }
 
-    //ダミーテスト
-    function testDummy()
+    function testSaveNotify()
     {
+        $uid = 1;
+        $team_id = 1;
+        $this->Notification->me['id'] = $uid;
+        $this->Notification->current_team_id = $team_id;
+        $data = [
+            'model_id' => 1,
+            'type'     => 1,
+            'team_id'  => $team_id
+        ];
+        $this->Notification->save($data);
+        $this->Notification->saveNotify($data, [$uid]);
+        $data = [
+            'model_id' => 1,
+            'type'     => 99,
+            'team_id'  => $team_id
+        ];
+        $this->Notification->saveNotify($data, [$uid]);
+    }
+
+    function testGetNotifyFromTodayUtc()
+    {
+        $uid = 1;
+        $team_id = 1;
+        $this->Notification->me['id'] = $uid;
+        $this->Notification->current_team_id = $team_id;
+        $this->Notification->getNotifyFromTodayUtc(1);
+    }
+
+    function testSaveNotifyOneOnOne()
+    {
+        $uid = 1;
+        $team_id = 1;
+        $this->Notification->me['id'] = $uid;
+        $this->Notification->current_team_id = $team_id;
+        $this->Notification->Team->TeamMember->me['id'] = $uid;
+        $this->Notification->Team->TeamMember->current_team_id = $team_id;
+        $data = [
+            'model_id' => 1,
+            'type'     => 1,
+            'team_id'  => $team_id
+        ];
+        $this->Notification->save($data);
+        $this->Notification->saveNotifyOneOnOne($data, [$uid, 2]);
+    }
+
+    function testGetTitle()
+    {
+        $from_user_names = ['aaa', 'bbb'];
+        $count_num = 1;
+        $item_name = json_encode(['ccc', 'ddd']);
+        $this->Notification->getTitle(Notification::TYPE_FEED_POST, $from_user_names, $count_num, $item_name);
+        $this->Notification->getTitle(Notification::TYPE_FEED_COMMENTED_ON_MY_COMMENTED_POST, $from_user_names,
+                                      $count_num, $item_name);
+        $this->Notification->getTitle(Notification::TYPE_FEED_COMMENTED_ON_MY_POST, $from_user_names, $count_num,
+                                      $item_name);
+        $this->Notification->getTitle(Notification::TYPE_CIRCLE_CHANGED_PRIVACY_SETTING, $from_user_names, $count_num,
+                                      $item_name);
+        $this->Notification->getTitle(Notification::TYPE_CIRCLE_USER_JOIN, $from_user_names, $count_num, $item_name);
+        $this->Notification->getTitle(Notification::TYPE_CIRCLE_ADD_USER, $from_user_names, $count_num, $item_name);
     }
 
 }
