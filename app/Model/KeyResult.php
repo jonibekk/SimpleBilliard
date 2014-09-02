66767f7390a800f2c5c519e2ca7fbfae99ba15f0
Daiki Hirakata <hirakatad@isao.co.jp>
2014-09-02 18:13:12 +0900

    キーリザルトの単位の名前をセット #343

--- app/Model/KeyResult.php
+++ app/Model/KeyResult.php
@@ -10,6 +10,34 @@ App::uses('AppModel', 'Model');
  */
 class KeyResult extends AppModel
 {
+    /**
+     * 目標値の単位
+     */
+    const UNIT_PERCENT = 0;
+    const UNIT_NUMBER = 1;
+    const UNIT_BINARY = 2;
+    const UNIT_YEN = 3;
+    const UNIT_DOLLAR = 4;
+
+    static public $UNIT = [
+        self::UNIT_PERCENT => "",
+        self::UNIT_NUMBER  => "",
+        self::UNIT_BINARY  => "",
+        self::UNIT_YEN     => "",
+        self::UNIT_DOLLAR  => "",
+    ];
+
+    /**
+     * 目標値の単位の表示名をセット
+     */
+    private function _setUnitName()
+    {
+        self::$UNIT[self::UNIT_PERCENT] = __d('gl', "%");
+        self::$UNIT[self::UNIT_NUMBER] = __d('gl', "数値");
+        self::$UNIT[self::UNIT_BINARY] = __d('gl', 'ON/OFF');
+        self::$UNIT[self::UNIT_YEN] = __d('gl', '¥');
+        self::$UNIT[self::UNIT_DOLLAR] = __d('gl', '$');
+    }
 
     /**
      * Display field
@@ -54,4 +82,11 @@ class KeyResult extends AppModel
     public $hasMany = [
         'KeyResultUser'
     ];
+
+    function __construct($id = false, $table = null, $ds = null)
+    {
+        parent::__construct($id, $table, $ds);
+        $this->_setUnitName();
+    }
+
 }
