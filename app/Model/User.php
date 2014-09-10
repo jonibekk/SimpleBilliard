4108b548c6ffb1ed9453d850ef02ffd5f77648fa
Daiki Hirakata <hirakatad@isao.co.jp>
2014-09-10 15:57:00 +0900

    デフォルト共有範囲選択項目の表示(debug中) #433

--- app/Model/User.php
+++ app/Model/User.php
@@ -790,6 +790,53 @@ class User extends AppModel
         return ['results' => $res];
     }
 
+    /**
+     * feedのselect2で使うデフォルトデータリスト(json)
+     *
+     * @return array(json)
+     */
+    function getAllUsersCirclesSelect2()
+    {
+        App::uses('UploadHelper', 'View/Helper');
+        $Upload = new UploadHelper(new View());
+
+        $circles = $this->CircleMember->getMyCircle();
+        $circle_res = [];
+        foreach ($circles as $val) {
+            $data['id'] = 'circle_' . $val['Circle']['id'];
+            $data['text'] = $val['Circle']['name'];
+            $data['image'] = $Upload->uploadUrl($val, 'Circle.photo', ['style' => 'small']);
+            $circle_res[] = $data;
+        }
+
+        $users = $this->getAllMember(false);
+        $user_res = [];
+        foreach ($users as $val) {
+            $data['id'] = 'user_' . $val['User']['id'];
+            $data['text'] = $val['User']['username'];
+            $data['image'] = $Upload->uploadUrl($val, 'User.photo', ['style' => 'small']);
+            $user_res[] = $data;
+        }
+        $res = array_merge($circle_res, $user_res);
+
+        return json_encode($res);
+
+    }
+
+    public function getAllMember($with_me = true)
+    {
+        $uid_list = $this->TeamMember->getAllMemberUserIdList($with_me);
+
+        $options = [
+            'conditions' => [
+                'id' => $uid_list,
+            ],
+            'fields'     => $this->profileFields,
+        ];
+        $res = $this->find('all', $options);
+        return $res;
+    }
+
     function getProfileAndEmail($uid, $lang = null)
     {
         $backup_lang = null;
