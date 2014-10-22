24e17ec42cb4022f9c3ff3a4268dac6f3badb6d1
Daiki Hirakata <hirakatad@isao.co.jp>
2014-10-23 07:04:13 +0900

    Collaboratorのkey_result_idをgoal_idに変更 #614

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -94,9 +94,9 @@ class AppSchema extends CakeSchema
     );
 
     public $collaborators = array(
-        'id'              => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary', 'comment' => 'キーリザルトユーザID'),
+        'id'              => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary', 'comment' => 'コラボレータID'),
         'team_id'         => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'チームID(belongsToでTeamモデルに関連)'),
-        'key_result_id'   => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'キーリザルトID(belongsToでKeyResultモデルに関連)'),
+        'goal_id'         => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'ゴールID(belongsToでGoalモデルに関連)'),
         'user_id'         => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'ユーザID(belongsToでUserモデルに関連)'),
         'type'            => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false, 'comment' => 'タイプ(0 = コラボレータ,1 = リーダー)'),
         'role'            => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '役割', 'charset' => 'utf8'),
@@ -106,11 +106,11 @@ class AppSchema extends CakeSchema
         'created'         => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '追加した日付時刻'),
         'modified'        => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '更新した日付時刻'),
         'indexes'         => array(
-            'PRIMARY'       => array('column' => 'id', 'unique' => 1),
-            'team_id'       => array('column' => 'team_id', 'unique' => 0),
-            'key_result_id' => array('column' => 'key_result_id', 'unique' => 0),
-            'user_id'       => array('column' => 'user_id', 'unique' => 0),
-            'del_flg'       => array('column' => 'del_flg', 'unique' => 0)
+            'PRIMARY' => array('column' => 'id', 'unique' => 1),
+            'team_id' => array('column' => 'team_id', 'unique' => 0),
+            'goal_id' => array('column' => 'goal_id', 'unique' => 0),
+            'user_id' => array('column' => 'user_id', 'unique' => 0),
+            'del_flg' => array('column' => 'del_flg', 'unique' => 0)
         ),
         'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
     );
