3f8242f8feb81afe300a7639a93bcfba7aebe622
Daiki Hirakata <hirakatad@isao.co.jp>
2015-01-26 10:59:43 +0900

    開発環境アップデート用shellにエラー処理を追加。

--- etc/local/update_app.sh
+++ etc/local/update_app.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+red="\033[31m"  #赤
+gre="\033[32m"  #緑
+whit="\033[1;37m"   #ホワイト
+blu="\033[34m"  #青
+
 TIME_A=`date +%s`
 
-echo "### Update Application. ###"
+echo "${blu}### Update Application. ###${whit}"
 echo "Please wait a minute..."
 echo "*** Updating all git submodules."
-git submodule update --init --recursive
-echo "*** Done."
+git submodule update --init --recursive || { echo "${red}*** Error!!${whit}" ; exit 1 ; }
+echo "${gre}*** Done.${whit}"
 echo "*** git fetch"
-git fetch
-echo "*** Done."
+git fetch || { echo "${red}*** Error!!${whit}" ; exit 1 ; }
+echo "${gre}*** Done.${whit}"
 echo "*** git pull"
-git pull
-echo "*** Done."
+git pull || { echo "${red}*** Error!!${whit}" ; exit 1 ; }
+echo "${gre}*** Done.${whit}"
 echo "*** Updating an environment by chef.(include DB schema, using library and more.)"
-vagrant provision
+vagrant provision || { echo "${red}*** Error!!${whit}" ; exit 1 ; }
 
 TIME_B=`date +%s`   #B
 PT=`expr ${TIME_B} - ${TIME_A}`
@@ -23,13 +28,13 @@ PT=`expr ${PT} % 3600`
 M=`expr ${PT} / 60`
 S=`expr ${PT} % 60`
 
-echo "*** All Done!"
+echo "${gre}*** All Done!${whit}"
 echo "*****************************"
 printf "Total Time: %02d:%02d:%02d\n" ${H} ${M} ${S}
 echo "*****************************"
 echo ""
-echo "Enjoy Development!"
-cat << EOF
+echo "${blu}Enjoy Development!${whit}"
+cat << 'EOF'
 
 
  ██████╗  ██████╗  █████╗ ██╗      ██████╗ ██╗   ██╗███████╗
