cd7d7c5d0ecd2975dd4e4c304ab2e027e86dedf2
saeki <s.saeki71@gmail.com>
2015-01-29 19:04:28 +0900

    通常の投稿に本文のバリデーションを設定 #1010

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -229,7 +229,15 @@ class Post extends AppModel
         $postData['Post']['user_id'] = $this->uid;
         $postData['Post']['team_id'] = $this->team_id;
         $postData['Post']['type'] = $type;
+
+        // bodyのバリデーションを一時的に追加
+        $this->addValidateCondBodyNotEmpty();
+
         $res = $this->save($postData);
+
+        // bodyのバリデーションを削除
+        $this->removeValidateCond('body');
+
         if (empty($res)) {
             return false;
         }
@@ -878,5 +886,23 @@ class Post extends AppModel
         }
         $res = $this->find('count', $options);
         return $res;
+    }
+
+    function addValidateCondBodyNotEmpty()
+    {
+        $bodyNotEmptyCond = [
+            'body' => [
+                'rule' => ['notEmpty']
+            ]
+        ];
+
+        $this->validate = array_merge($this->validate, $bodyNotEmptyCond);
+    }
+
+    function removeValidateCond($column)
+    {
+        if (array_key_exists($column, $this->validate)) {
+            unset($this->validate[$column]);
+        }
     }
 }
