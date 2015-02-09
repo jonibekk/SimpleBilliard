f7aef25762874aa375745ee5e5c281b25d586b16
saeki <s.saeki71@gmail.com>
2015-02-09 16:48:02 +0900

    fix comment bug and  delete triangle #1061

--- app/webroot/js/gl_basic.js
+++ app/webroot/js/gl_basic.js
@@ -1485,7 +1485,7 @@ function showMore(obj) {
             speedDown: 300,
             speedUp: 300,
             height: '105px',
-            showText: '<i class="fa fa-angle-double-down">' + cake.message.info.g + '</i>',
+            showText: '<i class="fa fa-angle-double-down">' + cake.message.info.e + '</i>',
             hideText: '<i class="fa fa-angle-double-up">' + cake.message.info.f + '</i>'
         });
     }
@@ -1501,7 +1501,7 @@ function showMore(obj) {
             speedDown: 300,
             speedUp: 300,
             height: '105px',
-            showText: '<i class="fa fa-angle-double-down">' + cake.message.info.g + '</i>',
+            showText: '<i class="fa fa-angle-double-down">' + cake.message.info.e + '</i>',
             hideText: '<i class="fa fa-angle-double-up">' + cake.message.info.h + '</i>'
         });
     }
