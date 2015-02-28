f20bf61d0810a8e57dd260e38e376eaec6a40716
Daiki Hirakata <hirakatad@isao.co.jp>
2015-03-01 01:46:25 +0900

    increase test coverage of User model. #1139

--- app/Test/Fixture/CircleFixture.php
+++ app/Test/Fixture/CircleFixture.php
@@ -56,7 +56,7 @@ class CircleFixture extends CakeTestFixture
         array(
             'id'                  => 2,
             'team_id'             => 1,
-            'name'                => 'test',
+            'name'                => 'firstname',
             'description'         => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
             'photo_file_name'     => null,
             'public_flg'          => 1,
