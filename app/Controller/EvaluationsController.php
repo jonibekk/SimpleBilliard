ef0fb4fad776faebe479ea6905a506cbb44a477f
Daiki Hirakata <hirakatad@isao.co.jp>
2015-03-12 14:24:22 +0900

    add function of viewable evaluation list. #1259

--- app/Controller/EvaluationsController.php
+++ app/Controller/EvaluationsController.php
@@ -3,14 +3,22 @@ App::uses('AppController', 'Controller');
 
 /**
  * Evaluations Controller
-
+ *
+ * @property Evaluation $Evaluation
  */
 class EvaluationsController extends AppController
 {
-
     function index()
     {
         $this->layout = LAYOUT_ONE_COLUMN;
+
+        try {
+            $this->Evaluation->checkAvailViewEvaluationList();
+        } catch (RuntimeException $e) {
+            $this->Pnotify->outError($e->getMessage());
+            return $this->redirect($this->referer());
+        }
+        //自己評価取得
 
     }
 
--- app/Model/Evaluation.php
+++ app/Model/Evaluation.php
@@ -64,4 +64,24 @@ class Evaluation extends AppModel
         'EvaluateScore',
         'Goal',
     ];
+
+    /**
+     * 評価リストの閲覧権限チェック
+     * ・評価画面の表示条件
+     * 　チームの評価機能がon かつ 自分の評価フラグがon
+     *
+     * @return bool
+     */
+    function checkAvailViewEvaluationList()
+    {
+        $my_team_member_status = $this->Team->TeamMember->getWithTeam();
+        if (!viaIsSet($my_team_member_status['TeamMember'])) {
+            throw new RuntimeException(__d('gl', "この画面にはアクセスできません。"));
+        }
+        if (!$my_team_member_status['TeamMember']['evaluation_enable_flg']) {
+            throw new RuntimeException(__d('gl', "評価設定がoffになっています。チーム管理者にご確認ください"));
+        }
+        return true;
+    }
+
 }
