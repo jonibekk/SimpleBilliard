9ff6be8d2ad6c19815672229fe032c8eeb9b3597
Daiki Hirakata <hirakatad@isao.co.jp>
2015-03-12 16:35:22 +0900

    add get setting process. #1259

--- app/Controller/EvaluationsController.php
+++ app/Controller/EvaluationsController.php
@@ -14,13 +14,20 @@ class EvaluationsController extends AppController
 
         try {
             $this->Evaluation->checkAvailViewEvaluationList();
+            if (!$this->Team->EvaluationSetting->isEnabled()) {
+                throw new RuntimeException(__d('gl', "チームの評価設定が有効になっておりません。チーム管理者にお問い合わせください。"));
+            }
         } catch (RuntimeException $e) {
             $this->Pnotify->outError($e->getMessage());
             return $this->redirect($this->referer());
         }
-        //評価設定の取得
 
-        //自己評価取得
+        //評価設定の取得
+        $is_self_on = $this->Team->EvaluationSetting->isEnabledSelf();
+        $is_evaluator_on = $this->Team->EvaluationSetting->isEnabledEvaluator();
+        $is_final_on = $this->Team->EvaluationSetting->isEnabledFinal();
+
+        $this->set(compact('is_self_on', 'is_evaluator_on', 'is_final_on'));
 
     }
 
