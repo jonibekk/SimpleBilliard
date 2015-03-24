6503b4e641f3c04518a5c2778a30eb9276c1ecab
kilhyungdoo <kilh@isao.co.jp>
2015-03-24 13:14:28 +0900

    無駄なセッション処理を除外 #1278

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -98,6 +98,8 @@ class AppController extends Controller
         $this->_setAppLanguage();
         //ログイン済みの場合のみ実行する
         if ($this->Auth->user()) {
+            $login_uid = $this->Auth->user('id');
+
             //ajaxの時以外で実行する
             if (!$this->request->is('ajax')) {
                 $this->_setMyTeam();
@@ -108,17 +110,17 @@ class AppController extends Controller
                 }
             }
             //permission check
-            $active_team_list = $this->User->TeamMember->getActiveTeamList($this->Auth->user('id'));
+            $active_team_list = $this->User->TeamMember->getActiveTeamList($login_uid);
             $set_default_team_id = !empty($active_team_list) ? key($active_team_list) : null;
 
             //デフォルトチームが設定されていない場合はアクティブなチームでカレントチームとデフォルトチームを書き換え
             if (!$this->Auth->user('default_team_id')) {
-                $this->User->updateDefaultTeam($set_default_team_id, true, $this->Auth->user('id'));
+                $this->User->updateDefaultTeam($set_default_team_id, true, $login_uid);
                 $this->Session->write('current_team_id', $set_default_team_id);
             }
             //デフォルトチームが設定されていて、カレントチームが非アクティブの場合は、デフォルトチームを書き換えてログオフ
-            elseif (!$this->User->TeamMember->isActive($this->Auth->user('id'))) {
-                $this->User->updateDefaultTeam($set_default_team_id, true, $this->Auth->user('id'));
+            elseif (!$this->User->TeamMember->isActive($login_uid)) {
+                $this->User->updateDefaultTeam($set_default_team_id, true, $login_uid);
                 //ログアウト
                 $this->Pnotify->outError(__d('gl', "アクセスしたチームのアクセス権限がありません"));
                 $this->Auth->logout();
@@ -133,6 +135,7 @@ class AppController extends Controller
         $this->set(compact('is_isao_user'));
         $my_channels_json = $this->User->getMyChannelsJson();
         $this->set(compact('my_channels_json'));
+
     }
 
     public function _setSecurity()
