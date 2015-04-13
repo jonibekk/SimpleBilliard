5a4c230900e0e82d00fc4f7c2d8ad6a3ff2ecb73
Daiki Hirakata <hirakatad@isao.co.jp>
2015-04-13 11:28:35 +0900

    fix test errors. #1436

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -24,7 +24,8 @@ class PostFixture extends CakeTestFixture
         'public_flg'           => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index'),
         'important_flg'        => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index'),
         'goal_id'              => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index'),
-        'action_result_id'     => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => 'アクションID'),
+        'circle_id'            => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'サークルID'),
+        'action_result_id'     => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'アクション結果ID'),
         'key_result_id'        => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'KR ID'),
         'photo1_file_name'     => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '投稿画像1', 'charset' => 'utf8'),
         'photo2_file_name'     => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '投稿画像2', 'charset' => 'utf8'),
@@ -36,9 +37,9 @@ class PostFixture extends CakeTestFixture
         'del_flg'              => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index', 'comment' => '削除フラグ'),
         'deleted'              => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '投稿を削除した日付時刻'),
         'created'              => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '投稿を追加した日付時刻'),
-        'modified'             => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => '投稿を更新した日付時刻'),
+        'modified'             => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => true, 'key' => 'primary', 'comment' => '投稿を更新した日付時刻'),
         'indexes'              => array(
-            'PRIMARY'          => array('column' => 'id', 'unique' => 1),
+            'PRIMARY'          => array('column' => array('id', 'modified'), 'unique' => 1),
             'user_id'          => array('column' => 'user_id', 'unique' => 0),
             'team_id'          => array('column' => 'team_id', 'unique' => 0),
             'goal_id'          => array('column' => 'goal_id', 'unique' => 0),
@@ -47,8 +48,10 @@ class PostFixture extends CakeTestFixture
             'type'             => array('column' => 'type', 'unique' => 0),
             'public_flg'       => array('column' => 'public_flg', 'unique' => 0),
             'important_flg'    => array('column' => 'important_flg', 'unique' => 0),
+            'team_id_modified' => array('column' => array('team_id', 'modified'), 'unique' => 0),
             'action_result_id' => array('column' => 'action_result_id', 'unique' => 0),
-            'key_result_id'    => array('column' => 'key_result_id', 'unique' => 0)
+            'key_result_id'    => array('column' => 'key_result_id', 'unique' => 0),
+            'circle_id'        => array('column' => 'circle_id', 'unique' => 0)
         ),
         'tableParameters'      => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
     );
