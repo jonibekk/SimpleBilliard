09a6bb309f3fc018eaa5d5ec1bd858de7dfba82b
saeki <s.saeki71@gmail.com>
2015-04-27 12:32:10 +0900

    Create outline #1568

---
+++ app/Controller/NotifyController.php
@@ -0,0 +1,30 @@
+<?php
+App::uses('AppController', 'Controller');
+
+/**
+ * Notify Controller
+ *
+ * @property SessionComponent   $Session
+ */
+class NotifyController extends AppController
+{
+
+    public function lists()
+    {
+    }
+
+    public function ajax_get_notify_list_more()
+    {
+    }
+
+    public function ajax_get_unread_count($last_id)
+    {
+    }
+
+    public function ajax_get_new_notifi_items($last_id)
+    {
+    }
+
+
+
+}
---
+++ app/Test/Case/Controller/NotifyControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+App::uses('NotifyController', 'Controller');
+
+/**
+ * NotifyController Test Case
+ * @method testAction($url = '', $options = array()) ControllerTestCase::_testAction
+
+ */
+class NotifyControllerTest extends ControllerTestCase
+{
+
+}
+
