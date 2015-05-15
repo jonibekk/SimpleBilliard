2afb00d7eed8036db9e294eb5b91fed8bf864528
Daiki Hirakata <hirakatad@isao.co.jp>
2015-05-16 03:21:12 +0900

    :arrow_upper_right: coverage. #1749

--- app/Test/Case/Controller/UsersControllerTest.php
+++ app/Test/Case/Controller/UsersControllerTest.php
@@ -348,7 +348,8 @@ class UsersControllerTest extends ControllerTestCase
                     );
         $session_value_map = [
             [null, [
-                'test' => 'test'
+                'test'   => 'test',
+                'Config' => ''
             ]],
         ];
         /** @noinspection PhpUndefinedMethodInspection */
