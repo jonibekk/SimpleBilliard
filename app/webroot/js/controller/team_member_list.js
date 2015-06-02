98ffbd2886838a5d00baec45d71ff74eab9ac775
kilhyungdoo <kilh@isao.co.jp>
2015-06-02 14:50:49 +0900

    javascriptのリファクタリング

--- app/webroot/js/controller/team_member_list.js
+++ app/webroot/js/controller/team_member_list.js
@@ -12,63 +12,81 @@ app.controller("TeamMemberMainController", function ($scope, $http) {
         // 無効化されたメンバーも表示する項目
         $scope.disp_active_flag = '1';
 
-        var showTextField = function () {
-            $scope.name_field_show = true;
-            $scope.group_field_show = false;
-        }
-        showTextField();
+        var url = '/teams/ajax_get_team_member/';
+        $http.get(url).success(function (data) {
+            $scope.team_list = data.user_info;
+        });
 
         var init = function () {
+
+            // 名前検索のテキストボックスを表示
+            $scope.name_field_show = true;
+            $scope.group_field_show = false;
+
+            // 入力値を初期化
+            $scope.name_field = '';
+            $scope.group_id = null;
+
+            // チーム全員のリストを取得する
             var url = '/teams/ajax_get_team_member_init/';
             $http.get(url).success(function (data) {
                 $scope.login_user_info = data.login_user_info;
             });
-
-            var url = '/teams/ajax_get_team_member/';
-            $http.get(url).success(function (data) {
-                $scope.team_list = data.user_info;
-            });
-        };
+        }
         init();
 
         $scope.changeFilter = function () {
+
             var filter_name = $scope.filter_name;
+
+            // グループ名が選択
             if (filter_name == 'group_name') {
-                var url = '/teams/ajax_get_current_team_group_list/';
+                init();
+                $scope.name_field_show = false;
+                $scope.group_field_show = true;
+                url = '/teams/ajax_get_current_team_group_list/';
                 $http.get(url).success(function (data) {
                     $scope.group_list = data;
                 });
-                $scope.name_field_show = false;
-                $scope.group_field_show = true;
-                init();
-            } else if (filter_name == 'coach_name') {
-                $scope.name_field = '';
 
+            // コーチ名が選択
+            } else if (filter_name == 'coach_name') {
+                init();
+                var url = '/teams/ajax_get_team_member/';
+                $http.get(url).success(function (data) {
+                    $scope.team_list = data.user_info;
+                });
+
+            //2段階認証OFF選択
             } else if (filter_name == 'two_step') {
-                $scope.name_field = '';
-                showTextField();
-                var url = '/teams/ajax_get_current_not_2fa_step_user_list/';
+                init();
+                url = '/teams/ajax_get_current_not_2fa_step_user_list/';
                 $http.get(url).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
+
+            // チーム管理者選択
             } else if (filter_name == 'team_admin') {
-                $scope.name_field = '';
-                showTextField();
-                var url = '/teams/ajax_get_current_team_admin_list/';
+                init();
+                url = '/teams/ajax_get_current_team_admin_list/';
                 $http.get(url).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
+
+            // その他
             } else {
                 init();
-                $scope.name_field = '';
-                showTextField();
+                var url = '/teams/ajax_get_team_member/';
+                $http.get(url).success(function (data) {
+                    $scope.team_list = data.user_info;
+                });
             }
         };
 
         $scope.changeGroupFilter = function () {
             var url = '/teams/ajax_get_group_member/' + $scope.group_id;
             if ($scope.group_id == null) {
-                var url = '/teams/ajax_get_team_member/';
+                url = '/teams/ajax_get_team_member/';
             }
             $http.get(url).success(function (data) {
                 $scope.team_list = data.user_info;
