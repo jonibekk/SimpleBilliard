1b25eedb5eb66dfcebec34657c64e63a33fc5a9c
kilhyungdoo <kilh@isao.co.jp>
2015-06-04 11:20:14 +0900

    codeclimateで指摘された項目の修正

--- app/webroot/js/controller/team_member_list.js
+++ app/webroot/js/controller/team_member_list.js
@@ -1,14 +1,12 @@
 var app = angular.module('myApp', ['ngRoute', 'pascalprecht.translate']).
 
     config(['$routeProvider', '$translateProvider', function ($routeProvider, $translateProvider) {
-        // ルーティング
         $routeProvider
             .when('/', {
                 controller: 'TeamMemberMainController',
                 templateUrl: '/template/team_member_list.html'
             });
 
-        // 多言語設定
         $translateProvider.useStaticFilesLoader({
             prefix: '/i18n/locale-',
             suffix: '.json'
@@ -19,88 +17,79 @@ var app = angular.module('myApp', ['ngRoute', 'pascalprecht.translate']).
 
 app.controller("TeamMemberMainController", function ($scope, $http, $translate) {
 
-        // 無効化されたメンバーも表示する項目
         $scope.disp_active_flag = '1';
 
-        var url = cake['url']['i'];
+        var url = cake.url.i;
         $http.get(url).success(function (data) {
             $scope.team_list = data.user_info;
         });
 
         var init = function () {
 
-            // 名前検索のテキストボックスを表示
             $scope.name_field_show = true;
             $scope.coach_name_field_show = false;
             $scope.group_field_show = false;
 
-            // 入力値を初期化
             $scope.name_field = '';
             $scope.group_id = null;
 
-            var url = cake['url']['j'];
+            var url = cake.url.j;
             $http.get(url).success(function (data) {
 
                 $scope.login_user_id = data.login_user_id;
                 $scope.login_user_admin_flg = data.login_user_admin_flg;
 
-                // 多言語設定（デフォルトは日本語）
                 $scope.login_user_language = data.login_user_language;
-                if ($scope.login_user_language == 'eng') {
+                if ($scope.login_user_language === 'eng') {
                     $translate.use('en');
                 }
 
                 $scope.admin_user_cnt = data.admin_user_cnt;
             });
-        }
+        };
         init();
 
         $scope.changeFilter = function () {
 
             var filter_name = $scope.filter_name;
 
-            // グループ名が選択
-            if (filter_name == 'group_name') {
+            if (filter_name === 'group_name') {
                 init();
                 $scope.name_field_show = false;
                 $scope.coach_name_field_show = false;
                 $scope.group_field_show = true;
-                url = cake['url']['k'];
+                url = cake.url.k;
                 $http.get(url).success(function (data) {
                     $scope.group_list = data;
                 });
 
-                // コーチ名が選択
-            } else if (filter_name == 'coach_name') {
+            } else if (filter_name === 'coach_name') {
                 init();
                 $scope.name_field_show = false;
                 $scope.coach_name_field_show = true;
                 $scope.group_field_show = false;
-                url = cake['url']['i'];
+                url = cake.url.i;
                 $http.get(url).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
 
-                //2段階認証OFF選択
-            } else if (filter_name == 'two_step') {
+            } else if (filter_name === 'two_step') {
                 init();
-                url = cake['url']['l'];
+                url = cake.url.l;
                 $http.get(url).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
 
-                // チーム管理者選択
-            } else if (filter_name == 'team_admin') {
+            } else if (filter_name === 'team_admin') {
                 init();
-                url = cake['url']['m'];
+                url = cake.url.m;
                 $http.get(url).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
 
-                // その他
             } else {
                 init();
-                var url = cake['url']['i'];
+                var url = cake.url.i;
                 $http.get(url).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
@@ -108,53 +97,51 @@ app.controller("TeamMemberMainController", function ($scope, $http, $translate) 
         };
 
         $scope.changeGroupFilter = function () {
-            var url = cake['url']['n'] + $scope.group_id;
-            if ($scope.group_id == null) {
-                url = cake['url']['i'];
+            var url = cake.url.n + $scope.group_id;
+            if ($scope.group_id === null) {
+                url = cake.url.i;
             }
             $http.get(url).success(function (data) {
                 $scope.team_list = data.user_info;
             });
-        }
+        };
 
         $scope.setActiveFlag = function (index, member_id, active_flg) {
-            var url = cake['url']['o'] + member_id + '/' + active_flg;
+            var url = cake.url.o + member_id + '/' + active_flg;
             $http.get(url).success(function (data) {
-                var show_flg = false;
-                if (active_flg == 'ON') {
-                    show_flg = true;
+                var active_show_flg = false;
+                if (active_flg === 'ON') {
+                    active_show_flg = true;
                 }
-                $scope.team_list[index]['TeamMember']['active_flg'] = show_flg;
+                $scope.team_list[index]['TeamMember']['active_flg'] = active_show_flg;
             });
-        }
+        };
 
         $scope.setAdminUserFlag = function (index, member_id, admin_flg) {
-            var url = cake['url']['p'] + member_id + '/' + admin_flg;
+            var url = cake.url.p + member_id + '/' + admin_flg;
             $http.get(url).success(function (data) {
 
-                if (admin_flg == 'ON') {
-                    // 設定ボタンの表示切り替え
-                    var show_flg = true;
+                var admin_show_flg = false;
+                if (admin_flg === 'ON') {
+                    admin_show_flg = true;
                     $scope.admin_user_cnt = $scope.admin_user_cnt + 1;
 
-                } else if (admin_flg == 'OFF') {
-                    var show_flg = false;
+                } else if (admin_flg === 'OFF') {
+                    admin_show_flg = false;
                     $scope.admin_user_cnt = $scope.admin_user_cnt - 1;
 
-                    // 自分を管理者から外した場合はすべての設定ボタンを表示しない
                     if ($scope.login_user_id == $scope.team_list[index]['User']['id']) {
                         $scope.login_user_admin_flg = false;
                     }
                 }
 
-                // 項目の切り替えのため(管理者にする or 管理者から外す)
-                $scope.team_list[index]['TeamMember']['admin_flg'] = show_flg;
+                $scope.team_list[index]['TeamMember']['admin_flg'] = admin_show_flg;
             });
-        }
+        };
 
         $scope.setEvaluationFlag = function (index, member_id, evaluation_flg) {
 
-            var url = cake['url']['q'] + member_id + '/' + evaluation_flg;
+            var url = cake.url.q + member_id + '/' + evaluation_flg;
             $http.get(url).success(function (data) {
 
                 var show_evaluation_flg = false;
@@ -162,7 +149,6 @@ app.controller("TeamMemberMainController", function ($scope, $http, $translate) 
                     show_evaluation_flg = true;
                 }
 
-                // 項目の切り替えのため(管理者にする or 管理者から外す)
                 $scope.team_list[index]['TeamMember']['evaluation_enable_flg'] = show_evaluation_flg;
             });
 
