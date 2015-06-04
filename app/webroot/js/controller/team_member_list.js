8988ecee3a3900fab7e8d921987d8d2bdb967745
kilhyungdoo <kilh@isao.co.jp>
2015-06-04 11:53:59 +0900

    codeclimateで指摘された項目の修正(２回目)

--- app/webroot/js/controller/team_member_list.js
+++ app/webroot/js/controller/team_member_list.js
@@ -18,11 +18,14 @@ var app = angular.module('myApp', ['ngRoute', 'pascalprecht.translate']).
 app.controller("TeamMemberMainController", function ($scope, $http, $translate) {
 
         $scope.disp_active_flag = '1';
+        var url_list = cake.url;
 
-        var url = cake.url.i;
-        $http.get(url).success(function (data) {
-            $scope.team_list = data.user_info;
-        });
+        var getAllTeamMember = function () {
+            $http.get(url_list.i).success(function (data) {
+                $scope.team_list = data.user_info;
+            });
+        };
+        getAllTeamMember();
 
         var init = function () {
 
@@ -33,8 +36,7 @@ app.controller("TeamMemberMainController", function ($scope, $http, $translate) 
             $scope.name_field = '';
             $scope.group_id = null;
 
-            var url = cake.url.j;
-            $http.get(url).success(function (data) {
+            $http.get(url_list.j).success(function (data) {
 
                 $scope.login_user_id = data.login_user_id;
                 $scope.login_user_admin_flg = data.login_user_admin_flg;
@@ -58,68 +60,64 @@ app.controller("TeamMemberMainController", function ($scope, $http, $translate) 
                 $scope.name_field_show = false;
                 $scope.coach_name_field_show = false;
                 $scope.group_field_show = true;
-                url = cake.url.k;
-                $http.get(url).success(function (data) {
+                $http.get(url_list.k).success(function (data) {
                     $scope.group_list = data;
                 });
+                getAllTeamMember();
 
             } else if (filter_name === 'coach_name') {
                 init();
                 $scope.name_field_show = false;
                 $scope.coach_name_field_show = true;
                 $scope.group_field_show = false;
-                url = cake.url.i;
-                $http.get(url).success(function (data) {
+                $http.get(url_list.i).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
 
             } else if (filter_name === 'two_step') {
                 init();
-                url = cake.url.l;
-                $http.get(url).success(function (data) {
+                $http.get(url_list.l).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
 
             } else if (filter_name === 'team_admin') {
                 init();
-                url = cake.url.m;
-                $http.get(url).success(function (data) {
+                $http.get(url_list.m).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
 
             } else {
                 init();
-                var url = cake.url.i;
-                $http.get(url).success(function (data) {
+                $http.get(url_list.i).success(function (data) {
                     $scope.team_list = data.user_info;
                 });
             }
         };
 
         $scope.changeGroupFilter = function () {
-            var url = cake.url.n + $scope.group_id;
+            var get_group_url = url_list.n + $scope.group_id;
             if ($scope.group_id === null) {
-                url = cake.url.i;
+                get_group_url = url_list.i;
             }
-            $http.get(url).success(function (data) {
+            $http.get(get_group_url).success(function (data) {
                 $scope.team_list = data.user_info;
             });
         };
 
         $scope.setActiveFlag = function (index, member_id, active_flg) {
-            var url = cake.url.o + member_id + '/' + active_flg;
-            $http.get(url).success(function (data) {
+            var change_active_flag_url = url_list.o + member_id + '/' + active_flg;
+            $http.get(change_active_flag_url).success(function (data) {
                 var active_show_flg = false;
                 if (active_flg === 'ON') {
                     active_show_flg = true;
                 }
-                $scope.team_list[index]['TeamMember']['active_flg'] = active_show_flg;
+                $scope.team_list[index].TeamMember.active_flg = active_show_flg;
             });
         };
 
         $scope.setAdminUserFlag = function (index, member_id, admin_flg) {
-            var url = cake.url.p + member_id + '/' + admin_flg;
-            $http.get(url).success(function (data) {
+            var change_admin_user_flag_url = url_list.p + member_id + '/' + admin_flg;
+            $http.get(change_admin_user_flag_url).success(function (data) {
 
                 var admin_show_flg = false;
                 if (admin_flg === 'ON') {
@@ -130,26 +128,26 @@ app.controller("TeamMemberMainController", function ($scope, $http, $translate) 
                     admin_show_flg = false;
                     $scope.admin_user_cnt = $scope.admin_user_cnt - 1;
 
-                    if ($scope.login_user_id == $scope.team_list[index]['User']['id']) {
+                    if ($scope.login_user_id == $scope.team_list[index].User.id) {
                         $scope.login_user_admin_flg = false;
                     }
                 }
 
-                $scope.team_list[index]['TeamMember']['admin_flg'] = admin_show_flg;
+                $scope.team_list[index].TeamMember.admin_flg = admin_show_flg;
             });
         };
 
         $scope.setEvaluationFlag = function (index, member_id, evaluation_flg) {
 
-            var url = cake.url.q + member_id + '/' + evaluation_flg;
-            $http.get(url).success(function (data) {
+            var change_evaluation_flag_url = url_list.q + member_id + '/' + evaluation_flg;
+            $http.get(change_evaluation_flag_url).success(function (data) {
 
                 var show_evaluation_flg = false;
-                if (evaluation_flg == 'ON') {
+                if (evaluation_flg === 'ON') {
                     show_evaluation_flg = true;
                 }
 
-                $scope.team_list[index]['TeamMember']['evaluation_enable_flg'] = show_evaluation_flg;
+                $scope.team_list[index].TeamMember.evaluation_enable_flg = show_evaluation_flg;
             });
 
         }
