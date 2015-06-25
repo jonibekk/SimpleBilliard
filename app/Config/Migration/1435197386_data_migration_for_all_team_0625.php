d91106a97eded6474de4f52f9d88e079a724bdb7
Daiki Hirakata <hirakatad@isao.co.jp>
2015-06-25 13:10:24 +0900

    :heavy_plus_sign: complete data migrations script for all team. #2068

--- app/Config/Migration/1435197386_data_migration_for_all_team_0625.php
+++ app/Config/Migration/1435197386_data_migration_for_all_team_0625.php
@@ -33,13 +33,19 @@ class DataMigrationForAllTeam0625 extends CakeMigration
     {
         if ($direction == 'up') {
             /**
-             * @var Team $Team
+             * @var Team            $Team
+             * @var TeamMember      $TeamMember
+             * @var Circle          $Circle
+             * @var CircleMember    $CircleMember
+             * @var Post            $Post
+             * @var PostShareCircle $PostShareCircle
              */
             $Team = ClassRegistry::init('Team');
-            /**
-             * @var Circle $Circle
-             */
+            $TeamMember = ClassRegistry::init('TeamMember');
             $Circle = ClassRegistry::init('Circle');
+            $Post = ClassRegistry::init('Post');
+            $CircleMember = ClassRegistry::init('CircleMember');
+            $PostShareCircle = ClassRegistry::init('PostShareCircle');
 
             //key: team_id, val: team_id
             $all_teams = $Team->find('list', ['fields' => 'id,id']);
@@ -52,33 +58,87 @@ class DataMigrationForAllTeam0625 extends CakeMigration
             ];
             //key: team_id, val: circle_id
             $exists_all_team_circles = $Circle->findWithoutTeamId('list', $options);
-            debug($exists_all_team_circles);
             //merge circle_id
             foreach ($all_teams as $key => $team_id) {
                 if (array_key_exists($team_id, $exists_all_team_circles)) {
-                    $all_teams[$key] = ['circle_id' => $exists_all_team_circles[$team_id]];
+                    //既にチーム全体サークルが存在する場合は除外
+                    unset($all_teams[$key]);
+                    continue;
+                }
+                //team_allの登録
+                $Circle->create();
+                $Circle->save(
+                    [
+                        'team_id'      => $team_id,
+                        'name'         => __d('gl', "チーム全体"),
+                        'description'  => __d('gl', "チーム全体"),
+                        'team_all_flg' => true,
+                        'public_flg'   => true,
+                    ]
+                );
+                $all_teams[$key] = ['circle_id' => $Circle->getLastInsertID()];
+            }
+            unset($exists_all_team_circles);
+
+            //team_membersテーブルからユーザデータを取得
+            foreach ($all_teams as $team_id => $val) {
+                //1件ずつ処理
+                $options = [
+                    'conditions' => ['team_id' => $team_id],
+                    'fields'     => ['user_id', 'admin_flg']
+                ];
+                if (empty($uid_list = $TeamMember->findWithoutTeamId('list', $options))) {
+                    //ユーザが存在しない場合は除外
+                    unset($all_teams[$team_id]);
                 }
                 else {
-                    //team_allの登録
-                    $Circle->create();
-                    $Circle->save(
-                        [
-                            'team_id'      => $team_id,
-                            'name'         => __d('gl', "チーム全体"),
-                            'description'  => __d('gl', "チーム全体"),
-                            'team_all_flg' => true,
-                            'public_flg'   => true,
-                        ]
-                    );
-                    $all_teams[$key] = ['circle_id' => $Circle->getLastInsertID()];
+                    $all_teams[$team_id]['uid_list'] = $uid_list;
                 }
             }
-            debug($all_teams);
+            $circle_members = [];
+            //circle_membersへのユーザ登録の準備
+            foreach ($all_teams as $team_id => $team) {
+                foreach ($team['uid_list'] as $uid => $admin_flg) {
+                    $circle_members[] = [
+                        'team_id'   => $team_id,
+                        'circle_id' => $team['circle_id'],
+                        'user_id'   => $uid,
+                        'admin_flg' => $admin_flg
+                    ];
+                }
+            }
+            //circle_membersへのユーザ登録
+            if (!empty($circle_members)) {
+                $CircleMember->saveAll($circle_members, ['validate' => false, 'deep' => false]);
+            }
 
-            //team_membersへのユーザの登録
+            //publicな全投稿idの取得
+            foreach ($all_teams as $team_id => $team) {
+                $options = [
+                    'conditions' => ['team_id' => $team_id, 'public_flg' => true],
+                    'fields'     => ['id', 'id']
+                ];
+                $all_teams[$team_id]['post_id_list'] = $Post->findWithoutTeamId('list', $options);
+            }
+            //post_share_circlesの投稿データ登録の準備
+            $post_share_circles = [];
+            //circle_membersへのユーザ登録の準備
+            foreach ($all_teams as $team_id => $team) {
+                foreach ($team['post_id_list'] as $post_id) {
+                    $post_share_circles[] = ['team_id' => $team_id, 'circle_id' => $team['circle_id'], 'post_id' => $post_id];
+                }
+            }
+            //post_share_circlesの投稿データ登録
+            //circle_membersへのユーザ登録
+            if (!empty($post_share_circles)) {
+                $PostShareCircle->saveAll($post_share_circles, ['validate' => false, 'deep' => false]);
+            }
 
-            //投稿データの移行
+            //全postのpublic_flgにfalseをセット
+            $Post->updateAll(['Post.public_flg' => false]);
+
         }
+
         if ($direction == 'down') {
 
         }
