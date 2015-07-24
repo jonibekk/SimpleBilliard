bbf691d9f1189c6d49b4007ece3042ee5f317d52
Daiki Hirakata <hirakatad@isao.co.jp>
2015-07-24 17:04:40 +0900

    :heavy_plus_sign: file type and model type on AttachedFile model. #2330

--- app/Model/AttachedFile.php
+++ app/Model/AttachedFile.php
@@ -11,6 +11,38 @@ App::uses('AppModel', 'Model');
  */
 class AttachedFile extends AppModel
 {
+    /**
+     * file type
+     */
+    const TYPE_FILE_IMG = 0;
+    const TYPE_FILE_VIDEO = 1;
+    const TYPE_FILE_DOC = 2;
+
+    static public $TYPE_FILE = [
+        self::TYPE_FILE_IMG   => null,
+        self::TYPE_FILE_VIDEO => null,
+        self::TYPE_FILE_DOC   => null,
+    ];
+
+    function _setFileTypeName()
+    {
+        self::$TYPE_FILE[self::TYPE_FILE_IMG] = __d('gl', "画像");
+        self::$TYPE_FILE[self::TYPE_FILE_VIDEO] = __d('gl', "動画");
+        self::$TYPE_FILE[self::TYPE_FILE_DOC] = __d('gl', "ドキュメント");
+    }
+
+    function __construct($id = false, $table = null, $ds = null)
+    {
+        parent::__construct($id, $table, $ds);
+
+        $this->_setFileTypeName();
+    }
+
+    /**
+     * model type
+     */
+    const TYPE_MODEL_POST = 0;
+    const TYPE_MODEL_COMMENT = 1;
 
     /**
      * Validation rules
@@ -45,19 +77,19 @@ class AttachedFile extends AppModel
      *
      * @var array
      */
-    public $belongsTo = array(
+    public $belongsTo = [
         'User',
         'Team',
-    );
+    ];
 
     /**
      * hasMany associations
      *
      * @var array
      */
-    public $hasMany = array(
+    public $hasMany = [
         'CommentFile',
         'PostFile',
-    );
+    ];
 
 }
