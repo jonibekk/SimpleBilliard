be33e20985269462896834398604efaa0c05dae7
kilhyungdoo <kilh@isao.co.jp>
2015-07-28 19:56:10 +0900

    オートスクロール実装

--- app/webroot/js/controller/message_detail.js
+++ app/webroot/js/controller/message_detail.js
@@ -1,11 +1,30 @@
-message_app.controller("MessageDetailCtrl",
-    function ($scope, $http, $translate, $sce, notificationService, getMessage, $pusher, $stateParams) {
+message_app.controller(
+    "MessageDetailCtrl",
+    function (
+        $scope,
+        $http,
+        $translate,
+        $sce,
+        notificationService,
+        getMessage,
+        $pusher,
+        $stateParams,
+        $anchorScroll,
+        $location
+    ){
+
+        var message_scroll = function () {
+            var length = $scope.message_list.length;
+            $location.hash('message_'+length);
+            $anchorScroll();
+        };
 
         var message_list = getMessage.message_list;
         angular.forEach(message_list, function (val, key) {
             message_list[key].Comment.created = $sce.trustAsHtml(val.Comment.created);
         });
         $scope.message_list = message_list;
+        message_scroll();
 
         // pusherメッセージ内容を受け取る
         var pusher = new Pusher(cake.pusher.key);
@@ -14,6 +33,7 @@ message_app.controller("MessageDetailCtrl",
         test_channel.bind('new_message', function (data) {
             data.Comment.created = $sce.trustAsHtml(data.Comment.created);
             $scope.$apply($scope.message_list.push(data));
+            message_scroll();
         });
 
         // メッセージを送信する
