59f662e1a458c323c4b83c308c40abb31d03227f
Daiki Hirakata <hirakatad@isao.co.jp>
2015-08-05 00:23:38 +0900

    :bug: fix javascript error on safari when adding a comment. #2412

--- app/webroot/js/gl_basic.js
+++ app/webroot/js/gl_basic.js
@@ -1,6 +1,12 @@
 $.ajaxSetup({
     cache: false
 });
+if (typeof String.prototype.startsWith != 'function') {
+    // see below for better implementation!
+    String.prototype.startsWith = function (str){
+        return this.indexOf(str) === 0;
+    };
+};
 $(document).ready(function () {
 
     setDefaultTab();
