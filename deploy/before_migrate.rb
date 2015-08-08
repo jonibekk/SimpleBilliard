93a6abcadee598bb5f07da2541c296993dde7de7
Daiki Hirakata <hirakatad@isao.co.jp>
2015-08-09 02:13:45 +0900

    :heavy_plus_sign: add processes of before_migrate. #2476 (cherry picked from commit bef3583)

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,8 +1,52 @@
-Chef::Log.logger.info '*********** before_migrate ***********'
-node[:deploy].each do |application,deploy|
-  deploy.each do |key,value|
-    Chef::Log.debug("deploy[:#{key}] = '#{value}'")
-  end
+Chef::Log.logger.info '*********** START OF before_migrate ***********'
+# デプロイ後の処理
+bash "composer update" do
+  code <<-EOS
+  cd #{release_path}; composer self-update; composer update --no-interaction --no-dev --prefer-dist
+  EOS
 end
-Chef::Log.debug("************ path")
-Chef::Log.debug("#{release_path}")
+bash "npm install" do
+  code <<-EOS
+  cd #{release_path}; npm install
+  EOS
+end
+
+bash "run grunt chef" do
+  code <<-EOS
+  cd #{release_path}; grunt chef
+  EOS
+end
+
+bash "ntpdate" do
+  user "root"
+  code <<-EOS
+  /etc/init.d/ntp stop
+  ntpdate ntp.ubuntu.com
+  /etc/init.d/ntp start
+  EOS
+end
+
+# tmpディレクトリ作成
+directory "#{release_path}/app/tmp" do
+  owner 'deploy'
+  group 'www-data'
+  mode 0777
+  action :create
+  not_if {::File.exists?("#{release_path}/app/tmp")}
+end
+
+# 外部API用Keyの定義
+template "#{release_path}/app/Config/extra_defines.php" do
+  mode 0644
+  source "extra_defines.php.erb"
+end
+
+# bash_profileの更新
+template "/home/ubuntu/.bash_profile" do
+  owner "ubuntu"
+  group "ubuntu"
+  mode 0644
+  source ".bash_profile"
+end
+
+Chef::Log.logger.info '*********** END OF before_migrate ***********'
