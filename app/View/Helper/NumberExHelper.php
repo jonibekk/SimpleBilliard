5abe8167ed1d1cb0bc08535bb71a7086198086ab
Yamada Kohei <dokodemologin@gmail.com>
2015-08-11 14:29:43 +0900

    メンバーページ、ゴールページの数字表記修正 #2534

--- app/View/Helper/NumberExHelper.php
+++ app/View/Helper/NumberExHelper.php
@@ -17,36 +17,43 @@ class NumberExHelper extends AppHelper
      *   1200000000  -> 1.2G
      *   12000000000 -> 12G
      *
-     * @param $num
+     * @param       $num  フォーマットする数値
+     * @param array $options
+     *                    convert_start: int  $num がこの値より大きな数値の場合だけフォーマットする
      *
      * @return string
      */
-    public function formatHumanReadable($num)
+    public function formatHumanReadable($num, $options = [])
     {
-        if ($num < 1000) {
+        $options = array_merge(
+            [
+                'convert_start' => 0,
+            ], $options);
+
+        if ($options['convert_start'] <= $num && $num < 1000) {
             // pass
         }
-        elseif ($num < 10000) {
+        elseif ($options['convert_start'] <= $num && $num < 10000) {
             // "1.2K"
             $num = sprintf("%.1fK", round($num / 1000, 1, PHP_ROUND_HALF_DOWN));
         }
-        elseif ($num < 1000000) {
+        elseif ($options['convert_start'] <= $num && $num < 1000000) {
             // "12K"
             $num = sprintf("%dK", floor($num / 1000));
         }
-        elseif ($num < 10000000) {
+        elseif ($options['convert_start'] <= $num && $num < 10000000) {
             // "1.2M"
             $num = sprintf("%.1fM", round($num / 1000000, 1, PHP_ROUND_HALF_DOWN));
         }
-        elseif ($num < 1000000000) {
+        elseif ($options['convert_start'] <= $num && $num < 1000000000) {
             // "12M"
             $num = sprintf("%dM", floor($num / 1000000));
         }
-        elseif ($num < 10000000000) {
+        elseif ($options['convert_start'] <= $num && $num < 10000000000) {
             // "1.2G"
             $num = sprintf("%.1fG", round($num / 1000000000, 1, PHP_ROUND_HALF_DOWN));
         }
-        elseif ($num < 1000000000000) {
+        elseif ($options['convert_start'] <= $num && $num < 1000000000000) {
             // "12G"
             $num = sprintf("%dG", floor($num / 1000000000));
         }
