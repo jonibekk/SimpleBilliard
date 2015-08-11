caa2c55c3a4fd427513f7767e6aa7470bae1e17d
Naruhito KUBOTA <kubotan@isao.co.jp>
2015-08-11 22:00:37 +0900

    :de: made up with a degradation #2561

--- app/webroot/js/gl_basic.js
+++ app/webroot/js/gl_basic.js
@@ -3011,6 +3011,9 @@ $(function () {
             return;
         }
 
+        // メッセージが存在するときだけ、ボタンの次の要素をドロップダウン対象にする
+        $('#click-header-message').next().addClass('dropdown-menu');
+
         // set notify number
         if (parseInt(cnt) <= 20) {
             $bellBox.children('span').html(cnt);
@@ -3066,7 +3069,13 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     var click_cnt = 0;
-    $(document).on("click", "#click-header-message", function () {
+    $(document).on("click", "#click-header-message", function (e) {
+        // 未読件数が 0 件の場合は、直接メッセージ一覧ページに遷移させる
+        if (getMessageNotifyCnt() == 0) {
+            location.href = cake.url.message_list;
+            return;
+        }
+
         initTitle();
         updateMessageListBox();
     });
@@ -3459,15 +3468,17 @@ $(document).ready(function () {
     /**
      * ドラッグ＆ドロップ対象エリアを設定する
      *
-     * selector : string  ドロップエリアにする要素のセレクタ
+     * selector : string  (* 必須) ドロップエリアにする要素のセレクタ
      * params: object {
-     *   formID: string|function  *必須 アップロードしたファイルIDを hidden で追加するフォームのID
-     *   previewContainerID: string|function  *必須  プレビューを表示するコンテナ要素のID
+     *   formID: string|function  (* 必須) アップロードしたファイルIDを hidden で追加するフォームのID
+     *   previewContainerID: string|function  (* 必須) プレビューを表示するコンテナ要素のID
      *   beforeAddedFile: function  コールバック関数
      *   beforeAccept: function   コールバック関数
      *   afterAccept: function  コールバック関数
      *   afterRemoveFile: function  コールバック関数
      *   afterSuccess: function   コールバック関数
+     *   beforeSending: function コールバック関数
+     *   afterQueueComplete: function コールバック関数
      * }
      * dzOptions: object {
      *    ...   Dropzone のオプション（デフォルトの設定を上書きする場合に指定）
@@ -3531,9 +3542,12 @@ $(document).ready(function () {
         $uploadFileForm._params.formID = formID;
         $uploadFileForm._params.previewContainerID = previewContainerID;
 
-        // Dropzone 設定
+        // Dropzone 設定を上書き
         // （Dropzone インスタンスは常に１つ）
         Dropzone.instances[0].options = $.extend({}, $uploadFileForm._dzDefaultOptions, dzOptions || {});
+        // acceptedFiles の設定は上書きされないので手動で設定
+        Dropzone.instances[0].hiddenFileInput.setAttribute("accept", Dropzone.instances[0].options.acceptedFiles);
+
 
         // コールバック関数登録
         var empty = function () {
