ec1786653a2e9cb16e10a57dbc103b7240eacb3a
Yamada Kohei <dokodemologin@gmail.com>
2015-08-13 15:35:26 +0900

    メッセージ送信後にテキストエリアにフォーカス戻す #2527

--- app/webroot/js/controller/message_detail.js
+++ app/webroot/js/controller/message_detail.js
@@ -121,6 +121,7 @@ message_app.controller(
                     event.target.disabled = ''
                     message_scroll($scope.message_list.length);
                     $scope.message = "";
+                    document.getElementById("message_text_input").focus();
                 });
             };
 
