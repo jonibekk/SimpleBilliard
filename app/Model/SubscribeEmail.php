17138a3f554a1bb8b3196109fe9b40a240689e9b
Daiki Hirakata <hirakatad@isao.co.jp>
2015-08-27 11:06:49 +0900

    :heavy_plus_sign: add email method on SubscribeEmail model. #2148

--- app/Model/SubscribeEmail.php
+++ app/Model/SubscribeEmail.php
@@ -12,16 +12,34 @@ class SubscribeEmail extends AppModel
      *
      * @var array
      */
-    public $validate = array(
-        'email'   => array(
-            'email' => array(
-                'rule' => array('email'),
-            ),
-        ),
-        'del_flg' => array(
-            'boolean' => array(
-                'rule' => array('boolean'),
-            ),
-        ),
-    );
+    public $validate = [
+        'email'   => [
+            'email' => [
+                'rule' => ['email'],
+            ],
+        ],
+        'del_flg' => [
+            'boolean' => [
+                'rule' => ['boolean'],
+            ],
+        ],
+    ];
+
+    /**
+     * @param $postData
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    function add($postData)
+    {
+        if (!$email = viaIsSet($postData['SubscribeEmail']['email'])) {
+            throw new RuntimeException(__d('gl', "送信されたデータが正しくありません。"));
+        }
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        if ($this->findByEmail($email)) {
+            throw new RuntimeException(__d('gl', "既に登録済のメールアドレスです。"));
+        }
+        return $this->save($postData);
+    }
 }
