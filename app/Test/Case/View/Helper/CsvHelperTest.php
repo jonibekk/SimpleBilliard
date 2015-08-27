20d0d43254679efe333227f93f5a59cb4b1d09dc
Daiki Hirakata <hirakatad@isao.co.jp>
2015-08-27 10:20:06 +0900

    :green_heart: add test for Helper. #2681

---
+++ app/Test/Case/View/Helper/CsvHelperTest.php
@@ -0,0 +1,127 @@
+<?php
+App::uses('View', 'View');
+App::uses('Helper', 'View');
+App::uses('CsvHelper', 'View/Helper');
+
+/**
+ * CsvHelper Test Case
+
+ */
+class CsvHelperTest extends CakeTestCase
+{
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $View = new View();
+        $this->Csv = new CsvHelper($View);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Csv);
+
+        parent::tearDown();
+    }
+
+    /**
+     * testClear method
+     *
+     * @return void
+     */
+    public function testClear()
+    {
+        $this->markTestIncomplete('testClear not implemented.');
+    }
+
+    /**
+     * testAddField method
+     *
+     * @return void
+     */
+    public function testAddField()
+    {
+        $this->markTestIncomplete('testAddField not implemented.');
+    }
+
+    /**
+     * testEndRow method
+     *
+     * @return void
+     */
+    public function testEndRow()
+    {
+        $this->markTestIncomplete('testEndRow not implemented.');
+    }
+
+    /**
+     * testAddRow method
+     *
+     * @return void
+     */
+    public function testAddRow()
+    {
+        $this->markTestIncomplete('testAddRow not implemented.');
+    }
+
+    /**
+     * testEncfputscv method
+     *
+     * @return void
+     */
+    public function testEncfputscv()
+    {
+        $this->markTestIncomplete('testEncfputscv not implemented.');
+    }
+
+    /**
+     * testRenderHeaders method
+     *
+     * @return void
+     */
+    public function testRenderHeaders()
+    {
+        $this->markTestIncomplete('testRenderHeaders not implemented.');
+    }
+
+    /**
+     * testSetFilename method
+     *
+     * @return void
+     */
+    public function testSetFilename()
+    {
+        $this->markTestIncomplete('testSetFilename not implemented.');
+    }
+
+    /**
+     * testRender method
+     *
+     * @return void
+     */
+    public function testRender()
+    {
+        $this->markTestIncomplete('testRender not implemented.');
+    }
+
+    /**
+     * testDefaultRender method
+     *
+     * @return void
+     */
+    public function testDefaultRender()
+    {
+        $this->markTestIncomplete('testDefaultRender not implemented.');
+    }
+
+}
