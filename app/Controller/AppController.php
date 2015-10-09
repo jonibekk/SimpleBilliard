d41ef5aa70933cbc7958627d8fa6fe35e4b4b104
Daiki Hirakata <hirakatad@isao.co.jp>
2015-10-09 15:38:52 +0900

    :heavy_minus_sign: filter only current goal on goal list for adding action. #2843

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -657,14 +657,26 @@ class AppController extends Controller
         $my_previous_goals = $this->Goal->getMyPreviousGoals(MY_PREVIOUS_GOALS_DISPLAY_NUMBER);
         $my_previous_goals_count = $this->Goal->getMyPreviousGoals(null, 1, 'count');
         //TODO 暫定的にアクションの候補を自分のゴールにする。あとでajax化する
-        $goal_list_for_action_option = Hash::combine(array_merge($my_goals, $collabo_goals), '{n}.Goal.id',
-                                                     '{n}.Goal.name');
+        $current_term_goals = $this->_filterCurrentTermGoals(array_merge($my_goals, $collabo_goals));
+        $goal_list_for_action_option = Hash::combine($current_term_goals, '{n}.Goal.id', '{n}.Goal.name');
         $goal_list_for_action_option = [null => __d('gl', 'ゴールを選択する')] + $goal_list_for_action_option;
         //vision
         $vision = $this->Team->TeamVision->getDisplayVisionRandom();
         $this->set(compact('vision', 'goal_list_for_action_option', 'my_goals', 'collabo_goals', 'follow_goals',
                            'my_goals_count', 'collabo_goals_count', 'follow_goals_count', 'my_previous_goals',
                            'my_previous_goals_count'));
+    }
+
+    function _filterCurrentTermGoals($goals)
+    {
+        $start = $this->Team->getCurrentTermStartDate();
+        $end = $this->Team->getCurrentTermEndDate();
+        foreach ($goals as $k => $goal) {
+            if (!($goal['Goal']['end_date'] >= $start && $goal['Goal']['end_date'] <= $end)) {
+                unset($goals[$k]);
+            }
+        }
+        return $goals;
     }
 
     /**
