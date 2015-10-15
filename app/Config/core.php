6867db314a842909c0b1bde3599d0d35d89bd13d
Daiki Hirakata <hirakatad@isao.co.jp>
2015-10-15 13:00:26 +0900

    :bug: fix session disconnect problem. #3049

--- app/Config/core.php
+++ app/Config/core.php
@@ -126,13 +126,11 @@ Configure::write('App.encoding', 'UTF-8');
  * Enables:
  *    `admin_index()` and `/admin/controller/index`
  *    `manager_index()` and `/manager/controller/index`
-
  */
 //Configure::write('Routing.prefixes', array('admin'));
 
 /**
  * Turn off all caching application-wide.
-
  */
 //Configure::write('Cache.disable', true);
 
@@ -142,7 +140,6 @@ Configure::write('App.encoding', 'UTF-8');
  * public $cacheAction inside your controllers to define caching settings.
  * You can either set it controller-wide by setting public $cacheAction = true,
  * or in each action using $this->cacheAction = true.
-
  */
 //Configure::write('Cache.check', true);
 
@@ -183,14 +180,14 @@ Configure::write('App.encoding', 'UTF-8');
  * Make sure the class implements `CakeSessionHandlerInterface` and set Session.handler to <name>
  * To use database sessions, run the app/Config/Schema/sessions.php schema using
  * the cake shell command: cake schema create Sessions
-
  */
 if (REDIS_SESSION_HOST) {
     Configure::write('Session', array(
-        'userAgent' => false,
-        'cookie'    => 'SID',
-        'timeout'   => 60 * 24 * 7 * 2, //60min * 24h * 7day * 2 = 2week
-        'handler'   => array(
+        'userAgent'      => false,
+        'cookie'         => 'SID',
+        'timeout'        => 60 * 24 * 7 * 2, //60min * 24h * 7day * 2 = 2week
+        'autoRegenerate' => true,
+        'handler'        => array(
             'engine' => 'RedisSession',
             'key'    => 'session:'
         ),
