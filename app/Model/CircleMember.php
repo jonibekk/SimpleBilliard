7cd62c3733650963d3df14a051031c09eca0ea91
Daiki Hirakata <hirakatad@isao.co.jp>
2015-11-13 10:36:25 +0900

    :heavy_plus_sign: set ttl for cashing my circle list. #3232

--- app/Model/CircleMember.php
+++ app/Model/CircleMember.php
@@ -143,6 +143,8 @@ class CircleMember extends AppModel
         }
         $res = $this->find('all', $options);
         if ($is_default) {
+            //TODO: このキャッシュは任意のタイミングでリセットするのが難しい(サークルに投稿がある度にリセットだとあまり効果ない)ので有効期限を特別にセット
+            Cache::set('duration', 60 * 60, 'user_data');//1 hour
             Cache::write($this->getCacheKey(CACHE_KEY_MY_CIRCLE_LIST, true), $res, 'user_data');
         }
         return $res;
