315ab24e95c6d3534047ffa7d4bb69ef87d58946
Sidd <gls.developer01@gmail.com>
2015-12-24 12:23:12 +0530

    #3452 - Regex issues resolved

--- app/View/Helper/TextExHelper.php
+++ app/View/Helper/TextExHelper.php
@@ -57,11 +57,41 @@ class TextExHelper extends AppHelper
         $options += array('escape' => true);
 
         # URLに使用可能な文字列のみ抽出
-
-        $pattern = '@(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.%-=#]*(\?\S+)?)?)?)@';
+        $regex = "#^" .
+            // protocol identifier
+            "(?:(?:https?|ftp):\\/\\/)?" .
+            // user:pass authentication
+            "(?:\\S+(?::\\S*)?@)?" .
+            "(?:" .
+            // IP address exclusion
+            // private & local networks
+            "(?!(?:10|127)(?:\\.\\d{1,3}){3})" .
+            "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" .
+            "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" .
+            // IP address dotted notation octets
+            // excludes loopback network 0.0.0.0
+            // excludes reserved space >= 224.0.0.0
+            // excludes network & broacast addresses
+            // (first & last IP address of each class)
+            "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" .
+            "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" .
+            "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" .
+            "|" .
+            // host name
+            "(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]-*)*[a-z\\x{00a1}-\\x{ffff}0-9]+)" .
+            // domain name
+            "(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]-*)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*" .
+            // TLD identifier
+            "(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,}))" .
+            ")" .
+            // port number
+            "(?::\\d{2,5})?" .
+            // resource path
+            "(?:\\/\\S*)?" .
+            "$#ui";
 
         $text = preg_replace_callback(
-            $pattern,
+            $regex,
             array(&$this, '_insertPlaceHolder'),
             $text
         );
