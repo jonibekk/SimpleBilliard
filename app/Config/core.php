82ce356dbf3844ea87cfc28240431cd38461f4b9
saeki <s.saeki71@gmail.com>
2016-03-01 15:21:33 +0900

    :bell: Modify debug mode | debugモードの変更 #3809

--- app/Config/core.php
+++ app/Config/core.php
@@ -21,7 +21,7 @@
 //外部ツールのKeyを取得
 require_once "extra_defines.php";
 
-Configure::write('debug', 0);
+Configure::write('debug', DEBUG_MODE);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
