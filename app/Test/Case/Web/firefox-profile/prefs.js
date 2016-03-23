d5b68b9de7a5be87b3bb1f37609c9f1c7c0231e7
iiokam <iiokam@isao.co.jp>
2016-03-23 16:23:37 +0900

    add a custom profile of firefox | firefoxのカスタムプロファイルを追加

---
+++ app/Test/Case/Web/firefox-profile/prefs.js
@@ -0,0 +1,1 @@
+user_pref("general.useragent.override", "Mozilla/5.0 (Selenium; iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25");
