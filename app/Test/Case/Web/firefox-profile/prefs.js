2752bb2e12480668f0935398d9a109add40d5f84
iiokam <iiokam@isao.co.jp>
2016-03-24 09:58:30 +0900

    change the UA to PC | UAをPCに変更
    because it is not a test of ios environment | iOS環境のテストではないため

--- app/Test/Case/Web/firefox-profile/prefs.js
+++ app/Test/Case/Web/firefox-profile/prefs.js
@@ -1,1 +1,1 @@
-user_pref("general.useragent.override", "Mozilla/5.0 (Selenium; iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25");
+user_pref("general.useragent.override", "Mozilla/5.0 (selenium; Macintosh; Intel Mac OS X 10.10; rv:44.0) Gecko/20100101 Firefox/44.0");
