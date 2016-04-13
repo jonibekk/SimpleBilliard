ad00d5382dc6c092629c9f3d6a580e65c3cecc66
saeki <s.saeki71@gmail.com>
2016-04-13 21:27:07 +0900

    :crystal_ball: refactor list page #3913

--- app/webroot/js/react/setup_guide/components/top.js
+++ app/webroot/js/react/setup_guide/components/top.js
@@ -5,11 +5,69 @@ export default class Top extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
+  listData() {
+    return ([
+      {
+        index: 1,
+        subject: 'Input your profile',
+        explain: 'Set your profile picture and self-info.',
+        link: '/setup'
+      },
+      {
+        index: 2,
+        subject: 'Login from mobile app',
+        explain: 'Install Goalous iOS and Android apps.',
+        link: '/setup'
+      },
+      {
+        index: 3,
+        subject: 'Create a goal',
+        explain: 'Create or collaborate with a goal.',
+        link: '/setup/goal_image'
+      },
+      {
+        index: 4,
+        subject: 'Do an action',
+        explain: 'Add an Action for your Goal.',
+        link: '/setup'
+      },
+      {
+        index: 5,
+        subject: 'Join a circle',
+        explain: 'Create a circle or join.',
+        link: '/setup'
+      },
+      {
+        index: 6,
+        subject: 'Post to a circle',
+        explain: 'Share your topic with a circle.',
+        link: '/setup'
+      }
+    ])
+  }
   render() {
+    var items = this.listData().map((text) => {
+      return (
+        <Link to={text.link} className="setup-items-item pt_10px mt_12px bd-radius_14px">
+          <div className="pull-left mt_3px ml_2px">
+            <div className="setup-items-item-radius-number inline-block">
+              {text.index}
+            </div>
+          </div>
+          <div className="setup-items-item-explain pull-left">
+            <p className="font_bold font_verydark">{text.subject}</p>
+            <p className="font_11px font_lightgray">{text.explain}</p>
+          </div>
+          <div className="setup-items-item-to-right pull-right mt_12px mr_5px">
+            <i className="fa fa-chevron-right font_18px"></i>
+          </div>
+        </Link>
+      )
+    });
     return (
       <div>
         <div className="setup-pankuzu font_18px">
-          :Set up Goalous
+          Set up Goalous
         </div>
         <div className="setup-status">
           <div className="setup-status-wrapper-progress col col-sm-9 col-xs-8">
@@ -28,91 +86,7 @@ export default class Top extends React.Component {
         <div className="setup-status-footer text-right font_18px">:STEPS LEFT</div>
 
         <div className="setup-items">
-          <Link to="/setup" className="setup-items-item pt_10px mt_12px bd-radius_14px">
-            <div className="pull-left mt_3px ml_2px">
-              <div className="setup-items-item-radius-number inline-block">
-                1
-              </div>
-            </div>
-            <div className="setup-items-item-explain pull-left">
-              <p className="font_bold font_verydark">:Input your profile</p>
-              <p className="font_11px font_lightgray">:Set your profile picture and self-info.</p>
-            </div>
-            <div className="setup-items-item-to-right pull-right mt_12px mr_5px">
-              <i className="fa fa-chevron-right font_18px"></i>
-            </div>
-          </Link>
-
-          <Link to="/setup" className="setup-items-item pt_10px mt_12px bd-radius_14px">
-            <div className="pull-left mt_3px ml_2px">
-              <div className="setup-items-item-radius-number inline-block">
-                2
-              </div>
-            </div>
-            <div className="setup-items-item-explain pull-left">
-              <p className="font_bold font_verydark">:Login from mobile app</p>
-              <p className="font_11px font_lightgray">:Install Goalous iOS and Android apps.</p>
-            </div>
-            <div className="setup-items-item-to-right pull-right mt_12px mr_5px">
-              <i className="fa fa-chevron-right font_18px"></i>
-            </div>
-          </Link>
-          <Link to="/setup/goal_image" className="setup-items-item pt_10px mt_12px bd-radius_14px">
-            <div className="pull-left mt_3px ml_2px">
-              <div className="setup-items-item-radius-number inline-block">
-                3
-              </div>
-            </div>
-            <div className="setup-items-item-explain pull-left">
-              <p className="font_bold font_verydark">:Create a goal</p>
-              <p className="font_11px font_lightgray">:Create or collaborate with a goal.</p>
-            </div>
-            <div className="setup-items-item-to-right pull-right mt_12px mr_5px">
-              <i className="fa fa-chevron-right font_18px"></i>
-            </div>
-          </Link>
-          <Link to="/setup" className="setup-items-item pt_10px mt_12px bd-radius_14px">
-            <div className="pull-left mt_3px ml_2px">
-              <div className="setup-items-item-radius-number inline-block">
-                4
-              </div>
-            </div>
-            <div className="setup-items-item-explain pull-left">
-              <p className="font_bold font_verydark">:Do an action</p>
-              <p className="font_11px font_lightgray">:Add an Action for your Goal.</p>
-            </div>
-            <div className="setup-items-item-to-right pull-right mt_12px mr_5px">
-              <i className="fa fa-chevron-right font_18px"></i>
-            </div>
-          </Link>
-          <Link to="/setup" className="setup-items-item pt_10px mt_12px bd-radius_14px">
-            <div className="pull-left mt_3px ml_2px">
-              <div className="setup-items-item-radius-number inline-block">
-                5
-              </div>
-            </div>
-            <div className="setup-items-item-explain pull-left">
-              <p className="font_bold font_verydark">:Join a circle</p>
-              <p className="font_11px font_lightgray">:Create a circle or join.</p>
-            </div>
-            <div className="setup-items-item-to-right pull-right mt_12px mr_5px">
-              <i className="fa fa-chevron-right font_18px"></i>
-            </div>
-          </Link>
-          <Link to="/setup" className="setup-items-item pt_10px mt_12px bd-radius_14px">
-            <div className="pull-left mt_3px ml_2px">
-              <div className="setup-items-item-radius-number inline-block">
-                6
-              </div>
-            </div>
-            <div className="setup-items-item-explain pull-left">
-              <p className="font_bold font_verydark">:Post to a circle</p>
-              <p className="font_11px font_lightgray">:Share your topic with a circle.</p>
-            </div>
-            <div className="setup-items-item-to-right pull-right mt_12px mr_5px">
-              <i className="fa fa-chevron-right font_18px"></i>
-            </div>
-          </Link>
+          {items}
         </div>
       </div>
     )
