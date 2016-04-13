59f25de74aa3f34bcaffd6536aed2aae635850c9
saeki <s.saeki71@gmail.com>
2016-04-13 19:45:09 +0900

    :earth_africa: enable react router server-side rendering #3913

--- app/Controller/SetupController.php
+++ app/Controller/SetupController.php
@@ -11,27 +11,31 @@ class SetupController extends AppController
         parent::beforeFilter();
         $this->layout = LAYOUT_ONE_COLUMN;
         $this->set('without_footer', true);
-        return $this->render();
     }
 
     public function index()
     {
+        return $this->render();
     }
 
     public function goal_image()
     {
+        return $this->render('index');
     }
 
     public function purpose_select()
     {
+        return $this->render('index');
     }
 
     public function goal_select()
     {
+        return $this->render('index');
     }
 
     public function goal_create()
     {
+        return $this->render('index');
     }
 
 }
