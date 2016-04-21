071e42cc501cdfd62c1899e9687a1bdae41a806e
saeki <s.saeki71@gmail.com>
2016-04-21 20:01:30 +0900

    :us: set method of translation to circle create page #4054

--- app/webroot/js/react/setup_guide/components/circle/circle_create.js
+++ app/webroot/js/react/setup_guide/components/circle/circle_create.js
@@ -9,41 +9,38 @@ export default class CircleCreate extends React.Component {
     return (
       <div>
         <div className="setup-pankuzu font_18px">
-          Set up Goalous <i className="fa fa-angle-right" aria-hidden="true"></i> Join a circle
+          {__("Set up Goalous")} <i className="fa fa-angle-right" aria-hidden="true"></i> {__("Join a circle")}
         </div>
         <div className="modal-header">
-            <button type="button" className="close font_33px close-design" data-dismiss="modal" aria-hidden="true">
-                <span className="close-icon">&times;</span></button>
-            <h4 className="modal-title">Create a circle</h4>
+            <h4 className="modal-title">{__("Create a circle")}</h4>
         </div>
         <form>
           <div className="modal-body modal-circle-body">
             <input ref="name" label="Circle name" />
               <div className="form-group">
-                  <label className="ccc control-label modal-label">Members</label>
+                  <label className="ccc control-label modal-label">{__("Members")}</label>
                   <div className="ddd">
                       <span className="help-block font_11px">Administrators</span>
                   </div>
               </div>
               <div className="font_brownRed font_11px">
-                  {() => {"You can't change this setting lator"}}
+                  {__("You can't change this setting lator")}
               </div>
               <div className="form-group">
-                <label className="eee control-label modal-label">プライバシー</label>
                 <label className="">
-                  <input type="radio" name="data[Circle][public_flg]" id="CirclePublicFlg1" value="1" checked="checked" /> 公開
+                  <input type="radio" name="data[Circle][public_flg]" id="CirclePublicFlg1" value="1" checked="checked" /> {__("Public")}
                   <span className="help-block font_11px">
-                    サークル名と参加メンバー、投稿がチーム内に公開されます。チームメンバーは誰でも自由に参加できます。</span>
+                    {__("Anyone can see the circle, its members and their posts.")}</span>
                 </label>
                 <label className="">
-                  <input type="radio" name="data[Circle][public_flg]" id="CirclePublicFlg0" value="0" /> 秘密
+                  <input type="radio" name="data[Circle][public_flg]" id="CirclePublicFlg0" value="0" /> {__("Privacy")}
                   <span className="help-block font_11px">
-                    サークル名と参加メンバー、投稿はこのサークルの参加メンバーだけに表示されます。サークル管理者だけがメンバーを追加できます。
+                    {__("Only members can find the circle and see posts.")}
                     </span>
                 </label>
               </div>
               <div className="form-group">
-                  <label className="f control-label modal-label">Circle Image</label>
+                  <label className="f control-label modal-label">{__("Circle Image")}</label>
                   <div className="ggg">
                       <div className="fileinput_small fileinput-new" data-provides="fileinput">
                           <div className="fileinput-preview thumbnail nailthumb-container photo-design"
@@ -52,18 +49,18 @@ export default class CircleCreate extends React.Component {
                           </div>
                           <span className="btn btn-default btn-file">
                               <span className="fileinput-new">
-                                  Select an image
+                                  {__("Select an image")}
                               </span>
                               <span className="fileinput-exists">Reselect an image</span>
                              <input type="file" name="photo" className="form-control addteam_input-design" id="GoalPhoto" />
                           </span>
-                          <span className="help-block font_11px inline-block">Smaller than 10MB</span>
+                          <span className="help-block font_11px inline-block">{__("Smaller than 10MB")}</span>
                       </div>
                   </div>
               </div>
           </div>
           <div className="panel-body">
-            <Link to="/setup/circle/select" className="btn btn-secondary setup-back-btn">Back</Link>
+            <Link to="/setup/circle/select" className="btn btn-secondary setup-back-btn">{__("Back")}</Link>
             <input type="submit" className="btn btn-primary" defaultValue="Create a goal" />
           </div>
         </form>
