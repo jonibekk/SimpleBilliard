428fd1ae07400aec8ad9260ccef835a6cba2b84f
saeki <s.saeki71@gmail.com>
2016-04-22 11:35:33 +0900

    :jack_o_lantern: improve UI of circle create page #4053

--- app/webroot/js/react/setup_guide/components/circle/circle_create.js
+++ app/webroot/js/react/setup_guide/components/circle/circle_create.js
@@ -5,63 +5,58 @@ export default class CircleCreate extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
+  handleSubmit() {
+
+  }
   render() {
     return (
       <div>
         <div className="setup-pankuzu font_18px">
           {__("Set up Goalous")} <i className="fa fa-angle-right" aria-hidden="true"></i> {__("Join a circle")}
         </div>
-        <div className="modal-header">
-            <h4 className="modal-title">{__("Create a circle")}</h4>
-        </div>
-        <form>
-          <div className="modal-body modal-circle-body">
-            <input ref="name" label="Circle name" />
-              <div className="form-group">
-                  <label className="ccc control-label modal-label">{__("Members")}</label>
-                  <div className="ddd">
-                      <span className="help-block font_11px">Administrators</span>
-                  </div>
-              </div>
-              <div className="font_brownRed font_11px">
-                  {__("You can't change this setting lator")}
-              </div>
-              <div className="form-group">
-                <label className="">
-                  <input type="radio" name="data[Circle][public_flg]" id="CirclePublicFlg1" value="1" checked="checked" /> {__("Public")}
-                  <span className="help-block font_11px">
-                    {__("Anyone can see the circle, its members and their posts.")}</span>
-                </label>
-                <label className="">
-                  <input type="radio" name="data[Circle][public_flg]" id="CirclePublicFlg0" value="0" /> {__("Privacy")}
-                  <span className="help-block font_11px">
-                    {__("Only members can find the circle and see posts.")}
-                    </span>
-                </label>
-              </div>
-              <div className="form-group">
-                  <label className="f control-label modal-label">{__("Circle Image")}</label>
-                  <div className="ggg">
-                      <div className="fileinput_small fileinput-new" data-provides="fileinput">
-                          <div className="fileinput-preview thumbnail nailthumb-container photo-design"
-                               data-trigger="fileinput">
-                              <i className="fa fa-plus photo-plus-large"></i>
-                          </div>
-                          <span className="btn btn-default btn-file">
-                              <span className="fileinput-new">
-                                  {__("Select an image")}
-                              </span>
-                              <span className="fileinput-exists">Reselect an image</span>
-                             <input type="file" name="photo" className="form-control addteam_input-design" id="GoalPhoto" />
-                          </span>
-                          <span className="help-block font_11px inline-block">{__("Smaller than 10MB")}</span>
-                      </div>
-                  </div>
-              </div>
+        <p className="font_bold font_verydark">{__("Create a new circle")}</p>
+        <form onSubmit={e => {this.handleSubmit.bind(this)}} className="form-horizontal" encType="multipart/form-data" method="post" acceptCharset="utf-8">
+          <div className="panel-body">
+            <span className="help-block">{__("Circle Name")}</span>
+            <input ref="circle_name" className="form-control addteam_input-design" />
           </div>
           <div className="panel-body">
-            <Link to="/setup/circle/select" className="btn btn-secondary setup-back-btn">{__("Back")}</Link>
-            <input type="submit" className="btn btn-primary" defaultValue="Create a goal" />
+            <span className="help-block">{__("Members")}</span>
+            <input ref="members" className="form-control addteam_input-design" />
+          </div>
+          <div className="panel-body setup-circle-public-group">
+              <input type="radio" name="public_flg" id="CirclePublicFlg1" value="1" checked="checked" /> {__("Public")}
+              <span className="help-block font_11px">
+                {__("Anyone can see the circle, its members and their posts.")}</span>
+              <input type="radio" name="public_flg" id="CirclePublicFlg0" value="0" /> {__("Privacy")}
+              <span className="help-block font_11px">
+                {__("Only members can find the circle and see posts.")}
+              </span>
+          </div>
+          <div className="panel-body">
+            <span className="help-block">{__("Circle Description")}</span>
+            <input ref="members" className="form-control addteam_input-design" />
+          </div>
+          <div className="panel-body">
+            <span className="help-block">{__("Circle Image")}</span>
+            <div className="form-inline">
+              <div className="fileinput_small fileinput-new" data-provides="fileinput">
+                <div className="fileinput-preview thumbnail nailthumb-container photo-design form-group" data-trigger="fileinput">
+                  <i className="fa fa-plus photo-plus-large"></i>
+                </div>
+                <div className="form-group">
+                  <span className="btn btn-default btn-file ml_16px">
+                    <span className="fileinput-new">{__("Select an image")}</span>
+                    <span className="fileinput-exists">{__("Reselect an image")}</span>
+                    <input type="file" name="circle_image" className="form-control addteam_input-design" />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Link to="/setup/circle/select" className="btn btn-secondary setup-back-btn">Back</Link>
+            <input type="submit" className="btn btn-primary setup-next-btn pull-right" defaultValue={__("Create")} />
           </div>
         </form>
       </div>
