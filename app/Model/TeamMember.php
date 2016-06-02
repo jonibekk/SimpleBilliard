2bc5e4c9ad9109173bc6a89144175d438acccccf
Shohei Saeki <s.saeki71@gmail.com>
2016-06-02 18:43:18 +0900

    fix typo #4228

--- app/Model/TeamMember.php
+++ app/Model/TeamMember.php
@@ -1643,7 +1643,7 @@ class TeamMember extends AppModel
                 break;
         }
 
-        // exclude email envirifed user
+        // exclude email unverified user
         $all_users_include_unverified_user = $this->find('all', $options);
         foreach ($all_users_include_unverified_user as $key => $user) {
             if (viaIsSet($user['User']['Email'][0]['email_verified'])) {
