e7fc3df22d61732c141f50e11a2cec7a6d283f15
Naveen Kumar <naveen.goalous@gmail.com>
2016-06-15 15:02:48 +0900

    added team name in email subject #559

--- app/Console/Command/SendMailShell.php
+++ app/Console/Command/SendMailShell.php
@@ -261,7 +261,8 @@ class SendMailShell extends AppShell
             'layout'   => 'default'
         );
         $options = array_merge($defaults, $options);
-        $options['subject'] = "[" . SERVICE_NAME . "]" . $options['subject'];
+        $team_name = $viewVars['team_name'];
+        $options['subject'] = "[" . $team_name . "]" . $options['subject'];
         /**
          * @var CakeEmail $Email
          */
