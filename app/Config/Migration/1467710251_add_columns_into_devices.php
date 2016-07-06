ee563b266519c9c04d1c259aeb9d6c050e30d00f
Daiki Hirakata <hirakatad@isao.co.jp>
2016-07-06 16:48:27 +0900

    :bug: fix migrations. #4426

--- app/Config/Migration/1467710251_add_columns_into_devices.php
+++ app/Config/Migration/1467710251_add_columns_into_devices.php
@@ -1,65 +1,115 @@
 <?php
-class AddColumnsIntoDevices extends CakeMigration {
 
-/**
- * Migration description
- *
- * @var string
- */
-	public $description = 'add_columns_into_devices';
+class AddColumnsIntoDevices extends CakeMigration
+{
 
-/**
- * Actions to be performed
- *
- * @var array $migration
- */
-	public $migration = array(
-		'up' => array(
-			'create_field' => array(
-				'devices' => array(
-					'installation_id' => array('type' => 'string', 'null' => false, 'key' => 'index', 'collate' => 'utf8mb4_general_ci', 'comment' => 'アプリインストール毎に発行される識別子', 'charset' => 'utf8mb4', 'after' => 'device_token'),
-					'version' => array('type' => 'string', 'null' => false, 'collate' => 'utf8mb4_general_ci', 'comment' => 'アプリバージョン', 'charset' => 'utf8mb4', 'after' => 'installation_id'),
-					'indexes' => array(
-						'device_token' => array('column' => 'device_token', 'unique' => 0, 'length' => array('191')),
-						'installation_id' => array('column' => 'installation_id', 'unique' => 0, 'length' => array('191')),
-					),
-				),
-			),
-			'alter_field' => array(
-				'devices' => array(
-					'device_token' => array('type' => 'string', 'null' => false, 'key' => 'index', 'collate' => 'utf8mb4_general_ci', 'comment' => 'nitfy cloud id', 'charset' => 'utf8mb4'),
-				),
-			),
-		),
-		'down' => array(
-			'drop_field' => array(
-				'devices' => array('installation_id', 'version', 'indexes' => array('device_token', 'installation_id')),
-			),
-			'alter_field' => array(
-				'devices' => array(
-					'device_token' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => 'アプリインストール毎に発行される識別子', 'charset' => 'utf8mb4'),
-				),
-			),
-		),
-	);
+    /**
+     * Migration description
+     *
+     * @var string
+     */
+    public $description = 'add_columns_into_devices';
 
-/**
- * Before migration callback
- *
- * @param string $direction Direction of migration process (up or down)
- * @return bool Should process continue
- */
-	public function before($direction) {
-		return true;
-	}
+    /**
+     * Actions to be performed
+     *
+     * @var array $migration
+     */
+    public $migration = array(
+        'up'   => array(
+            'create_field' => array(
+                'devices' => array(
+                    'installation_id' => array(
+                        'type'    => 'string',
+                        'null'    => true,
+                        'default' => null,
+                        'key'     => 'index',
+                        'collate' => 'utf8mb4_general_ci',
+                        'comment' => 'アプリインストール毎に発行される識別子',
+                        'charset' => 'utf8mb4',
+                        'after'   => 'device_token'
+                    ),
+                    'version'         => array(
+                        'type'    => 'string',
+                        'null'    => true,
+                        'default' => null,
+                        'collate' => 'utf8mb4_general_ci',
+                        'comment' => 'アプリバージョン',
+                        'charset' => 'utf8mb4',
+                        'after'   => 'installation_id'
+                    ),
+                    'indexes'         => array(
+                        'device_token'    => array(
+                            'column' => 'device_token',
+                            'unique' => 0,
+                            'length' => array('191')
+                        ),
+                        'installation_id' => array(
+                            'column' => 'installation_id',
+                            'unique' => 0,
+                            'length' => array('191')
+                        ),
+                    ),
+                ),
+            ),
+            'alter_field'  => array(
+                'devices' => array(
+                    'device_token' => array(
+                        'type'    => 'string',
+                        'null'    => false,
+                        'default' => null,
+                        'key'     => 'index',
+                        'collate' => 'utf8mb4_general_ci',
+                        'comment' => 'nitfy cloud id',
+                        'charset' => 'utf8mb4'
+                    ),
+                ),
+            ),
+        ),
+        'down' => array(
+            'drop_field'  => array(
+                'devices' => array(
+                    'installation_id',
+                    'version',
+                    'indexes' => array('device_token', 'installation_id')
+                ),
+            ),
+            'alter_field' => array(
+                'devices' => array(
+                    'device_token' => array(
+                        'type'    => 'string',
+                        'null'    => false,
+                        'default' => null,
+                        'collate' => 'utf8mb4_general_ci',
+                        'comment' => 'アプリインストール毎に発行される識別子',
+                        'charset' => 'utf8mb4'
+                    ),
+                ),
+            ),
+        ),
+    );
 
-/**
- * After migration callback
- *
- * @param string $direction Direction of migration process (up or down)
- * @return bool Should process continue
- */
-	public function after($direction) {
-		return true;
-	}
+    /**
+     * Before migration callback
+     *
+     * @param string $direction Direction of migration process (up or down)
+     *
+     * @return bool Should process continue
+     */
+    public function before($direction)
+    {
+        return true;
+    }
+
+    /**
+     * After migration callback
+     *
+     * @param string $direction Direction of migration process (up or down)
+     *
+     * @return bool Should process continue
+     */
+    public function after($direction)
+    {
+        return true;
+    }
 }
