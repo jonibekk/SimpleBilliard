117526e2711d7a8ccfb7d6f3b179b31d8099c4f1
Daiki Hirakata <hirakatad@isao.co.jp>
2016-07-12 11:32:55 +0900

    :put_litter_in_its_place: rm debug lines. #4459

--- app/Controller/DevicesController.php
+++ app/Controller/DevicesController.php
@@ -28,12 +28,12 @@ class DevicesController extends AppController
      *
      * @return CakeResponse
      */
-    public function add($user_id, $installation_id, $current_version)
+    public function add()
     {
-//        $this->request->allowMethod('post');
-//        $user_id = $this->request->data['user_id'];
-//        $installation_id = $this->request->data['installation_id'];
-//        $current_version = isset($this->request->data['current_version']) ? $this->request->data['current_version'] : null;
+        $this->request->allowMethod('post');
+        $user_id = $this->request->data['user_id'];
+        $installation_id = $this->request->data['installation_id'];
+        $current_version = isset($this->request->data['current_version']) ? $this->request->data['current_version'] : null;
 
         try {
             $device = $this->NotifyBiz->saveDeviceInfo($user_id, $installation_id, $current_version);
