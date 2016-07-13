ee24bd89627b39198450837325f08384bc455658
Daiki Hirakata <hirakatad@isao.co.jp>
2016-07-13 12:46:32 +0900

    :pencil2: spec for getting email verify code. #4469

--- app/Controller/SignupController.php
+++ app/Controller/SignupController.php
@@ -4,10 +4,14 @@ App::uses('AppController', 'Controller');
 /**
  * Signup Controller
  *
- * @property Signup $Signup
+ * @property Email $Email
  */
 class SignupController extends AppController
 {
+    public $uses = [
+        'Email',
+    ];
+
     /**
      * beforeFilter callback
      *
@@ -37,5 +41,45 @@ class SignupController extends AppController
 
     public function term()
     {
+    }
+
+    /**
+     * getting email verify code
+     * [GET] method only
+     * return value is json encoded
+     * e.g.
+     * {
+     * error: false,//true or false,
+     * message:"something",//if error is true then message exists. if no error, blank text
+     * verify_code:"something",//string. if unnecessary, blank text
+     * }
+     * verify code can be returned in only the following cases.
+     * 1. not exists
+     * 2. not verified
+     *
+     * @param $email
+     *
+     * @return CakeResponse
+     */
+    public function ajax_get_email_verify_code($email)
+    {
+        $this->request->allowMethod('get');
+        //init response values
+        $res = [
+            'error'       => false,
+            'message'     => "",
+            'verify_code' => "",
+        ];
+
+        try {
+            //TODO WIP
+            /** @noinspection PhpUndefinedMethodInspection */
+            $code = $this->Email->getVerifyCode($email);
+            $res['verify_code'] = $code;
+        } catch (RuntimeException $e) {
+            $res['error'] = true;
+            $res['message'] = $e->getMessage();
+        }
+        return $this->_ajaxGetResponse($res);
     }
 }
