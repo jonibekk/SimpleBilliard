9c80d5fcf4472ada234d75e8421fedff80203ea7
Shohei Saeki <s.saeki71@gmail.com>
2016-07-13 19:18:30 +0900

    クライアント側データ設計途中まで #4481

---
+++ app/webroot/js/react/signup/constants/ActionTypes.js
@@ -0,0 +1,9 @@
+// auth
+export const POST_AUTH_CODE = 'POST_AUTH_CODE'
+export const CHECKING_AUTH_CODE = 'CHECKING_AUTH_CODE'
+export const CHECKING_AUTH_CODE = 'EMAIL_IS_LOCKED'
+
+// team
+
+
+// user
---
+++ app/webroot/js/react/signup/reducers/auth.js
@@ -0,0 +1,8 @@
+import {  } from '../constants/ActionTypes'
+
+const initialState = {
+  goals: [],
+  selected_action_goal: {},
+  can_click_submit_button: false,
+  fetched_goals: false
+}
