2ad32020a51c7fe62f84e00e5aefe4265e93ffe4
Daiki Hirakata <hirakatad@isao.co.jp>
2016-07-13 16:02:31 +0900

    :pencil2: change spec for getting email verify code. #4469

--- app/Controller/SignupController.php
+++ app/Controller/SignupController.php
@@ -44,24 +44,23 @@ class SignupController extends AppController
     }
 
     /**
-     * getting email verify code
+     * generating email verify code
+     * and sending it by e-mail
      * [GET] method only
      * return value is json encoded
      * e.g.
      * {
      * error: false,//true or false,
-     * message:"something",//if error is true then message exists. if no error, blank text
-     * verify_code:"something",//string. if unnecessary, blank text
+     * message:"something is wrong",//if error is true then message exists. if no error, blank text
      * }
-     * verify code can be returned in only the following cases.
-     * 1. not exists
-     * 2. not verified
+     * verify code can be sent in only not verified.
+     * if not verified and record exists, remove and regenerate it.
      *
      * @param $email
      *
      * @return CakeResponse
      */
-    public function ajax_get_email_verify_code($email)
+    public function ajax_generate_email_verify_code($email)
     {
         $this->request->allowMethod('get');
         //init response values
@@ -76,6 +75,9 @@ class SignupController extends AppController
             /** @noinspection PhpUndefinedMethodInspection */
             $code = $this->Email->getVerifyCode($email);
             $res['verify_code'] = $code;
+
+            //store 6digit code for session
+
         } catch (RuntimeException $e) {
             $res['error'] = true;
             $res['message'] = $e->getMessage();
