574f456e1a8384a4088e386873b5d83a26ccf8db
Daiki Hirakata <hirakatad@isao.co.jp>
2016-07-13 18:47:34 +0900

    :pencil2: add spec for validation api. #4474

--- app/Controller/SignupController.php
+++ app/Controller/SignupController.php
@@ -17,6 +17,11 @@ class SignupController extends AppController
     {
         parent::beforeFilter();
         $this->layout = LAYOUT_ONE_COLUMN;
+        $allowed_actions = ['ajax_validation_fields'];
+        //ajaxのPOSTではフォーム改ざんチェック用のハッシュ生成ができない為、ここで改ざんチェックを除外指定
+        if (in_array($this->request->params['action'], $allowed_actions)) {
+            $this->Security->validatePost = false;
+        }
     }
 
     public function auth()
@@ -37,5 +42,44 @@ class SignupController extends AppController
 
     public function term()
     {
+    }
+
+    /**
+     * validation fields
+     * e.g.
+     * $this->request->data['User']['first_name']
+     *
+     * @return CakeResponse|null
+     */
+    public function ajax_validation_fields()
+    {
+        $this->_ajaxPreProcess();
+        $this->request->allowMethod('post');
+        //init response values
+        $res = [
+            'error'   => false,
+            'message' => "",
+        ];
+        //TODO WIP
+        $white_list = [
+            'User'  => [
+                'first_name',
+                'last_name'
+            ],
+            'Email' => [
+                'email'
+            ]
+        ];
+        try {
+            if (empty($this->request->data)) {
+                throw new RuntimeException(__('No Datas'));
+            }
+            //white list checking
+
+        } catch (RuntimeException $e) {
+            $res['error'] = true;
+            $res['message'] = $e->getMessage();
+        }
+        return $this->_ajaxGetResponse($res);
     }
 }
