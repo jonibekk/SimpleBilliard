7e33579d72f81d202b932abe367be0ad4234d299
Shohei Saeki <s.saeki71@gmail.com>
2016-07-14 15:58:55 +0900

    design password code data and write test #4481

--- app/webroot/js/react/signup/reducers/password.js
+++ app/webroot/js/react/signup/reducers/password.js
@@ -1,18 +1,26 @@
-import {  } from '../constants/ActionTypes'
+import { INPUT_PASSWORD, PASSWORD_IS_INVALID, CHECKING_PASSWORD } from '../constants/ActionTypes'
 
 const initialState = {
-  inputed_code: null,
-  checking_auth_code: false,
-  email_is_locked: false,
-  code_is_invalid: false,
+  inputed_password: '',
+  checking_password: false,
+  password_is_invalid: false,
   invalid_message: ''
 }
 
 export default function password(state = initialState, action) {
   switch (action.type) {
-    case INPUT_CODE:
+    case INPUT_PASSWORD:
       return Object.assign({}, state, {
-        inputed_code: action.inputed_code
+        inputed_password: action.inputed_password
+      })
+    case PASSWORD_IS_INVALID:
+      return Object.assign({}, state, {
+        password_is_invalid: true,
+        invalid_message: action.invalid_message
+      })
+    case CHECKING_PASSWORD:
+      return Object.assign({}, state, {
+        checking_password: true
       })
     default:
       return state;
