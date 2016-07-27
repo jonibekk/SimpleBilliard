cb10bd774edd6249e351b3093319279a033b76f9
Naveen Kumar <naveen.goalous@gmail.com>
2016-07-27 12:18:57 +0900

    added code for new tag #4603

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -151,6 +151,8 @@ define('OOV_BASE_URL', 'https://view.officeapps.live.com/op/view.aspx?src=');
 define('SELECT2_QUERY_LIMIT', 200);
 //リクエストされた時点のタイムスタンプ
 define('REQUEST_TIMESTAMP', time());
+// timestamp before 1hr
+define('REQUEST_TIMESTAMP_ONE_HR_AGO', time()-3600);
 //右カラム各要素の表示数
 define('MY_GOALS_DISPLAY_NUMBER', 5);
 define('MY_COLLABO_GOALS_DISPLAY_NUMBER', 5);
