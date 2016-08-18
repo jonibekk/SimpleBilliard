378bfc36492c8795ed1bf4fd041650376666ddc8
Daiki Hirakata <hirakatad@isao.co.jp>
2016-08-18 10:55:23 +0900

    :heavy_plus_sign: add column to messages table. #4304

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -807,6 +807,7 @@ class AppSchema extends CakeSchema {
 		'team_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'チームID(belongsToでTeamモデルに関連)'),
 		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => 'メッセ本文', 'charset' => 'utf8mb4'),
 		'type' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 3, 'unsigned' => true, 'comment' => 'メッセタイプ(1:Nomal,2:メンバー追加,3:メンバー削除,4:トピック名変更)'),
+		'target_user_ids_if_member_changed' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '変更したユーザのuser_idをカンマ区切りで指定', 'charset' => 'utf8mb4'),
 		'del_flg' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'comment' => '削除フラグ'),
 		'deleted' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'comment' => '削除した日付時刻'),
 		'created' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => true, 'key' => 'primary', 'comment' => '追加した日付時刻'),
--- app/Test/Fixture/MessageFixture.php
+++ app/Test/Fixture/MessageFixture.php
@@ -13,7 +13,7 @@ class MessageFixture extends CakeTestFixtureEx
      * @var array
      */
     public $fields = [
-        'id'                 => [
+        'id'                                => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
@@ -21,14 +21,14 @@ class MessageFixture extends CakeTestFixtureEx
             'key'      => 'primary',
             'comment'  => 'ID'
         ],
-        'topic_id'           => [
+        'topic_id'                          => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
             'unsigned' => true,
             'comment'  => 'topic ID(belongsToでTopicモデルに関連)'
         ],
-        'user_id'            => [
+        'user_id'                           => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
@@ -36,7 +36,7 @@ class MessageFixture extends CakeTestFixtureEx
             'key'      => 'index',
             'comment'  => 'メッセしたユーザID(belongsToでUserモデルに関連)'
         ],
-        'team_id'            => [
+        'team_id'                           => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
@@ -44,7 +44,7 @@ class MessageFixture extends CakeTestFixtureEx
             'key'      => 'index',
             'comment'  => 'チームID(belongsToでTeamモデルに関連)'
         ],
-        'body'               => [
+        'body'                              => [
             'type'    => 'text',
             'null'    => true,
             'default' => null,
@@ -52,7 +52,7 @@ class MessageFixture extends CakeTestFixtureEx
             'comment' => 'メッセ本文',
             'charset' => 'utf8mb4'
         ],
-        'type'               => [
+        'type'                              => [
             'type'     => 'integer',
             'null'     => false,
             'default'  => '1',
@@ -60,15 +60,28 @@ class MessageFixture extends CakeTestFixtureEx
             'unsigned' => true,
             'comment'  => 'メッセタイプ(1:Nomal,2:メンバー追加,3:メンバー削除,4:メンバー離脱)'
         ],
-        'del_flg'            => ['type' => 'boolean', 'null' => false, 'default' => '0', 'comment' => '削除フラグ'],
-        'deleted'            => [
+        'target_user_ids_if_member_changed' => [
+            'type'    => 'string',
+            'null'    => true,
+            'default' => null,
+            'collate' => 'utf8mb4_general_ci',
+            'comment' => '変更したユーザのuser_idをカンマ区切りで指定',
+            'charset' => 'utf8mb4'
+        ],
+        'del_flg'                           => [
+            'type'    => 'boolean',
+            'null'    => false,
+            'default' => '0',
+            'comment' => '削除フラグ'
+        ],
+        'deleted'                           => [
             'type'     => 'integer',
             'null'     => true,
             'default'  => null,
             'unsigned' => true,
             'comment'  => '削除した日付時刻'
         ],
-        'created'            => [
+        'created'                           => [
             'type'     => 'integer',
             'null'     => false,
             'default'  => '0',
@@ -76,20 +89,24 @@ class MessageFixture extends CakeTestFixtureEx
             'key'      => 'primary',
             'comment'  => '追加した日付時刻'
         ],
-        'modified'           => [
+        'modified'                          => [
             'type'     => 'integer',
             'null'     => true,
             'default'  => null,
             'unsigned' => true,
             'comment'  => '更新した日付時刻'
         ],
-        'indexes'            => [
-            'PRIMARY' => ['column' => ['id', 'created'], 'unique' => 1],
+        'indexes'                           => [
+            'PRIMARY' => ['column' => ['id'], 'unique' => 1],
             'user_id' => ['column' => 'user_id', 'unique' => 0],
             'team_id' => ['column' => 'team_id', 'unique' => 0],
             'created' => ['column' => 'created', 'unique' => 0]
         ],
-        'tableParameters'    => ['charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB']
+        'tableParameters'                   => [
+            'charset' => 'utf8mb4',
+            'collate' => 'utf8mb4_general_ci',
+            'engine'  => 'InnoDB'
+        ]
     ];
 
     /**
