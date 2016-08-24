a54cbaf74cd9fd2a4a66460d239b7477d5965c30
Daiki Hirakata <hirakatad@isao.co.jp>
2016-08-24 11:57:52 +0900

    :heavy_plus_sign: add script for removing all instances on layer. #4820

--- etc/server/rm_all_instance_on_layer.sh
+++ etc/server/rm_all_instance_on_layer.sh
@@ -11,12 +11,19 @@ do
   esac
 done
 
-DESC_LAYER=`aws opsworks --region us-east-1 describe-instances --layer-id ${LAYER} | jq '.Instances[].InstanceId'`
-while read line
-do
-echo ${line}
-#    echo "test ${line} test"
-#    aws opsworks --region us-east-1 delete-instance --instance-id ${line}
-done <<END
-$DESC_LAYER
-END
+DESC_LAYER=$(aws opsworks --region us-east-1 describe-instances --layer-id ${LAYER})
+
+len=$(echo $DESC_LAYER | jq '.Instances | length')
+removed_count=0
+for i in $( seq 0 $(($len - 1)) ); do
+  InstanceId=$(echo $DESC_LAYER | jq .Instances[$i].InstanceId)
+  Status=$(echo $DESC_LAYER | jq .Instances[$i].Status)
+  if [ $Status = '"stopped"' ]; then
+    $(aws opsworks delete-instance --region us-east-1 --instance-id ${InstanceId:1:36})
+    echo "$InstanceId was deleted"
+    removed_count=$((removed_count+1))
+  fi
+
+done
+
+echo  "$removed_count/$len has been deleted!"
