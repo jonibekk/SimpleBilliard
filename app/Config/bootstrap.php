b3ef657ab6fcb9f8c4acb46746eb5e5c66d36969
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-07 00:35:04 +0900

    :white_check_mark: fix testing. #4876

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -107,7 +107,7 @@ App::build([
         ],
     ]
 );
-if (preg_match('/api\/(v[0-9]+)/i', $_SERVER['REQUEST_URI'], $matches)) {
+if (isset($_SERVER['REQUEST_URI']) && preg_match('/api\/(v[0-9]+)/i', $_SERVER['REQUEST_URI'], $matches)) {
     App::build([
         'Controller' => [
             ROOT . DS . APP_DIR . DS . 'Controller' . DS . 'Api' . DS . strtoupper($matches[1]) . DS,
