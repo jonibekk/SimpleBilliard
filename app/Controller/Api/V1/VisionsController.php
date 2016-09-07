63be3616ef18bcbb1181894b750a7bc71999eff5
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-07 17:56:06 +0900

    :heavy_plus_sign: add api for getting visions. #4876

---
+++ app/Controller/Api/V1/VisionsController.php
@@ -0,0 +1,41 @@
+<?php
+App::uses('ApiController', 'Controller/Api');
+App::uses('TeamVision', 'Model');
+App::uses('GroupVision', 'Model');
+
+/**
+ * Visions Controller
+ * Created by PhpStorm.
+ * User: daikihirakata
+ * Date: 9/6/16
+ * Time: 16:38
+ *
+ * @property TeamVision  $TeamVision
+ * @property GroupVision $GroupVision
+ */
+class VisionsController extends ApiController
+{
+    public $uses = [
+        'TeamVision',
+        'GroupVision',
+    ];
+
+    /**
+     * GET visions
+     *
+     * @return string
+     */
+    function index()
+    {
+        $team_visions = Hash::insert(
+            Hash::extract($this->TeamVision->getTeamVision($this->current_team_id, true, true),
+                '{n}.TeamVision'), '{n}.type', 'team_vision');
+        $group_visions = Hash::insert(
+            Hash::extract($this->GroupVision->getMyGroupVision(true),
+                '{n}.GroupVision'), '{n}.type', 'group_vision');
+
+        $visions = am($team_visions, $group_visions);
+        return json_encode($visions);
+    }
+
+}
