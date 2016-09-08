c50c8cf4b4521700da802972164a2663c95a9c4b
Shohei Saeki <s.saeki71@gmail.com>
2016-09-08 14:46:36 +0900

    put approval html mock to spa mock #4886

--- app/webroot/js/react/goal_approval/components/detail.js
+++ app/webroot/js/react/goal_approval/components/detail.js
@@ -5,9 +5,44 @@ export default class DetailComponent extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Approval Detail</h1>
-      </div>
+      <section className="panel panel-default col-sm-8 col-sm-offset-2 clearfix">
+
+          <h1 className="goals-approval-detial-heading">New goal created !</h1>
+
+          <div className="goals-approval-detial-display-user">
+              <img src="" alt="" />
+              <p>name</p>
+              <p>time</p>
+          </div>
+
+          <div className="goals-approval-detial-display-tkr">
+              <img src="" alt="" />
+              <p>goal name</p>
+
+              <h4>Top KR :</h4>
+              <ul>
+                  <li>tKR name</li>
+                  <li>tKR value</li>
+                  <li>tKR Desc</li>
+              </ul>
+          </div>
+
+          <Link className="btn" to="">Follow</Link>
+          <Link className="btn" to="">View Deatil</Link>
+
+          <form className="goals-approval-detial-input" action="">
+              <p>Is this top KR clear ?</p>
+              <Link className="btn btn-primary" to="">Yes, clear!</Link>
+              <Link className="btn btn-primary" to="">Not clear...</Link>
+
+              <div>Clear</div>
+              <div>Not Clear</div>
+
+              <textarea className="form-control goals-approval-detial-input-advice-form" name="" id="" cols="30" rows="10" placeholder="Your advice..."></textarea>
+          </form>
+
+      </section>
+
     )
   }
 }
--- app/webroot/js/react/goal_approval/components/list.js
+++ app/webroot/js/react/goal_approval/components/list.js
@@ -5,9 +5,17 @@ export default class ListComponent extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Approval List</h1>
-      </div>
+      <section className="panel panel-default col-sm-8 col-sm-offset-2 clearfix">
+          <div className="goals-approval-list-item">
+              <img className="goals-approval-list-item-image" src="" alt="" />
+              <p>goal name</p>
+              <p>user name</p>
+              <Link className="btn" to="/goals/approval/detail">Edit</Link>
+              <Link className="btn" to="">Not to be evaluated</Link>
+          </div>
+
+      </section>
+
     )
   }
 }
