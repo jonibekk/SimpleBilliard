35f41c0c3ec882933791c4022a127923bf6ddb46
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-11 06:59:19 +0900

    :art: refactor maintenance scripts. #4982

--- etc/server/stg_switch_back_from_mente_env.sh
+++ etc/server/stg_switch_back_from_mente_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
-CHANGE_ID=$(aws route53 change-resource-record-sets --hosted-zone-id Z11UORJ1AGZ33F --change-batch file://${SCRIPT_DIR}/mente_to_stg.json --output text --query ChangeInfo.Id)
+aws route53 change-resource-record-sets --hosted-zone-id Z11UORJ1AGZ33F --change-batch file://${SCRIPT_DIR}/json/mente_to_stg.json
 
