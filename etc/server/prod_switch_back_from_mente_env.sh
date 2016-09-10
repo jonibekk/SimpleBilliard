bc78c7b8a6c4a5bca2e5b54271d3bf1fae530955
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-11 06:41:30 +0900

    :heavy_minus_sign: rm stopping instance for maintenance. #4982

--- etc/server/prod_switch_back_from_mente_env.sh
+++ etc/server/prod_switch_back_from_mente_env.sh
@@ -2,4 +2,3 @@
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 CHANGE_ID=$(aws route53 change-resource-record-sets --hosted-zone-id Z11UORJ1AGZ33F --change-batch file://${SCRIPT_DIR}/mente_to_prod.json --output text --query ChangeInfo.Id)
-aws route53 wait resource-record-sets-changed --id "${CHANGE_ID}"; aws ec2 stop-instances --instance-ids i-4d1c1abe
