16dc296b5ac90320364c8c6c7404659716354f68
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-12 06:02:10 +0900

    :heavy_plus_sign: get categories on api. #4849

--- app/Controller/Api/V1/GoalsController.php
+++ app/Controller/Api/V1/GoalsController.php
@@ -20,4 +20,27 @@ class GoalsController extends ApiController
     {
         return $this->_getResponseDefaultValidation($this->Goal);
     }
+
+    /**
+     * Goal作成&編集においての初期化処理
+     * formで利用する値を取得する
+     *
+     * @query_params bool categories
+     * @query_params bool tags
+     */
+    function get_init_form()
+    {
+        $res = [];
+
+        if ($this->request->query('categories') == true) {
+            $categories = $this->Goal->GoalCategory->getCategoryList();
+            $res['categories'] = $categories;
+        }
+
+//        if ($this->request->query('tags') == true) {
+//            $res['tags'] = true;
+//        }
+
+        return $this->_getResponseSuccess($res);
+    }
 }
