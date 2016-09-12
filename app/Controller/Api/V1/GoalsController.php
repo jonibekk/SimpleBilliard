52d1b8376a8da54e58e559f320e74dc94ad37676
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-12 11:57:41 +0900

    :art: validation on GoalsController. #4860

--- app/Controller/Api/V1/GoalsController.php
+++ app/Controller/Api/V1/GoalsController.php
@@ -23,17 +23,7 @@ class GoalsController extends ApiController
 
     function post()
     {
-        if (!viaIsSet($this->request->data['key_result'])) {
-            return $this->_getResponseBadFail(__('top Key Result is required!'));
-        }
-        $validation = $this->_validationExtract($this->Goal->validateGoalCreate($this->request->data));
-        $kr_validation = $this->_validationExtract($this->Goal->KeyResult->validateKrCreate($this->request->data['key_result']));
-        if (!empty($kr_validation)) {
-            $validation['key_result'] = $kr_validation;
-        }
-        if (!empty($validation)) {
-            return $this->_getResponseBadFail(__('Saving Data Failed!'), $validation);
-        }
+        $this->_validateCreateGoal($this->request->data);
 
         /**
          * 登録処理
@@ -44,6 +34,27 @@ class GoalsController extends ApiController
          */
 
         return $this->_getResponseSuccess(['goal_id' => $this->Goal->getLastInsertID()]);
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return bool|void
+     */
+    function _validateCreateGoal($data)
+    {
+        if (!viaIsSet($data['key_result'])) {
+            return $this->_getResponseBadFail(__('top Key Result is required!'));
+        }
+        $validation = $this->_validationExtract($this->Goal->validateGoalCreate($data));
+        $kr_validation = $this->_validationExtract($this->Goal->KeyResult->validateKrCreate($data['key_result']));
+        if (!empty($kr_validation)) {
+            $validation['key_result'] = $kr_validation;
+        }
+        if (!empty($validation)) {
+            return $this->_getResponseBadFail(__('Saving Data Failed!'), $validation);
+        }
+        return true;
     }
 
 }
