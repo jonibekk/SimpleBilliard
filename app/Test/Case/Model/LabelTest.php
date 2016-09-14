09ef9e8c9f86fda56f1f3cc2b3db71773cadde2d
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-14 10:57:14 +0900

    :white_check_mark: add testGetListWithGoalCount on LabelTest. #4860

--- app/Test/Case/Model/LabelTest.php
+++ app/Test/Case/Model/LabelTest.php
@@ -92,9 +92,45 @@ class LabelTest extends GoalousTestCase
         parent::tearDown();
     }
 
-    function testDummy()
+    function testGetListWithGoalCount()
     {
+        $this->_setDefault();
+        $this->_saveDefaultData();
+        $actual = $this->Label->getListWithGoalCount();
+        $this->assertEquals(['test3', 'test4', 'test2', 'test1'], Hash::extract($actual, '{n}.Label.name'));
+    }
 
+    function _setDefault()
+    {
+        $this->Label->current_team_id = 1;
+        $this->Label->my_uid = 1;
+    }
+
+    function _saveDefaultData()
+    {
+        $fixture_data = [
+            [
+                'name'             => 'test1',
+                'team_id'          => 1,
+                'goal_label_count' => '1',
+            ],
+            [
+                'name'             => 'test2',
+                'team_id'          => 1,
+                'goal_label_count' => '2',
+            ],
+            [
+                'name'             => 'test3',
+                'team_id'          => 1,
+                'goal_label_count' => '4',
+            ],
+            [
+                'name'             => 'test4',
+                'team_id'          => 1,
+                'goal_label_count' => '3',
+            ],
+        ];
+        $this->Label->saveAll($fixture_data);
     }
 
 }
