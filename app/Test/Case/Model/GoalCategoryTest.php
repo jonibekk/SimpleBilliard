2df6a81dfedd9f6af48ef296a84d57edea14ec2e
Daiki Hirakata <hirakatad@isao.co.jp>
2016-09-14 11:53:19 +0900

    :green_heart: exclude unnecessary fixtures. #4860

--- app/Test/Case/Model/GoalCategoryTest.php
+++ app/Test/Case/Model/GoalCategoryTest.php
@@ -17,30 +17,6 @@ class GoalCategoryTest extends GoalousTestCase
     public $fixtures = array(
         'app.goal_category',
         'app.team',
-        'app.badge',
-        'app.user',
-        'app.email',
-        'app.notify_setting',
-        'app.comment_like',
-        'app.comment',
-        'app.post',
-        'app.post_share_user',
-        'app.post_share_circle',
-        'app.circle',
-        'app.circle_member',
-        'app.post_like',
-        'app.post_read',
-        'app.comment_mention',
-        'app.given_badge',
-        'app.post_mention',
-        'app.comment_read',
-
-        'app.oauth_token',
-        'app.team_member',
-        'app.group',
-        'app.job_category',
-        'app.local_name',
-        'app.invite',
         'app.goal'
     );
 
