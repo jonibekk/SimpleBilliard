1f19268eeb8875bec49694f866149d843c041c76
endam <yoshidam2@isao.co.jp>
2016-09-20 11:48:08 +0900

    fix bug 「Cannot read property」 #5037

--- app/webroot/js/react/goal_create/reducers/goal.js
+++ app/webroot/js/react/goal_create/reducers/goal.js
@@ -48,7 +48,7 @@ export default function goal(state = initialState, action) {
         inputData,
         suggestionsExcludeSelected,
         toNextPage: false,
-        validationErrors:{}
+        validationErrors:{key_result:{}}
       })
 
     case types.REQUEST_SUGGEST:
