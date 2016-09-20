bbaaa3271c079ab3470160a20d21ee675a7e7b10
Shohei Saeki <s.saeki71@gmail.com>
2016-09-20 17:29:24 +0900

    refactor #4882

--- app/webroot/js/react/goal_approval/components/elements/list_coachee_card.js
+++ app/webroot/js/react/goal_approval/components/elements/list_coachee_card.js
@@ -3,20 +3,24 @@ import { Link } from 'react-router'
 
 export class CoacheeCard extends React.Component {
   render() {
+    const goal_approval = this.props.goal_approval
+    const is_leader = goal_approval.collaborator.type == 1
+    const is_evaluated = goal_approval.collaborator.approval_status == 1
+    const is_wating = goal_approval.collaborator.status_type !== null
     let role = ''
     let status = ''
     let is_incomplete = false
 
     // Define role
-    if(this.props.goal_approval.collaborator.type == 1) {
+    if(is_leader) {
       role = __('Leader')
     } else {
       role = __('Collaborator')
     }
      // Define status
-    if(this.props.goal_approval.collaborator.approval_status == 1) {
+    if(is_evaluated) {
       status = __('Evaluated')
-    } else if(this.props.goal_approval.collaborator.status_type !== null ) {
+    } else if(is_wating) {
       status = __('Waiting')
       is_incomplete = true
     } else {
@@ -25,15 +29,13 @@ export class CoacheeCard extends React.Component {
     return (
       <li className="goals-approval-list-item">
           <div className={`goals-approval-list-item ${ is_incomplete ? "is-incomplete is-waiting" : "is-complete" }`}>
-              <Link className="goals-approval-list-item-link" to={ `/goals/approval/detail/${this.props.goal_approval.id}` }>
-                  <img className="goals-approval-list-item-image" src={ this.props.goal_approval.collaborator.user.photo_file_name } alt="" width="32" height="32" />
-
+              <Link className="goals-approval-list-item-link" to={ `/goals/approval/detail/${goal_approval.id}` }>
+                  <img className="goals-approval-list-item-image" src={ goal_approval.collaborator.user.photo_file_name } alt="" width="32" height="32" />
                   <div className="goals-approval-list-item-info">
-                      <p className="goals-approval-list-item-info-user-name">{ this.props.goal_approval.collaborator.user.name }</p>
-                      <p className="goals-approval-list-item-info-goal-name">{ this.props.goal_approval.name }</p>
+                      <p className="goals-approval-list-item-info-user-name">{ goal_approval.collaborator.user.name }</p>
+                      <p className="goals-approval-list-item-info-goal-name">{ goal_approval.name }</p>
                       <p className="goals-approval-list-item-info-goal-attr">{ role }・<span className="mod-status">{ status }</span></p>
                   </div>
-
                   <p className="goals-approval-list-item-detail">
                     <i className="fa fa-check" ariaHidden="true"></i>
                   </p>
