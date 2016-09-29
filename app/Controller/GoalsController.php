1418b987a24ffb1a49b1b9c34e15bd28bcd093df
Shohei Saeki <s.saeki71@gmail.com>
2016-09-29 15:52:15 +0900

    コラボ編集がコーチに通知されないbugを修正 #5043

--- app/Controller/GoalsController.php
+++ app/Controller/GoalsController.php
@@ -417,6 +417,8 @@ class GoalsController extends AppController
             return $this->redirect($this->referer());
         }
 
+
+        $collaborator = $this->request->data['Collaborator'];
         $collaborator_id = $collaborator_id ? $collaborator_id : $this->Goal->Collaborator->getLastInsertID();
 
         //success case.
