74552c00feddafe289e6e1830d2dc6928eadcbc5
Shohei Saeki <s.saeki71@gmail.com>
2016-10-06 15:20:54 +0900

    fix value reference error #5170

--- app/webroot/js/react/common/components/approval/CoacheeFooter.js
+++ app/webroot/js/react/common/components/approval/CoacheeFooter.js
@@ -13,36 +13,39 @@ export class CoacheeFooter extends React.Component {
    * TODO: Edit Role機能実装後この処理は行わないので削除する
    */
   componentDidMount() {
-    if(!this.props.collaborator.is_leader) {
+    const ApprovalStatus = Collaborator.ApprovalStatus
+
+    if(status == ApprovalStatus.DONE && !this.props.collaborator.is_leader) {
       ReactDOM.findDOMNode(this.refs.collabo_link).setAttribute("data-toggle", "modal")
       ReactDOM.findDOMNode(this.refs.collabo_link).setAttribute("data-target", `#ModalCollabo_${this.props.goal_id}`)
     }
   }
 
-  toNextButton() {
-    const status = this.props.collaborator.approval_status
-    const ApprovalStatus = Collaborator.ApprovalStatus
-
-    // Withdraw
-    if(status == ApprovalStatus.NEW || status == ApprovalStatus.REAPPLICATION) {
-      return <a className="btn goals-approval-btn-fullsize-active"
-                onClick={ this.props.handleClickWithdraw }>{ __("Withdraw") }</a>
-    }
-    // Edit goal
-    if(this.props.collaborator.is_leader) {
-      return <a className="btn goals-approval-btn-fullsize-active"
-                onClick={ () => { document.location.href = `/goals/${this.props.goal_id}/edit` }}>{ __('Edit Goal') }</a>
-    }
-    // Edit role
-    return <a href={`/goals/ajax_get_collabo_change_modal/goal_id:${this.props.goal_id}`}
-              className="btn goals-approval-btn-fullsize-active modal-ajax-get-collabo"
-              ref="collabo_link">{ __('Edit Role') }</a>
-  }
-
   render() {
+    const nextPageButton = (() => {
+      const status = this.props.collaborator.approval_status
+      const ApprovalStatus = Collaborator.ApprovalStatus
+
+      // Withdraw
+      if(status == ApprovalStatus.NEW || status == ApprovalStatus.REAPPLICATION) {
+        return <a className="btn goals-approval-btn-fullsize-active"
+                  onClick={ this.props.handleClickWithdraw }>{ __("Withdraw") }</a>
+      }
+      // Edit goal
+      if(this.props.collaborator.is_leader) {
+        return <a>aaa</a>
+        return <a className="btn goals-approval-btn-fullsize-active"
+                  onClick={ () => { document.location.href = `/goals/${this.props.goal_id}/edit` }}>{ __('Edit Goal') }</a>
+      }
+      // Edit role
+      return <a href={`/goals/ajax_get_collabo_change_modal/goal_id:${this.props.goal_id}`}
+                className="btn goals-approval-btn-fullsize-active modal-ajax-get-collabo"
+                ref="collabo_link">{ __('Edit Role') }</a>
+    })()
+
     return (
       <div className="goals-approval-detail-choice">
-          { this.toNextButton() }
+          { nextPageButton }
           <Link to="/goals/approval/list" className="btn goals-approval-btn-cancel">{ __("Back") }</Link>
       </div>
     )
