464d18e2333e81ba6cfe3faa4bdd0dba79c7e169
Daiki Hirakata <hirakatad@isao.co.jp>
2016-10-25 14:39:04 +0900

    :pencil2: comment for Experiment Model. GL-5241

--- app/Model/Experiment.php
+++ app/Model/Experiment.php
@@ -3,6 +3,11 @@ App::uses('AppModel', 'Model');
 
 /**
  * Experiment Model
+ * 実験を管理するテーブル
+ * nameは自由に追加可能。
+ * 例：フィードの通知設定を非表示にする実験(以下が対象チーム数分存在する)
+ *   name: 'DisappearNotificationSettingsForFeed'
+ *   team_id: 対象のチームID
  */
 class Experiment extends AppModel
 {
@@ -23,4 +28,20 @@ class Experiment extends AppModel
             ],
         ],
     ];
+
+    /**
+     * 定義済みの実験かどうかの判定
+     * @param $name
+     *
+     * @return bool
+     */
+    function isDefined($name)
+    {
+        $options = [
+            'name'    => $name,
+            'team_id' => $this->current_team_id
+        ];
+        $ret = $this->find('first', $options);
+        return (bool)!empty($ret);
+    }
 }
