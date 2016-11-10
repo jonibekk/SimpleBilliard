a269417d05f5b856b2dc19b9db67cb11e9710a28
Daiki Hirakata <hirakatad@isao.co.jp>
2016-11-10 12:45:25 +0900

    :bug: fix displaying goal buttons. GL-5342

--- app/View/Helper/PostHelper.php
+++ app/View/Helper/PostHelper.php
@@ -1,15 +1,20 @@
 <?php
+App::uses('AppHelper', 'View/Helper');
+App::uses('SessionHelper', 'View/Helper');
+
 /**
  * Created by PhpStorm.
  * User: saekis
  * Date: 16/03/16
  * Time: 18:08
+ *
+ * @property SessionHelper $Session
  */
-
-App::uses('AppHelper', 'View/Helper');
-
 class PostHelper extends AppHelper
 {
+    var $helpers = [
+        'Session'
+    ];
 
     /**
      * @param $json_site_info
@@ -28,6 +33,47 @@ class PostHelper extends AppHelper
         }
 
         return '';
+    }
+
+    /**
+     * @param array $post
+     * @param array $current_term
+     *
+     * @return bool
+     */
+    function isDisplayableGoalButtons(array $post, array $current_term)
+    {
+        if (!Hash::get($post, 'Goal.id')) {
+            return false;
+        }
+        //KR達成、ゴール作成以外はボタン表示しない
+        if (!in_array($post['Post']['type'], [Post::TYPE_KR_COMPLETE, Post::TYPE_CREATE_GOAL])) {
+            return false;
+        }
+        //本人がゴールリーダの場合はボタン表示しない
+        if ($post['Goal']['user_id'] == $this->Session->read('Auth.User.id')) {
+            return false;
+        }
+        //本人が投稿主の場合はボタン表示しない
+        if ($post['Post']['user_id'] == $this->Session->read('Auth.User.id')) {
+            return false;
+        }
+        //ゴール期限が今期より前の場合はボタン表示しない
+        if ($post['Goal']['end_date'] <= $current_term['start_date']) {
+            return false;
+        }
+        //完了済みのゴールはボタン表示しない
+        if (!is_null($post['Goal']['completed'])) {
+            return false;
+        }
+        //KR達成の場合、KRの期限が過去の場合はボタン表示しない
+        if ($post['Post']['type'] == Post::TYPE_KR_COMPLETE &&
+            REQUEST_TIMESTAMP > $post['KeyResult']['end_date']
+        ) {
+            return false;
+        }
+
+        return true;
     }
 
 }
