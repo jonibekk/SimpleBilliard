9175ea96a2aca0b762275b9d499394ddbfbfdd77
Shohei Saeki <s.saeki71@gmail.com>
2016-12-16 15:02:58 +0900

    バリデーションにおけるログ出力を変更 GL-5380

--- app/Service/GoalMemberService.php
+++ app/Service/GoalMemberService.php
@@ -184,19 +184,19 @@ class GoalMemberService extends AppService
         // パラメータ存在チェック
         $goalId = Hash::get($formData, 'Goal.id');
         if (empty($goalId) || empty($changeType)) {
-            return __("Some error occurred. Please try again from the start.");
+            return __("Invalid value");
         }
 
         // ゴールが存在するか
         $goal = $Goal->findById($goalId);
         if (empty($goal)) {
-            return __("Some error occurred. Please try again from the start.");
+            return __("The goal doesn't exist.");
         }
 
         // 今期以降のゴールか
         $isAfterCurrentGoal = $GoalService->isGoalAfterCurrentTerm($goalId);
         if(!$isAfterCurrentGoal) {
-            return __("Some error occurred. Please try again from the start.");
+            return __("You can't change leader in the goal before current term.");
         }
 
         // 評価開始前か
@@ -205,7 +205,8 @@ class GoalMemberService extends AppService
             $currentTermId = $EvaluateTerm->getCurrentTermId();
             $isStartedEvaluation = $EvaluateTerm->isStartedEvaluation($currentTermId);
             if ($isStartedEvaluation) {
-                return __("Some error occurred. Please try again from the start.");
+                $this->log(sprintf("[%s]%s", __METHOD__, sprintf("Failed to change leader being evaluating. goalId:%s", $goalId)));
+                return __("You cant't change leader in the goal during the evaluation period.");
             }
         }
 
@@ -213,26 +214,30 @@ class GoalMemberService extends AppService
         if ($changeType === self::CHANGE_LEADER_FROM_GOAL_MEMBER) {
             // 自分がゴールメンバーかどうか
             if (!$GoalMember->isCollaborated($goalId)) {
-                return __("Some error occurred. Please try again from the start.");
+                $this->log(sprintf("[%s]%s", __METHOD__, sprintf("Failed to change leader not being goal member. goalId:%s, userId:%s" , $goalId, $GoalMember->my_uid)));
+                return __("You don't have a permission to edit this goal.");
             }
 
             // アクティブなリーダーが存在する場合は、ゴールメンバーである自分にはリーダー変更権限がない
             $goalLeader = $GoalMember->getActiveLeader($goalId);
             if ($goalLeader) {
-                return __("Some error occurred. Please try again from the start.");
+                $this->log(sprintf("[%s]%s", __METHOD__, sprintf("Failed to change leader existing leader. goalId:%s, userId:%s" , $goalId, $GoalMember->my_uid)));
+                return __("You don't have a permission to edit this goal.");
             }
         // 自分がリーダーのケース
         } else {
             // 自分がリーダーかどうか
             $loginUserIsLeader = $GoalMember->isLeader($goalId, $GoalMember->my_uid);
             if (!$loginUserIsLeader) {
-                return __("Some error occurred. Please try again from the start.");
+                $this->log(sprintf("[%s]%s", __METHOD__, sprintf("Failed to change leader not being leader. goalId:%s, userId:%s" , $goalId, $GoalMember->my_uid)));
+                return __("You don't have a permission to edit this goal.");
             }
 
             // リーダーを変更して自分がコラボする場合
             if ($changeType === self::CHANGE_LEADER_WITH_COLLABORATION) {
                 $GoalMember->set($formData);
                 if (!$GoalMember->validates()) {
+                    $this->log(sprintf("[%s]%s", __METHOD__, sprintf("Failed to change leader not being able to collabo. data:%s" , var_export($formData, true))));
                     return __("Invalid value");
                 }
             }
@@ -241,6 +246,7 @@ class GoalMemberService extends AppService
         // 変更後のリーダーがアクティブなゴールメンバーかどうか
         $newLeaderId = Hash::get($formData, 'NewLeader.id');
         if (!$GoalMember->isActiveGoalMember($newLeaderId, $goalId)) {
+            $this->log(sprintf("Failed to change leader not being active member. goalId:%s, newLeaderId:%s" , $goalId, $newLeaderId));
             return __("Some error occurred. Please try again from the start.");
         }
 
