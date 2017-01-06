08b32b7877e310b845e057999f162001cb8b6367
Shohei Saeki <s.saeki71@gmail.com>
2017-01-06 18:20:07 +0900

    create dashboard getting api mock

--- app/Controller/Api/V1/GoalsController.php
+++ app/Controller/Api/V1/GoalsController.php
@@ -548,4 +548,24 @@ class GoalsController extends ApiController
 
         return $this->_getResponseSuccess();
     }
+
+    /**
+     * トップページ右カラムの初期表示データ取得API
+     *
+     * @return CakeResponse
+     */
+    public function get_dashboard()
+    {
+        // TODO: これはモックです。API実装の際に上書きしましょう。
+        $res = [
+            'progress_logs' => [
+                ['sweet_spot_top', 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                ['data', 0, 10, 10, 20, 30, 40, 50, 50, 55, 60, 65],
+                ['sweet_spot_bottom', 0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60],
+            ],
+            'kr_list'       => []
+        ];
+        return $this->_getResponseSuccess($res);
+    }
+
 }
