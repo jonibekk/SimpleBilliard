19ec7f6b74c83bc395583d1d864489bd03a2cb05
Daiki Hirakata <daiki@bigplants.net>
2017-02-14 23:25:52 +0900

    :white_check_mark: add test cases for Util to respond to FB. GL-5572

--- app/Test/Case/Lib/Util/AppUtilTest.php
+++ app/Test/Case/Lib/Util/AppUtilTest.php
@@ -55,6 +55,7 @@ class AppUtilTest extends GoalousTestCase
     function test_diffHourFloorByMin()
     {
         $baseTime = strtotime('00:00:00');
+        //30min
         $this->assertEquals(0, AppUtil::diffHourFloorByMinute(strtotime('00:00:00'), $baseTime));
         $this->assertEquals(0, AppUtil::diffHourFloorByMinute(strtotime('00:29:59'), $baseTime));
         $this->assertEquals(0.5, AppUtil::diffHourFloorByMinute(strtotime('00:30:00'), $baseTime));
@@ -64,6 +65,15 @@ class AppUtilTest extends GoalousTestCase
         $this->assertEquals(0.5, AppUtil::diffHourFloorByMinute(strtotime('00:59:00'), strtotime('00:29:00')));
         $this->assertEquals(0.5, AppUtil::diffHourFloorByMinute(strtotime('01:01:00'), strtotime('00:31:00')));
         $this->assertEquals(0, AppUtil::diffHourFloorByMinute(strtotime('01:30:00'), strtotime('01:01:00')));
+        //15min
+        $this->assertEquals(0, AppUtil::diffHourFloorByMinute(strtotime('00:14:59'), $baseTime, 15));
+        $this->assertEquals(0.25, AppUtil::diffHourFloorByMinute(strtotime('00:15:00'), $baseTime, 15));
+        $this->assertEquals(1.5, AppUtil::diffHourFloorByMinute(strtotime('01:30:00'), $baseTime, 15));
+        $this->assertEquals(23.75, AppUtil::diffHourFloorByMinute(strtotime('23:59:59'), $baseTime, 15));
+        $this->assertEquals(0.25, AppUtil::diffHourFloorByMinute(strtotime('00:58:00'), strtotime('00:29:00'), 15));
+        $this->assertEquals(0.75, AppUtil::diffHourFloorByMinute(strtotime('01:16:00'), strtotime('00:31:00'), 15));
+        $this->assertEquals(0.25, AppUtil::diffHourFloorByMinute(strtotime('01:30:00'), strtotime('01:01:00'), 15));
+
     }
 
 }
