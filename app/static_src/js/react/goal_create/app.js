e7436fdac750e2a554d6414e8031c635e514ee4d
endam <yoshidam2@isao.co.jp>
2017-02-15 10:35:01 +0900

    :bug: revert debug code GL-5318

--- app/static_src/js/react/goal_create/app.js
+++ app/static_src/js/react/goal_create/app.js
@@ -3,13 +3,6 @@ import React from 'react'
 /* eslint-enable no-unused-vars */
 import ReactDOM from 'react-dom'
 import Routes, { unlisten } from './config/routes'
-import { AppContainer } from 'react-hot-loader';
-// ReactDOM.render(
-//   <AppContainer>
-//     <Routes />
-//   </AppContainer>
-//   , document.getElementById("goal-create-app"));
-//
 
 ReactDOM.render(<Routes />, document.getElementById("goal-create-app"));
 
