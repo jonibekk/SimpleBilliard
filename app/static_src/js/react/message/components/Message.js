98077d4fa14a05f59a5b66e981b6d4f5c0637523
Shohei Saeki <s.saeki71@gmail.com>
2017-03-08 17:06:48 +0900

    upper lower case probrem GL-5646

--- app/static_src/js/react/message/components/Message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class Message extends React.Component {
-  render() {
-    return (
-      <div>Message一覧</div>
-    )
-  }
-}
--- app/static_src/js/react/message/components/topic.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class Topic extends React.Component {
-  render() {
-    return (
-      <div>Topic一覧</div>
-    )
-  }
-}
