f8324e0dee7ff43f6e0b0ff12eb291339a8dc863
Shohei Saeki <s.saeki71@gmail.com>
2017-03-08 16:47:06 +0900

    rename component file name GL-5546

---
+++ app/static_src/js/react/message/components/Message.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Message extends React.Component {
+  render() {
+    return (
+      <div>message詳細</div>
+    )
+  }
+}
---
+++ app/static_src/js/react/message/components/Topic.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Topic extends React.Component {
+  render() {
+    return (
+      <div>Topic一覧</div>
+    )
+  }
+}
