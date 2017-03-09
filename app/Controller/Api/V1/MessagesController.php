b36abc7c330e17bc5ebe5cd37baff36ce1a110b9
Daiki Hirakata <daiki@bigplants.net>
2017-03-09 17:29:12 +0900

    :heavy_plus_sign: add API mocks. GL-5645

--- app/Controller/Api/V1/MessagesController.php
+++ app/Controller/Api/V1/MessagesController.php
@@ -6,5 +6,36 @@ App::uses('ApiController', 'Controller/Api');
  */
 class MessagesController extends ApiController
 {
+    /**
+     * Send Like message
+     * url: POST /api/v1/messages
+     *
+     * @data integer $topic_id required
+     * @data string $body optional
+     * @data array $file_ids optional
+     * @return CakeResponse
+     */
+    function post()
+    {
+        $topicId = $this->request->data('topic_id');
+        $body = $this->request->data('body');
+        $fileIds = $this->request->data('file_ids');
+        $dataMock = ['message_id' => 1234];
+        return $this->_getResponseSuccessSimple($dataMock);
+    }
+
+    /**
+     * Send Like message
+     * url: POST /api/v1/messages/like
+     *
+     * @data integer $topic_id required
+     * @return CakeResponse
+     */
+    function post_like()
+    {
+        $topicId = $this->request->data('topic_id');
+        $dataMock = ['message_id' => 1234];
+        return $this->_getResponseSuccessSimple($dataMock);
+    }
 
 }
