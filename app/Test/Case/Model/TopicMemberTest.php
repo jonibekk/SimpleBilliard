e45b099e83ed074ed9d011fa983ca1482e30fbb9
Daiki Hirakata <daiki@bigplants.net>
2017-03-16 07:20:35 +0900

    :white_check_mark: add test cases in TopicMemberTest. GL-5681

--- app/Test/Case/Model/TopicMemberTest.php
+++ app/Test/Case/Model/TopicMemberTest.php
@@ -60,6 +60,27 @@ class TopicMemberTest extends GoalousTestCase
     function test_countReadMember()
     {
         $this->setDefaultTeamIdAndUid();
+        $topicId = $this->saveTopic([1, 2]);
+
+        // nobody has last_read_message_id, so `readCount` is expected 0
+        $actual = $this->TopicMember->countReadMember($topicId, 1);
+        $this->assertEquals(0, $actual);
+
+        $this->TopicMember->updateAll(
+            ['TopicMember.last_read_message_id' => 1],
+            ['user_id' => 1]
+        );
+        // 1 member has last_read_message_id, so `readCount` is expected 1
+        $actual = $this->TopicMember->countReadMember($topicId, 1);
+        $this->assertEquals(1, $actual);
+
+        $this->TopicMember->updateAll(
+            ['TopicMember.last_read_message_id' => 1],
+            ['user_id' => 2]
+        );
+        // 2 members have last_read_message_id, so `readCount` is expected 2
+        $actual = $this->TopicMember->countReadMember($topicId, 1);
+        $this->assertEquals(2, $actual);
     }
 
     function test_findMembers()
