f68e439e4575e1fdb517c5a9b89e3e8fca4dc030
Shohei Saeki <s.saeki71@gmail.com>
2017-03-17 16:50:14 +0900

    markup search box and middle area GL-5687

--- app/static_src/js/react/message/components/Index.js
+++ app/static_src/js/react/message/components/Index.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router'
 
 export default class Index extends React.Component {
   render() {
     return (
-      <div>
-        <div>Index</div>
-        <Link to="/topics/1/detail">Detailへ</Link>
+      <div className="panel panel-default topic">
+        <div className="topic-header">
+          <div className="searchBox">
+            <div className="searchBox-icon">
+              <i className="fa fa-search"></i>
+            </div>
+            <input className="searchBox-input"
+                   placeholder={__("Search topic")} />
+          </div>
+          <div className="topic-header-middle">
+            <div className="topic-header-middle-label">
+              {__("TOPICS")}
+            </div>
+            <a href="" className="topic-header-middle-add">
+              <i className="fa fa-plus-circle"></i> {__("New Message")}
+            </a>
+          </div>
+        </div>
       </div>
     )
   }
