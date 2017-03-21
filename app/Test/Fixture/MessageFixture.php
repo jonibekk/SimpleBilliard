67f6bdae6282893e40601a6523b99a4c30237b09
Daiki Hirakata <hirakatad@isao.co.jp>
2017-03-21 13:22:26 +0900

    :art: reformat. GL-5694

--- app/Test/Fixture/MessageFixture.php
+++ app/Test/Fixture/MessageFixture.php
@@ -13,78 +13,98 @@ class MessageFixture extends CakeTestFixtureEx
      * @var array
      */
     public $fields = [
-        'id'                  => array(
+        'id'                  => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
             'unsigned' => true,
             'key'      => 'primary',
             'comment'  => 'ID'
-        ),
-        'topic_id'            => array(
+        ],
+        'topic_id'            => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
             'unsigned' => true,
             'comment'  => 'topic ID(belongsTo Topic Model)'
-        ),
-        'sender_user_id'      => array(
+        ],
+        'sender_user_id'      => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
             'unsigned' => true,
             'key'      => 'index',
             'comment'  => 'UserID as Sender(belongsTo User Model)'
-        ),
-        'team_id'             => array(
+        ],
+        'team_id'             => [
             'type'     => 'biginteger',
             'null'     => false,
             'default'  => null,
             'unsigned' => true,
             'key'      => 'index',
             'comment'  => 'TeamID(belongsTo Team Model)'
-        ),
-        'body'                => array(
+        ],
+        'body'                => [
             'type'    => 'text',
             'null'    => true,
             'default' => null,
             'collate' => 'utf8mb4_general_ci',
             'comment' => 'Body of message',
             'charset' => 'utf8mb4'
-        ),
-        'type'                => array(
+        ],
+        'type'                => [
             'type'     => 'integer',
             'null'     => false,
             'default'  => '1',
             'length'   => 3,
             'unsigned' => true,
             'comment'  => 'Message Type(1:Nomal,2:Add member,3:Remove member,4:Change topic name)'
-        ),
-        'target_user_ids'     => array(
+        ],
+        'target_user_ids'     => [
             'type'    => 'text',
             'null'    => true,
             'default' => null,
             'collate' => 'utf8mb4_general_ci',
             'comment' => 'comma spalated list for target users(e.g. 1,2,3) if add or remove members.',
             'charset' => 'utf8mb4'
-        ),
-        'attached_file_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => true),
-        'del_flg'             => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-        'deleted'             => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true),
-        'created'             => array('type'     => 'integer',
-                                       'null'     => false,
-                                       'default'  => null,
-                                       'unsigned' => true,
-                                       'key'      => 'primary'
-        ),
-        'modified'            => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true),
-        'indexes'             => array(
-            'PRIMARY' => array('column' => array('id', 'created'), 'unique' => 1),
-            'team_id' => array('column' => 'team_id', 'unique' => 0),
-            'created' => array('column' => 'created', 'unique' => 0),
-            'user_id' => array('column' => 'sender_user_id', 'unique' => 0)
-        ),
-        'tableParameters'     => array('charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB')
+        ],
+        'attached_file_count' => [
+            'type'     => 'integer',
+            'null'     => false,
+            'default'  => '0',
+            'unsigned' => true
+        ],
+        'del_flg'             => [
+            'type'    => 'boolean',
+            'null'    => false,
+            'default' => '0'
+        ],
+        'deleted'             => [
+            'type'     => 'integer',
+            'null'     => true,
+            'default'  => null,
+            'unsigned' => true
+        ],
+        'created'             => [
+            'type'     => 'integer',
+            'null'     => false,
+            'default'  => null,
+            'unsigned' => true,
+            'key'      => 'primary'
+        ],
+        'modified'            => [
+            'type'     => 'integer',
+            'null'     => true,
+            'default'  => null,
+            'unsigned' => true
+        ],
+        'indexes'             => [
+            'PRIMARY' => ['column' => ['id', 'created'], 'unique' => 1],
+            'team_id' => ['column' => 'team_id', 'unique' => 0],
+            'created' => ['column' => 'created', 'unique' => 0],
+            'user_id' => ['column' => 'sender_user_id', 'unique' => 0]
+        ],
+        'tableParameters'     => ['charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB']
     ];
 
     /**
