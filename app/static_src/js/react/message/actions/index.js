ac5c1f72edeb48362c9960aca2ba574cadbc050f
Shohei Saeki <s.saeki71@gmail.com>
2017-03-23 14:23:23 +0900

    implement get default topic list js GL-5688

--- app/static_src/js/react/message/actions/index.js
+++ app/static_src/js/react/message/actions/index.js
@@ -1,5 +1,53 @@
-import * as ActionTypes from "~/message/constants/ActionTypes";
+import * as types from "~/message/constants/ActionTypes";
+import { get } from "~/util/api";
 
-export function hoge() {
+export function fetchInitData() {
+  return (dispatch) => {
+    dispatch({
+      type: types.FETCHING_TOPICS
+    })
+    return get('/api/v1/topics/search')
+      .then((response) => {
+        // const topics = response.data.data
+        const topics = response.data.data
+        const next_url = response.data.paging.next
+        dispatch({
+          type: types.FETCH_TOPICS,
+          data: {
+            topics,
+            next_url
+          }
+        })
+      })
+      .catch((response) => {
+        /* eslint-disable no-console */
+        console.log(response)
+        /* eslint-enable no-console */
+      })
+  }
+}
 
+export function fetchMoreTopics(url) {
+  return (dispatch) => {
+    dispatch({
+      type: types.FETCHING_TOPICS
+    })
+    return get(url)
+      .then((response) => {
+        const topics = response.data.data
+        const next_url = response.data.paging.next
+        dispatch({
+          type: types.FETCH_MORE_TOPICS,
+          data: {
+            topics,
+            next_url
+          }
+        })
+      })
+      .catch((response) => {
+        /* eslint-disable no-console */
+        console.log(response)
+        /* eslint-enable no-console */
+      })
+  }
 }
