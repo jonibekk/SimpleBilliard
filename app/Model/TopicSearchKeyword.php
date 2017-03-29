997840904e6036f425e14417c1bfc0ebdb4cf626
Shohei Saeki <s.saeki71@gmail.com>
2017-03-29 16:49:58 +0900

    implement update search keywords table GL-5705

--- app/Model/TopicSearchKeyword.php
+++ app/Model/TopicSearchKeyword.php
@@ -79,19 +79,20 @@ SQL;
     }
 
     /**
-     * update by user id
+     * update keyword records for search by user id
      * - update all topics related by user
-     * FIXME: DO NOT move now.
-     *        error msg -> "Column not found: 1054 Unknown column 'tsk.topic_id' in 'on clause'"
+     * - in super exceptional, writing sql directory
+     *  - For Reducing query cost
      *
-     * @param  [type] $userId [description]
-     * @return bool           [description]
+     * @param  int $userId
+     * @return bool
      */
     function updateByUserId($userId): bool
     {
         $query = <<<SQL
 UPDATE topic_search_keywords tsk,
 (SELECT
+    t.id as topic_id,
     CONCAT(
         '\n',
         GROUP_CONCAT(DISTINCT(u.last_name) SEPARATOR '\n'),
@@ -109,15 +110,56 @@ UPDATE topic_search_keywords tsk,
         u.id = tm.user_id
     LEFT JOIN local_names ln ON
         ln.user_id = u.id
+    WHERE t.id in (
+      SELECT tm2.topic_id from topic_members as tm2
+      WHERE tm2.user_id = $userId
+    )
+    GROUP BY t.id
 ) tsk2
-INNER JOIN topics t ON
-    tsk.topic_id = t.id
-INNER JOIN topic_members tm ON
-    t.id = tm.topic_id
-INNER JOIN users u ON
-    tm.user_id = u.id
 SET tsk.keywords = tsk2.keywords
-WHERE u.id = $userId
+WHERE tsk.topic_id = tsk2.topic_id
+SQL;
+        $this->query($query);
+        return true;
+    }
+
+    /**
+     * update keyword records for search by topic id
+     * - update all topics related by user
+     * - in super exceptional, writing sql directory
+     *  - For Reducing query cost
+     *
+     * @param  int $userId
+     * @return bool
+     */
+    function updateByTopicId(int $userId): bool
+    {
+        $query = <<<SQL
+UPDATE topic_search_keywords tsk,
+(SELECT
+    t.id as topic_id,
+    CONCAT(
+        '\n',
+        GROUP_CONCAT(DISTINCT(u.last_name) SEPARATOR '\n'),
+        '\n',
+        GROUP_CONCAT(DISTINCT(u.first_name) SEPARATOR '\n'),
+        '\n',
+        GROUP_CONCAT(DISTINCT(ln.last_name) SEPARATOR '\n'),
+        '\n',
+        GROUP_CONCAT(DISTINCT(ln.first_name) SEPARATOR '\n')
+    ) as keywords
+    FROM topics t
+    INNER JOIN topic_members tm ON
+        t.id = tm.topic_id
+    INNER JOIN users u ON
+        u.id = tm.user_id
+    LEFT JOIN local_names ln ON
+        ln.user_id = u.id
+    WHERE t.id = $topicId
+    GROUP BY t.id
+) tsk2
+SET tsk.keywords = tsk2.keywords
+WHERE tsk.topic_id = tsk2.topic_id
 SQL;
         $this->query($query);
         return true;
