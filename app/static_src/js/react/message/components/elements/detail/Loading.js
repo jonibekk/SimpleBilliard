ac8ca57b8a0cfbaa0153ca80cf7a5dfaf20e36f7
endam <yoshidam2@isao.co.jp>
2017-04-09 14:11:23 +0900

    :bug: Fixed No103 bug

--- app/static_src/js/react/message/components/elements/detail/Loading.js
+++ app/static_src/js/react/message/components/elements/detail/Loading.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+// TODO:componentize ui parts
 export default class Loading extends React.Component {
   render() {
     return (
       <div className="text-align_c">
-        <img src="/img/ajax-loader.gif" width="16" height="16"/>
+        <img src="/img/lightbox/loading.gif" width="16" height="16"/>
       </div>
     )
   }
