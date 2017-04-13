7deda8f4166bee86006744f62f49f9c5219f5337
Daiki Hirakata <daiki@bigplants.net>
2017-04-13 17:32:00 +0900

    :heavy_plus_sign: add column for storing last message read datetime. GL-5725

---
+++ app/Config/Migration/1492072210_add_last_message_read_datetime_0413.php
@@ -0,0 +1,53 @@
+<?php
+class AddLastMessageReadDatetime0413 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_last_message_read_datetime_0413';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'topic_members' => array(
+					'last_message_read_datetime' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index', 'comment' => 'It\'s update when read message.', 'after' => 'last_message_sent'),
+					'indexes' => array(
+						'last_message_read_datetime' => array('column' => 'last_message_read_datetime', 'unique' => 0),
+					),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'topic_members' => array('last_message_read_datetime', 'indexes' => array('last_message_read_datetime')),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
