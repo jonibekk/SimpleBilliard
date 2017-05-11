2c1be957028e86524a256d7545df9e15661d0dcb
Joshua McCarthy <joshua.f.mccarthy@gmail.com>
2017-05-11 22:20:07 +0900

    added comments

--- app/static_src/js/view/settings.js
+++ app/static_src/js/view/settings.js
@@ -1,23 +1,29 @@
+// Global Object for all Team Setting functions
 var teamSettings = {
+    //Some shorthands for important DOM elements
     button:document.getElementById("editTerm").getElementsByClassName("btn")[0],
     agree:document.getElementById("term_agreement"),
     editedTerm:document.getElementById("editTerm").getElementsByClassName("edited-term")[0],
     caret:document.getElementById("editTerm").getElementsByClassName("fa-caret-down")[0],
     formStart:document.getElementById("term_start"),
     formLength:document.getElementById("term_length"),
+    //DOM elements that displays term dates
     view:{
         currentEnd:document.getElementsByClassName("edited-term")[0].getElementsByClassName("this-end")[0],
         nextStart:document.getElementsByClassName("edited-term")[0].getElementsByClassName("next-start")[0],
         nextEnd:document.getElementsByClassName("edited-term")[0].getElementsByClassName("next-end")[0]
     },
+    //Two booleans to check when to enable submit button
     status:{
         agreement:false,
         changed:false
     },
+    //Store default dates so we know when to re-disable submit button
     defaults:{
         start:document.getElementById("term_start").value,
         length:document.getElementById("term_length").value
     },
+    //Check whether to enable/disable submit button
     testSubmit:function(){
         if(this.status.agreement && this.status.changed){
             this.button.removeAttribute("disabled");
@@ -25,6 +31,7 @@ var teamSettings = {
             this.button.setAttribute("disabled","disabled");
         }
     },
+    //Check whether start date or term have been changed back to default value
     testDefaults:function(start,length){
         if(this.defaults.start != start || this.defaults.length != length){
             this.status.changed=true;
@@ -40,12 +47,14 @@ var teamSettings = {
             },100);
         }
     },
+    //Create calendar objects 
     calendar:{
         currentStart:new Date(),
         currentEnd:new Date(),
         nextStart:new Date(),
         nextEnd:new Date()
     },
+    //Define array that matches month number with month name
     months:[
         "Jan",
         "Feb",
@@ -60,6 +69,11 @@ var teamSettings = {
         "Nov",
         "Dec"
     ],
+    /* Initialize the calender object
+        Grabs the starting month of current term
+        Grabs the term length
+        Defines all other dates based on these two variables
+    */
     calendarInit:function(){
         this.calendar.currentStart.setMonth(
             parseInt(document.getElementById("currentStart").getAttribute("data-date").substring(0,2))-1
@@ -69,12 +83,14 @@ var teamSettings = {
         );
         this.startUpdate();
     },
+    // Check that term data is updated
     termUpdate:function(){
         this.calendar.nextEnd.setMonth(
             this.calendar.nextStart.getMonth()+(parseInt(document.getElementById("term_length").value)-1)
         );
         this.viewUpdate();
     },
+    // Check that start date is updated
     startUpdate:function(){
         this.calendar.nextStart.setMonth(
             parseInt(document.getElementById("term_start").value.substring(0,2))-1
@@ -93,6 +109,7 @@ var teamSettings = {
         );
         this.termUpdate();
     },
+    // Update the view with the new date values
     viewUpdate:function(){
         if(this.view.currentEnd.getAttribute("data-date")!=(parseInt(this.calendar.currentEnd.getMonth())+1)+"_"+this.calendar.currentEnd.getFullYear()){
             this.view.currentEnd.classList.add("edited");
@@ -121,11 +138,18 @@ var teamSettings = {
     }
 };
 
+//Page load statements//
+
+//Disable submit button
 teamSettings.button.setAttribute("disabled","disabled");
-teamSettings.editedTerm.setAttribute("data-height",teamSettings.editedTerm.clientHeight);
+//Hide Edited term box for now
 teamSettings.editedTerm.classList.add("mod-hide");
 teamSettings.caret.classList.add("mod-hide");
+
+//Run calendar initializer
 teamSettings.calendarInit();
+
+//Define function when agreement checkbox is clicked
 teamSettings.agree.onclick = function(){
     if(teamSettings.agree.getAttribute("checked")=="checked"){
         teamSettings.agree.removeAttribute("checked","checked");
@@ -137,6 +161,8 @@ teamSettings.agree.onclick = function(){
         teamSettings.testSubmit();
     }
 };
+
+//Define functions when term start or term length is updated
 teamSettings.formStart.onchange = function(){
     teamSettings.testDefaults(teamSettings.formStart.value, teamSettings.formLength.value);
     teamSettings.startUpdate();
