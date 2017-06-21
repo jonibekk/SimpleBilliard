ef8a0c0a9bf6f65b4cdca0500a50fe915c1df0bd
Daiki Hirakata <hirakatad@isao.co.jp>
2017-06-21 16:15:59 +0900

    :heavy_plus_sign: add initial term data. GL-5885

---
+++ app/Config/Schema/local_test_data/TermData.php
@@ -0,0 +1,57 @@
+<?php
+
+class TermData
+{
+    public $name = 'Term';
+
+    public $records = [
+        [
+            'id'         => '1',
+            'team_id'    => '1',
+            'start_date' => "2017-01-01",
+            'end_date'   => "2017-12-31",
+        ],
+        [
+            'id'         => '2',
+            'team_id'    => '1',
+            'start_date' => "2018-01-01",
+            'end_date'   => "2018-12-31",
+        ],
+        [
+            'id'         => '3',
+            'team_id'    => '1',
+            'start_date' => "2019-01-01",
+            'end_date'   => "2019-12-31",
+        ],
+        [
+            'id'         => '4',
+            'team_id'    => '1',
+            'start_date' => "2020-01-01",
+            'end_date'   => "2020-12-31",
+        ],
+        [
+            'id'         => '5',
+            'team_id'    => '2',
+            'start_date' => "2017-01-01",
+            'end_date'   => "2017-12-31",
+        ],
+        [
+            'id'         => '6',
+            'team_id'    => '2',
+            'start_date' => "2018-01-01",
+            'end_date'   => "2018-12-31",
+        ],
+        [
+            'id'         => '7',
+            'team_id'    => '2',
+            'start_date' => "2019-01-01",
+            'end_date'   => "2019-12-31",
+        ],
+        [
+            'id'         => '8',
+            'team_id'    => '2',
+            'start_date' => "2020-01-01",
+            'end_date'   => "2020-12-31",
+        ],
+    ];
+}
