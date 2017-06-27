ce1ba9b8294f79ac2a5832169e10e0ef64b0b147
Shohei Saeki <s.saeki71@gmail.com>
2017-06-28 00:50:14 +0900

    delete unneeded lines

--- app/static_src/js/react/goal_search/app.js
+++ app/static_src/js/react/goal_search/app.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './config/routes'
-import KrColumn from '~/kr_column/components/KrColumn'
 
 ReactDOM.render(<Routes />, document.getElementById("GoalSearch"));
-ReactDOM.render(<KrColumn />, document.getElementById("kr-column"));
--- app/static_src/js/react/message/app.js
+++ app/static_src/js/react/message/app.js
@@ -3,12 +3,10 @@ import React from 'react'
 /* eslint-enable no-unused-vars */
 import ReactDOM from 'react-dom'
 import Routes, { unlisten } from './config/routes'
-import KrColumn from '~/kr_column/components/KrColumn'
 import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
 
 ReactDOM.render(<Routes />, document.getElementById("message-app"));
-ReactDOM.render(<KrColumn />, document.getElementById("kr-column"));
 
 // Stop to listen browzer history changing
 unlisten()
