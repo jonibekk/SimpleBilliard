7132d33666fb749c8dcc98267ad309b788e65c90
Daiki Hirakata <daiki@bigplants.net>
2017-07-20 23:06:54 +0900

    :heavy_plus_sign: add skeleton of sending alert shell. GL-6047

---
+++ app/Console/Command/SendAlertMailToAdminShell.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * # Batch processing for sending e-mail of expires alert.
+ * ## Target status
+ * - 0: free trial
+ * - 2: read only
+ * ## How to notify?
+ * - e-mail
+ * ## Execution timing
+ * - It's defined in the following.
+ *    app/Config/extra_defines.php
+ *   EXPIRE_ALERT_NOTIFY_BEFORE_DAYS
+ *    Expire alert will be send in specified days before expires. That days should be comma separated.
+ *      ex) EXPIRE_ALERT_NOTIFY_BEFORE_DAYS=10,5,3,2,1
+ *    ※ The shell will be executed at 10am(JPT)
+ * ## Target User
+ * - Team Admins
+ *   * If user is admin in multiple teams, it will send multiple e-mails.
+ * ## Usage
+ * Console/cake send_alert_mail_to_admin
+ *
+ * @property Team       $Team
+ * @property TeamMember $TeamMember
+ */
+class SendAlertMailToAdminShell extends AppShell
+{
+    public $uses = [
+        'Team',
+        'TeamMember'
+    ];
+
+    public function startup()
+    {
+        parent::startup();
+    }
+
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+
+        $options = [
+            'expires' => ['short' => 'e', 'help' => 'Y-m-d', 'required' => false,],
+        ];
+        $parser->addOptions($options);
+        return $parser;
+    }
+
+    public function main()
+    {
+
+        return;
+    }
+
+}
