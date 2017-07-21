183f7c356bde88c458d745dc0e253a88678f8eb1
Mauro Fujihara <fujiharam@isao.co.jp>
2017-07-21 17:54:51 +0900

    Change endpoint to /api/v1/payment/creditCard Only accept credit card payment Remove Payment.parameter path from the parameter

--- app/Controller/Api/V1/PaymentController.php
+++ app/Controller/Api/V1/PaymentController.php
@@ -11,79 +11,76 @@ class PaymentController extends ApiController
     /**
      * Create a new payment register
      *
+     * Endpoint: /api/v1/payment/creditCard
+     *
      * Parameters:
      *
-     * Payment.token            Stripe Credit Card Token
-     * Payment.type             Payment Type (0 = Invoice, 1 = Credit Card)
-     * Payment.amount_per_user  Amount Per user
-     * Payment.payer_name       Credit Card Name or Invoice Payer
-     * Payment.company_name     Company Name
-     * Payment.company_address  Company Address
-     * Payment.company_tel      Company Telephone number
-     * Payment.email            Payer email address
-     * Payment.payment_base_day Payment day every month
-     * Payment.currency         Currency (1 = JPY, 2 = USD)
+     * token            Stripe Credit Card Token
+     * amount_per_user  Amount Per user
+     * payer_name       Credit Card Name or Invoice Payer
+     * company_name     Company Name
+     * company_address  Company Address
+     * company_tel      Company Telephone number
+     * email            Payer email address
+     * payment_base_day Payment day every month
+     * currency         Currency (1 = JPY, 2 = USD)
      *
      * @return CakeResponse
      */
-    function post()
+    function post_creditCard()
     {
-        // Set teamId for validation
+        // Set teamId and payment type for validation
         $teamId = $this->current_team_id;
-        $this->request->data = Hash::insert($this->request->data, 'Payment.team_id', $teamId);
+        $requestData = Hash::insert($this->request->data, 'team_id', $teamId);
+        $requestData = Hash::insert($requestData, 'type', PaymentSetting::PAYMENT_TYPE_CREDIT_CARD);
 
         /** @var PaymentService $PaymentService */
         $PaymentService = ClassRegistry::init("PaymentService");
-        $validation = $PaymentService->validateCreate(Hash::get($this->request->data, 'Payment'));
+        $validation = $PaymentService->validateCreate($requestData);
 
         if ($validation !== true) {
             return $this->_getResponseValidationFail($validation);
         }
 
-        $paymentType = Hash::get($this->request->data, 'Payment.type');
         // Register Credit Card
-        if ($paymentType == PaymentSetting::PAYMENT_TYPE_CREDIT_CARD) {
-            /** @var ApiStripeService $ApiStripeService */
-            $ApiStripeService = ClassRegistry::init("ApiStripeService");
-            $token = Hash::get($this->request->data, 'Payment.token');
-            $email = Hash::get($this->request->data, 'Payment.email');
-            $companyName = Hash::get($this->request->data, 'Payment.company_name');
+        /** @var ApiStripeService $ApiStripeService */
+        $ApiStripeService = ClassRegistry::init("ApiStripeService");
+        $token = Hash::get($requestData, 'token');
+        $email = Hash::get($requestData, 'email');
+        // Set description as "Team ID: 2" to identify it on Stripe Dashboard
+        $description = "Team ID: $teamId";
 
-            // Register customer at Stripe
-            $stripeResponse = $ApiStripeService->registerCustomer($token, $email, $companyName);
-            if ($stripeResponse['error'] === true) {
-                return $this->_getResponseBadFail($stripeResponse['message']);
-            }
-
-            // Stripe customer id
-            $customerID = $stripeResponse['customer_id'];
-            if (empty($customerID)) {
-                // It never should happen
-                return $this->_getResponseBadFail(__("An error occurred while processing."));
-            }
-
-            // Check if the Payment if in the correct currency
-            $currency = Hash::get($this->request->data, 'Payment.currency');
-            if ($stripeResponse['card']['country'] == 'JP' && $currency != PaymentSetting::CURRENCY_JPY) {
-                // Delete customer from Stripe
-                $ApiStripeService->deleteCustomer($customerID);
-
-                // TODO: Add translation for message
-                return $this->_getResponseBadFail("Your Credit Card does not match your country settings");
-            }
-
-            // Register Payment on database
-            $userId = $this->Auth->user('id');
-            $res = ($PaymentService->createCreditCardPayment(Hash::get($this->request->data, 'Payment'), $customerID, $userId));
-            if (!$res) {
-                return $this->_getResponseBadFail(__("An error occurred while processing."));
-            }
-
-            // New Payment registered with sucess
-            return $this->_getResponseSuccess();
+        // Register customer at Stripe
+        $stripeResponse = $ApiStripeService->registerCustomer($token, $email, $description);
+        if ($stripeResponse['error'] === true) {
+            return $this->_getResponseBadFail($stripeResponse['message']);
         }
 
-        // TODO: Register Invoice
+        // Stripe customer id
+        $customerID = $stripeResponse['customer_id'];
+        if (empty($customerID)) {
+            // It never should happen
+            return $this->_getResponseBadFail(__("An error occurred while processing."));
+        }
+
+        // Check if the Payment if in the correct currency
+        $currency = Hash::get($requestData, 'currency');
+        if ($stripeResponse['card']['country'] == 'JP' && $currency != PaymentSetting::CURRENCY_JPY) {
+            // Delete customer from Stripe
+            $ApiStripeService->deleteCustomer($customerID);
+
+            // TODO: Add translation for message
+            return $this->_getResponseBadFail("Your Credit Card does not match your country settings");
+        }
+
+        // Register Payment on database
+        $userId = $this->Auth->user('id');
+        $res = ($PaymentService->createCreditCardPayment($requestData, $customerID, $userId));
+        if (!$res) {
+            return $this->_getResponseBadFail(__("An error occurred while processing."));
+        }
+
+        // New Payment registered with sucess
         return $this->_getResponseSuccess();
     }
 }
