79bdbd5627fba3ee94b8132395b8e90e42e49c57
Joshua McCarthy <mccarthyj@isao.co.jp>
2017-07-24 16:59:49 +0900

    in progress

--- app/Controller/PaymentController.php
+++ app/Controller/PaymentController.php
@@ -2,8 +2,13 @@
 # /app/Controller/PaymentController.php
 
 class PaymentController extends AppController {
-    public function choose() {
+    public function choosePayment() {
         $this->layout = LAYOUT_ONE_COLUMN;
         $this->render('/Payment/choose_payment_type');
+    }
+
+    public function enterCCInfo() {
+        $this->layout = LAYOUT_ONE_COLUMN;
+        $this->render('/Payment/enter_credit_card');
     }
 }
