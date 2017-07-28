fde18ed762ab1aca9dfefcea07e22c011e71ad75
masuichig <masuichig@isao.co.jp>
2017-07-28 14:22:05 +0900

    rename method name to better name GL-5962

--- app/Lib/Request/MobileAppVersion.php
+++ app/Lib/Request/MobileAppVersion.php
@@ -3,12 +3,24 @@
 class MobileAppVersion
 {
     /**
+     * return true if Goalous Mobile App version is supporting
      * @param string $versionAtLeast Goalous guaranteed mobile app version (最低動作保証バージョン)
      * @param string $versionCompare comparing version, usually, mobile app version
      * @return bool
      */
-    static function isGuaranteed(string $versionAtLeast, string $versionCompare): bool
+    static function isSupporting(string $versionAtLeast, string $versionCompare): bool
     {
         return version_compare($versionAtLeast, $versionCompare, '<=');
+    }
+
+    /**
+     * return true if Goalous Mobile App version is out of support
+     * @param string $versionAtLeast
+     * @param string $versionCompare
+     * @return bool
+     */
+    static function isExpired(string $versionAtLeast, string $versionCompare): bool
+    {
+        return !static::isSupporting($versionAtLeast, $versionCompare);
     }
 }
--- app/Test/Case/Lib/Request/MobileAppVersionTest.php
+++ app/Test/Case/Lib/Request/MobileAppVersionTest.php
@@ -4,20 +4,24 @@ App::uses('MobileAppVersion', 'Request');
 
 class MobileAppVersionTest extends GoalousTestCase
 {
-    public function test_versionGuaranteed()
+    public function test_versionSupporting()
     {
-        $this->assertTrue(MobileAppVersion::isGuaranteed('1.0.0', '1.0.0'));
-        $this->assertTrue(MobileAppVersion::isGuaranteed('10.0.0', '10.0.1'));
-        $this->assertTrue(MobileAppVersion::isGuaranteed('1.0.10', '1.0.11'));
-        $this->assertTrue(MobileAppVersion::isGuaranteed('1.20.', '1.20.1'));
-        $this->assertTrue(MobileAppVersion::isGuaranteed('1.0.0.0', '1.0.0.1'));
+        $this->assertTrue(MobileAppVersion::isSupporting('1.0.0', '1.0.0'));
+        $this->assertTrue(MobileAppVersion::isSupporting('10.0.0', '10.0.1'));
+        $this->assertTrue(MobileAppVersion::isSupporting('1.0.10', '1.0.11'));
+        $this->assertTrue(MobileAppVersion::isSupporting('1.20.', '1.20.1'));
+        $this->assertTrue(MobileAppVersion::isSupporting('1.0.0.0', '1.0.0.1'));
+
+        $this->assertFalse(MobileAppVersion::isExpired('1.0.0', '1.0.0'));
     }
 
-    public function test_versionNotGuaranteed()
+    public function test_versionNotSupporting()
     {
-        $this->assertFalse(MobileAppVersion::isGuaranteed('1', ''));
-        $this->assertFalse(MobileAppVersion::isGuaranteed('2', '1'));
-        $this->assertFalse(MobileAppVersion::isGuaranteed('2.0.0', '1.0.1'));
-        $this->assertFalse(MobileAppVersion::isGuaranteed('1.10.0', '1.9.0'));
+        $this->assertFalse(MobileAppVersion::isSupporting('1', ''));
+        $this->assertFalse(MobileAppVersion::isSupporting('2', '1'));
+        $this->assertFalse(MobileAppVersion::isSupporting('2.0.0', '1.0.1'));
+        $this->assertFalse(MobileAppVersion::isSupporting('1.10.0', '1.9.0'));
+
+        $this->assertTrue(MobileAppVersion::isExpired('2', '1'));
     }
 }
