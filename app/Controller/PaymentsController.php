92c659eef66d3a8692fe46c77d7a0d2a00701b75
Daiki Hirakata <daiki@bigplants.net>
2017-08-01 12:28:18 +0900

    :heavy_plus_sign: add ctp files. GL-6077

--- app/Controller/PaymentsController.php
+++ app/Controller/PaymentsController.php
@@ -1,22 +1,33 @@
 <?php
 # /app/Controller/PaymentsController.php
 
-class PaymentsController extends AppController {
-    public function apply() {
+class PaymentsController extends AppController
+{
+    public function index()
+    {
+        $this->layout = LAYOUT_ONE_COLUMN;
+    }
+
+    public function apply()
+    {
         $this->layout = LAYOUT_ONE_COLUMN;
         $this->render('choose_payment_type');
     }
 
-    public function enterCCInfo() {
+    public function enterCCInfo()
+    {
         $this->layout = LAYOUT_ONE_COLUMN;
         $this->render('credit_card_entry');
     }
 
-    public function enterCompanyInfo() {
+    public function enterCompanyInfo()
+    {
         $this->layout = LAYOUT_ONE_COLUMN;
         $this->render('company_info');
     }
-    public function thankyou() {
+
+    public function thankyou()
+    {
         $this->layout = LAYOUT_ONE_COLUMN;
         $this->render('thank_you');
     }
---
+++ app/View/Payments/cannot_use_service.ctp
@@ -0,0 +1,7 @@
+<?= $this->App->viewStartComment() ?>
+<section class="panel choose-payment">
+    <div class="panel-container">
+        <h3><?= __("You cannot use service on the team.") ?></h3>
+    </div>
+</section>
+<?= $this->App->ViewEndComment() ?>
