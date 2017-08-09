2a865459720f52a0d51ccf570cac21f2755d5bad
endam <yoshidam2@isao.co.jp>
2017-08-09 20:34:00 +0900

    :art: fix db migration  GL-6060

--- app/Config/Migration/1502278387_add_contact_person_columns_into_payment_setting.php
+++ app/Config/Migration/1502278387_add_contact_person_columns_into_payment_setting.php
@@ -25,6 +25,9 @@ class AddContactPersonColumnsIntoPaymentSetting extends CakeMigration {
 					'contact_person_email' => array('type' => 'string', 'null' => false, 'collate' => 'utf8mb4_general_ci', 'comment' => 'Contact person.email address', 'charset' => 'utf8mb4', 'after' => 'contact_person_tel'),
 				),
 			),
+			'drop_field' => array(
+				'payment_settings' => array('payer_name', 'email'),
+			),
 			'alter_field' => array(
 				'payment_settings' => array(
 					'company_tel' => array('type' => 'string', 'null' => false, 'length' => 20, 'collate' => 'utf8mb4_general_ci', 'comment' => 'Company tel number', 'charset' => 'utf8mb4'),
@@ -34,6 +37,12 @@ class AddContactPersonColumnsIntoPaymentSetting extends CakeMigration {
 		'down' => array(
 			'drop_field' => array(
 				'payment_settings' => array('contact_person_first_name', 'contact_person_first_name_kana', 'contact_person_last_name', 'contact_person_last_name_kana', 'contact_person_tel', 'contact_person_email'),
+			),
+			'create_field' => array(
+				'payment_settings' => array(
+					'payer_name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => 'Payer name', 'charset' => 'utf8mb4'),
+					'email' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => 'Payer email', 'charset' => 'utf8mb4'),
+				),
 			),
 			'alter_field' => array(
 				'payment_settings' => array(
