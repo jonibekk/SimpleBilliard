156fdbeef056c6da691882fad5a64a3191f6f2c1
endam <yoshidam2@isao.co.jp>
2017-08-28 17:30:32 +0900

    :art: implmenet to display terms of service dynamically GL-6225

--- app/Model/TermsOfService.php
+++ app/Model/TermsOfService.php
@@ -6,4 +6,21 @@ App::uses('AppModel', 'Model');
  */
 class TermsOfService extends AppModel
 {
+
+    /**
+     * Get current terms of service
+     * 
+     * @return array
+     */
+    function getCurrent(): array
+    {
+        $res = $this->find('first', [
+            'conditions' => [
+                'start_date <=' => date('Y-m-d')
+            ],
+            'order' => 'id DESC'
+        ]);
+
+        return Hash::get($res, 'TermsOfService') ?? [];
+    }
 }
