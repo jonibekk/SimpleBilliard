57b7a6967d00a757a648079e182b045dd7416cf5
Shohei Saeki <s.saeki71@gmail.com>
2017-09-04 18:24:02 +0900

    add todo comment GL-6015

--- app/Controller/PaymentsController.php
+++ app/Controller/PaymentsController.php
@@ -202,6 +202,8 @@ class PaymentsController extends AppController
      */
     public function receipt(int $historyId)
     {
+        // TODO.Payment: Should implement passing history vals to pdf after @joshua implement.
+
         // If remove comment out, you can check on /payments/receipt
         // $this->layout = 'pdf/one_column';
         // $this->render('pdf/receipt');
