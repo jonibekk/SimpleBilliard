a70e33c45dce67340e3f4b8102a26d886539c9d4
Daiki Hirakata <hirakatad@isao.co.jp>
2017-09-14 16:15:45 +0900

    :ambulance: disable multiple submit. No73 on http://bit.ly/2vUxEMe .

--- app/static_src/js/view/payments/credit_card_form.js
+++ app/static_src/js/view/payments/credit_card_form.js
@@ -74,6 +74,7 @@ if(document.enterCCInfo){
         if (result.token) {
             // Call function to update the card
             updateCreditCard(result.token);
+            return true;
         }
 
         submitButton.disabled = false;
