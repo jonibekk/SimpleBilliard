bd49aac432be8010a96299f7bd314cc5b18546f4
Mauro Fujihara <fujiharam@isao.co.jp>
2017-09-19 14:29:59 +0900

    Add jquery plugin

--- gulp/config.js
+++ gulp/config.js
@@ -75,7 +75,8 @@ const config = {
   },
   js_prerender: {
     src: [
-      node_modules_dir + '/jquery/dist/jquery.js'
+      node_modules_dir + '/jquery/dist/jquery.js',
+      node_modules_dir + '/intl-tel-input/build/js/intlTelInput.min.js',
     ],
     output: {
       file_name: 'goalous.prerender',
@@ -181,8 +182,10 @@ const config = {
       // Collapsed design latest node_modules css
       assets_dir + '/css/select2.css',
       assets_dir + '/css/select2-bootstrap.css',
+      assets_dir + '/css/intlTelInputCustom.css',
 
       node_modules_dir + '/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css',
+      node_modules_dir + '/intl-tel-input/build/css/intlTelInput.css',
     ],
     watch_files: [assets_dir + '/css/**/*.css'],
     output: {
