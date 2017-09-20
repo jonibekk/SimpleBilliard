eba10696ffbab3b32f934f7584f28b72dea5e03a
masuichig <masuichig@isao.co.jp>
2017-09-20 20:29:11 +0900

    date updated to "0000-00-00" on stage (cant reproduce on local env)

--- app/Service/TeamService.php
+++ app/Service/TeamService.php
@@ -191,9 +191,12 @@ class TeamService extends AppService
             'service_use_state_start_date' => "'$startDate'",
             'service_use_state_end_date'   => $endDate ? "'$endDate'" : null,
         ];
+        $condition = [
+            'Team.id' => $teamId,
+        ];
 
         try {
-            if (!$Team->save($data)) {
+            if (!$Team->updateAll($data, $condition)) {
                 throw new Exception(sprintf("Failed update Team use status. data: %s, validationErrors: %s",
                     AppUtil::varExportOneLine($data),
                     AppUtil::varExportOneLine($Team->validationErrors)));
