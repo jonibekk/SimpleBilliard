a21f5f2c1ca576791761d6640511d4376490f2fb
Mauro Fujihara <fujiharam@isao.co.jp>
2017-10-24 13:51:40 +0900

    Add price_plan_id filed to price_plan_purchase_teams table

---
+++ app/Config/Migration/1508820560_add_price_plan_id_to_price_plan_purchase_teams.php
@@ -0,0 +1,50 @@
+<?php
+class AddPricePlanIdToPricePlanPurchaseTeams extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_price_plan_id_to_price_plan_purchase_teams';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'price_plan_purchase_teams' => array(
+					'price_plan_id' => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => false, 'after' => 'team_id'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'price_plan_purchase_teams' => array('price_plan_id'),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
