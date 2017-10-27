e732ea2cf1c02cf685ed5ed0245f9059adc77f35
Mauro Fujihara <fujiharam@isao.co.jp>
2017-10-27 16:13:26 +0900

    Tests

--- app/Test/Case/Service/CampaignServiceTest.php
+++ app/Test/Case/Service/CampaignServiceTest.php
@@ -3,9 +3,25 @@ App::uses('GoalousTestCase', 'Test');
 
 /**
  * Class CampaignServiceTest
+ *
+ * @property CampaignService    $CampaignService
  */
 class CampaignServiceTest extends GoalousTestCase
 {
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = array(
+        'app.user',
+        'app.team',
+        'app.team_member',
+        'app.campaign_team',
+        'app.mst_price_plan_groups',
+        'app.mst_price_plans',
+    );
+
     /**
      * setUp method
      *
@@ -13,7 +29,66 @@ class CampaignServiceTest extends GoalousTestCase
      */
     public function setUp()
     {
-        // TODO:campaign create test cases
+        parent::setUp();
+        $this->setDefaultTeamIdAndUid();
+        $this->CampaignService = ClassRegistry::init('CampaignService');
+    }
+
+    private function setupCampaign()
+    {
+        /** @var CampaignPriceGroup $CampaignPriceGroup */
+        $CampaignPriceGroup = ClassRegistry::init('CampaignPriceGroup');
+        /** @var CampaignPricePlan $CampaignPricePlan */
+        $CampaignPricePlan = ClassRegistry::init('CampaignPricePlan');
+        /** @var CampaignTeam $CampaignTeam */
+        $CampaignTeam = ClassRegistry::init('CampaignTeam');
+        // Create price group
+        $pricePlanGroup = [
+            'currency' => 1,
+        ];
+        $CampaignPriceGroup->create();
+        $CampaignPriceGroup->save($pricePlanGroup, false);
+
+        // Price plans
+        $pricePlans = [
+            [
+                'group_id' => 1,
+                'code' => 'C50',
+                'max_members' => 50,
+            ],
+            [
+                'group_id' => 1,
+                'code' => 'C200',
+                'max_members' => 200,
+            ],
+            [
+                'group_id' => 1,
+                'code' => 'C300',
+                'max_members' => 300,
+            ],
+            [
+                'group_id' => 1,
+                'code' => 'C400',
+                'max_members' => 400,
+            ],
+            [
+                'group_id' => 1,
+                'code' => 'C500',
+                'max_members' => 500,
+            ]
+        ];
+        $CampaignPricePlan->bulkInsert($pricePlans);
+
+        // Create campaign team
+        $campaignTeam = [
+            'team_id' => 1,
+            'campaign_type' => 0,
+            'price_plan_group_id' => 1,
+            'start_date' => '2017-10-27',
+        ];
+
+        $CampaignTeam->create();
+        $CampaignTeam->save($campaignTeam);
     }
 
     function test_isCampaignTeam()
