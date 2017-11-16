faac1f3e8ffdca213b6b465b5f0a0cd1ff548ed6
Daiki Hirakata <hirakatad@isao.co.jp>
2017-11-16 15:29:14 +0900

    fix test datas.

--- app/Config/Schema/local_test_data/TeamMemberData.php
+++ app/Config/Schema/local_test_data/TeamMemberData.php
@@ -9,49 +9,58 @@ class TeamMemberData
             'team_id'   => '1',
             'user_id'   => '1',
             'admin_flg' => '1',
+            'status'    => '1',
             'member_no' => 'member_admin_team_a'
         ],
         [
             'team_id'   => '2',
             'user_id'   => '1',
             'admin_flg' => '1',
+            'status'    => '1',
             'member_no' => 'member_admin_team_b'
         ],
         [
             'team_id'       => '1',
             'user_id'       => '2',
+            'status'        => '1',
             'member_no'     => 'member_a_team_a',
             'coach_user_id' => '1',
         ],
         [
             'team_id'       => '1',
             'user_id'       => '3',
+            'status'        => '1',
             'member_no'     => 'member_b_team_a',
             'coach_user_id' => '1',
         ],
         [
             'team_id'   => '1',
             'user_id'   => '4',
+            'status'    => '1',
             'member_no' => 'member_c_team_a'
         ],
         [
             'team_id'   => '1',
             'user_id'   => '5',
+            'status'    => '1',
             'member_no' => 'member_d_team_a'
         ],
         [
             'team_id'   => '2',
             'user_id'   => '6',
+            'status'    => '1',
             'member_no' => 'member_e_team_b'
         ],
         [
             'team_id'   => '2',
             'user_id'   => '7',
+            'status'    => '1',
             'member_no' => 'member_f_team_b'
         ],
         [
             'team_id'   => '2',
             'user_id'   => '8',
+            'status'    => '1',
             'member_no' => 'member_g_team_b'
         ],
     ];
