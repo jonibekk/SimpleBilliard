ae1bbdb99d15bbee3596635f0b7b19a63f40ea3a
masuichig <masuichig@isao.co.jp>
2017-12-04 18:39:03 +0900

    implemented VideoStreamService::updateFromTranscodeProgressData() and test

--- app/Service/VideoStreamService.php
+++ app/Service/VideoStreamService.php
@@ -13,6 +13,78 @@ use Goalous\Model\Enum as Enum;
  */
 class VideoStreamService extends AppService
 {
+    public function updateFromTranscodeProgressData(array $videoStream, TranscodeProgressData $transcodeProgressData): bool
+    {
+        $videoStreamId = $videoStream['id'];
+        $progressState = $transcodeProgressData->getProgressState();
+
+        /** @var VideoStream $VideoStream */
+        $VideoStream = ClassRegistry::init('VideoStream');
+        $currentVideoStreamStatus = new Enum\Video\VideoTranscodeStatus(intval($videoStream['status_transcode']));
+        if ($progressState->equals(Enum\Video\VideoTranscodeProgress::PROGRESS())) {
+            // if transcode is started
+            if (!$currentVideoStreamStatus->equals(Enum\Video\VideoTranscodeStatus::QUEUED())) {
+                throw new RuntimeException("video_streams.id({$videoStreamId}) is not queued");
+            }
+            $status = Enum\Video\VideoTranscodeStatus::TRANSCODING;
+            $videoStream['status_transcode'] = $status;
+            $videoStream['transcode_info'] = "[]";// TODO: add ETS JobId at least
+            $VideoStream->save($videoStream);
+            CakeLog::info(sprintf('transcode status changed: %s', AppUtil::jsonOneLine([
+                'video_streams.id' => $videoStreamId,
+                'state' => $progressState->getValue(),
+                'status_value_from' => $currentVideoStreamStatus->getValue(),
+                'status_value_to' => $status,
+            ])));
+            return true;
+        } else if ($progressState->equals(Enum\Video\VideoTranscodeProgress::ERROR())) {
+            // if transcode is error
+            $status = Enum\Video\VideoTranscodeStatus::ERROR;
+            if (!$currentVideoStreamStatus->equals(Enum\Video\VideoTranscodeStatus::TRANSCODING())) {
+                throw new RuntimeException("video_streams.id({$videoStreamId}) is not transcoding");
+            }
+            $videoStream['status_transcode'] = $status;
+            $VideoStream->save($videoStream);
+            CakeLog::info(sprintf('transcode status changed: %s', AppUtil::jsonOneLine([
+                'video_streams.id' => $videoStreamId,
+                'state' => $progressState->getValue(),
+                'status_value_from' => $currentVideoStreamStatus->getValue(),
+                'status_value_to' => $status,
+            ])));
+            return true;
+        } else if ($progressState->equals(Enum\Video\VideoTranscodeProgress::COMPLETE())) {
+            // if transcode is completed
+            $status = Enum\Video\VideoTranscodeStatus::TRANSCODE_COMPLETE;
+            if (!$currentVideoStreamStatus->equals(Enum\Video\VideoTranscodeStatus::TRANSCODING())) {
+                throw new RuntimeException("video_streams.id({$videoStreamId}) is not transcoding");
+            }
+            $videoStream['status_transcode']     = Enum\Video\VideoTranscodeStatus::TRANSCODE_COMPLETE;
+            $videoStream['duration']             = $transcodeProgressData->getDuration();
+            $videoStream['aspect_ratio']         = $transcodeProgressData->getAspectRatio();
+            $videoStream['master_playlist_path'] = $transcodeProgressData->getPlaylistPath();
+            $VideoStream->save($videoStream);
+            CakeLog::info(sprintf('transcode status changed: %s', AppUtil::jsonOneLine([
+                'video_streams.id' => $videoStreamId,
+                'state' => $progressState->getValue(),
+                'status_value_from' => $currentVideoStreamStatus->getValue(),
+                'status_value_to' => $status,
+            ])));
+
+            /** @var PostDraft $PostDraft */
+            $PostDraft = ClassRegistry::init('PostDraft');
+            $postDraft = $PostDraft->getFirstByResourceTypeAndResourceId(Enum\Post\PostResourceType::VIDEO_STREAM(), $videoStreamId);
+            // TODO: ここ、複数紐付いている下書きがあった場合、一つしかpostされない可能性がある
+            if (!empty($postDraft)) {
+                /** @var Post $Post */
+                $Post = ClassRegistry::init('Post');
+                $this->current_team_id = $postDraft['team_id'];
+                $Post->addNormalFromPostDraft($postDraft);
+            }
+            return true;
+        }
+        throw new RuntimeException("video_streams.id({$videoStreamId}) is not transcoding");
+    }
+
     public function findVideoStreamIfExists(int $userId, int $teamId, string $hash): array
     {
         /** @var Video $Video */
