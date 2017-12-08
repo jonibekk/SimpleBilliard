3f16cc56c3e0a0fa980cfbfabd51035d7f17eac7
masuichig <masuichig@isao.co.jp>
2017-12-08 14:07:49 +0900

    add batch shell that video transcode with no notification make to be error

---
+++ app/Console/Command/Batch/Video/Console/Command/FailStuckedTranscodeShell.php
@@ -0,0 +1,61 @@
+<?php
+App::uses('AppModel', 'Model');
+App::uses('VideoStream', 'Model');
+
+use Goalous\Model\Enum as Enum;
+
+class FailStuckedTranscodeShell extends AppShell
+{
+    public function startup()
+    {
+        parent::startup();
+    }
+
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+        $options = [
+            'seconds_to_be_no_progress' => [
+                'help'    => 'no progress for passed second decided to be trasncode stopped',
+                'default' => 3600,
+            ],
+        ];
+        $parser->addOptions($options);
+        return $parser;
+    }
+
+    public function main()
+    {
+        $seconds = Hash::get($this->params, 'seconds_to_be_no_progress');
+        $targetDateTime = GoalousDateTime::now()->subSecond($seconds);
+        GoalousLog::info('target base time to decide transcode stopped', [
+            'seconds_to_be_no_progress' => $seconds,
+            'timestamp' => $targetDateTime->getTimestamp(),
+            'date'      => $targetDateTime->toIso8601String(),
+        ]);
+
+        /** @var VideoStream $VideoStream */
+        $VideoStream = ClassRegistry::init('VideoStream');
+        $stuckedVideoStreams = $VideoStream->getNoProgressBeforeTimestamp(
+            $targetDateTime->getTimestamp()
+        );
+
+        GoalousLog::info('fetched stucked video stream', [
+            'count' => count($stuckedVideoStreams),
+        ]);
+
+        foreach ($stuckedVideoStreams as $videoStream) {
+            GoalousLog::info('stucked video stream change to error', [
+                'id' => $videoStream['id'],
+                'status_transcode'   => $videoStream['status_transcode'],
+                'elapse time from modified'   => GoalousDateTime::now()->diffInSeconds(
+                    GoalousDateTime::createFromTimestamp($videoStream['modified'])
+                ),
+            ]);
+            $videoStream['status_transcode'] = Enum\Video\VideoTranscodeStatus::ERROR();
+            $VideoStream->save($videoStream);
+            $VideoStream->setTranscodeInfo($videoStream, 'no_notification', true);
+        }
+    }
+
+}
--- app/Model/VideoStream.php
+++ app/Model/VideoStream.php
@@ -1,11 +1,37 @@
 <?php
 App::uses('AppModel', 'Model');
 
+use Goalous\Model\Enum as Enum;
+
 /**
  * Class VideoStream
  */
 class VideoStream extends AppModel
 {
+    public function setTranscodeInfo(array $videoStream, string $key, $value)
+    {
+        $infos = [];
+        if (!isset($videoStream['id'])) {
+            GoalousLog::warning('video_streams.id is undefined');
+        }
+        if (!isset($videoStream['transcode_info']) || !is_string($videoStream['transcode_info'])) {
+            GoalousLog::notice('video_streams.transcode_info is not set', [
+                'video_streams.id' => video_streams['id'],
+            ]);
+        }
+        $currentTranscodeInfo = json_decode($videoStream['transcode_info'], true);
+        if (is_null($currentTranscodeInfo)) {
+            GoalousLog::notice('video_streams.transcode_info is not json', [
+                'video_streams.id' => video_streams['id'],
+            ]);
+        }
+        $currentTranscodeInfo[$key] = $value;
+        $this->save([
+            'id' => $videoStream['id'],
+            'transcode_info' => json_encode($currentTranscodeInfo),
+        ]);
+    }
+
     public function getFirstByVideoId(int $videoId): array
     {
         $options = [
@@ -33,6 +59,31 @@ class VideoStream extends AppModel
             ],
         ];
 
+        return Hash::extract($this->find('all', $options), '{n}.VideoStream');
+    }
+
+    /**
+     * get video_streams that status not changed from passed $timestamp
+     * transcode is in progress (QUEUED, TRANSCODING)
+     *
+     * @param int $timestamp
+     *
+     * @return array
+     */
+    public function getNoProgressBeforeTimestamp(int $timestamp): array
+    {
+        $options = [
+            'conditions' => [
+                'status_transcode' => [
+                    Enum\Video\VideoTranscodeStatus::TRANSCODING,
+                    Enum\Video\VideoTranscodeStatus::QUEUED,
+                    Enum\Video\VideoTranscodeStatus::UPLOADING,
+                    Enum\Video\VideoTranscodeStatus::UPLOAD_COMPLETE,
+                ],
+                'modified <' => $timestamp,
+                'del_flg' => 0,
+            ],
+        ];
         return Hash::extract($this->find('all', $options), '{n}.VideoStream');
     }
 }
