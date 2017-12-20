f95fb49cef9dcc8a81afae65d3c467c673932b28
endam <yoshidam2@isao.co.jp>
2017-12-20 11:17:37 +0900

    :dolphin: create db migration file for new saved_posts table

---
+++ app/Config/Migration/1513735941_add_saved_post_table.php
@@ -0,0 +1,66 @@
+<?php
+class AddSavedPostTable extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_saved_post_table';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'saved_posts' => array(
+					'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+					'team_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+					'post_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+					'user_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+					'del_flg' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+					'deleted' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true),
+					'created' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+					'modified' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'saved_posts_unique' => array('column' => array('post_id', 'user_id'), 'unique' => 1),
+						'team_id' => array('column' => 'team_id', 'unique' => 0),
+						'post_id' => array('column' => 'post_id', 'unique' => 0),
+						'user_id' => array('column' => 'user_id', 'unique' => 0),
+						'created' => array('column' => 'created', 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'saved_posts'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
