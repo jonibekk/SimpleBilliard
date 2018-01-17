b63f3a4039c8b3e45df909765c034659e16e3279
masuichig <masuichig@isao.co.jp>
2018-01-17 16:23:27 +0900

    add authentication on videos belongs team

--- app/Controller/Api/V1/VideoStreamsController.php
+++ app/Controller/Api/V1/VideoStreamsController.php
@@ -2,20 +2,24 @@
 App::uses('ApiController', 'Controller/Api');
 App::uses('VideoStream', 'Model');
 App::uses('Video', 'Model');
+App::uses('TeamMember', 'Model');
 App::uses('TranscodeOutputVersionDefinition', 'Model/Video/Transcode');
 App::uses('VideoStorageClient', 'Model/Video');
+App::uses('TeamStatus', 'Lib/Status');
 
 use Goalous\Model\Enum as Enum;
 
 /**
  * @property VideoStream $VideoStream
  * @property Video $Video
+ * @property TeamMember $TeamMember
  */
 class VideoStreamsController extends ApiController
 {
     public $uses = [
         'VideoStream',
         'Video',
+        'TeamMember',
     ];
 
     function get_source($id)
@@ -24,11 +28,27 @@ class VideoStreamsController extends ApiController
         if (empty($type)) {
             return $this->_getResponseBadFail('bad request');
         }
+        // retrieve video data
         $videoStream = $this->VideoStream->findById($id);
         if (empty($videoStream)) {
             return $this->_getResponseNotFound();
         }
         $videoStream = reset($videoStream);
+        $video = $this->Video->findById($videoStream['video_id']);
+        if (empty($video)) {
+            return $this->_getResponseNotFound();
+        }
+        $video = reset($video);
+
+        // check target video can playable by user belongs teams
+        $videosTeamId = $video['team_id'];
+        $userId = $this->Auth->user('id');
+        $teamsUserBelongs = $this->TeamMember->getAllTeam($userId);
+        $teamIdsUserBelongs = Hash::extract($teamsUserBelongs, '{n}.TeamMember.team_id');
+        if (!in_array($videosTeamId, $teamIdsUserBelongs)) {
+            return $this->_getResponseNotFound();
+        }
+
         $videoStoragePath = $videoStream['storage_path'];
         $outputVersion = new Enum\Video\TranscodeOutputVersion(intval($videoStream['output_version']));
         $transcodeOutputVersionDefinition = TranscodeOutputVersionDefinition::getVersion($outputVersion);
