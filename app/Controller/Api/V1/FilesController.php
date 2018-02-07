865d69756d5c2b22984901064ee4e37aac9fcaa4
masuichig <masuichig@isao.co.jp>
2018-02-07 11:09:21 +0900

    move video mime-type definition to config

--- app/Controller/Api/V1/FilesController.php
+++ app/Controller/Api/V1/FilesController.php
@@ -78,9 +78,6 @@ class FilesController extends ApiController
      * $requestFileUpload should be the
      * value get from Hash::get($this->request->params, 'form');
      *
-     * @see FYI: defined mime-types in IANA
-     *      https://www.iana.org/assignments/media-types/media-types.xhtml#video
-     *
      * @return bool
      */
     public function isVideo(array $requestFileUpload): bool
@@ -94,86 +91,9 @@ class FilesController extends ApiController
 
         // checking in mime-types in the file for more certain info
         $fileMimeType = mime_content_type($requestFileUpload['file']['tmp_name']);
-        return in_array($fileMimeType, [
-            // this video mime-types is referred from iana.org list
-            'video/1d-interleaved-parityfec',
-            'video/3gpp',
-            'video/3gpp2',
-            'video/3gpp-tt',
-            'video/BMPEG',
-            'video/BT656',
-            'video/CelB',
-            'video/DV',
-            'video/encaprtp',
-            'video/example',
-            'video/H261',
-            'video/H263',
-            'video/H263-1998',
-            'video/H263-2000',
-            'video/H264',
-            'video/H264-RCDO',
-            'video/H264-SVC',
-            'video/H265',
-            'video/iso.segment',
-            'video/JPEG',
-            'video/jpeg2000',
-            'video/mj2',
-            'video/MP1S',
-            'video/MP2P',
-            'video/MP2T',
-            'video/mp4',
-            'video/MP4V-ES',
-            'video/MPV',
-            'video/mpeg4-generic',
-            'video/nv',
-            'video/ogg',
-            'video/pointer',
-            'video/quicktime',
-            'video/raptorfec',
-            'video/rtp-enc-aescm128',
-            'video/rtploopback',
-            'video/rtx',
-            'video/smpte291',
-            'video/SMPTE292M',
-            'video/ulpfec',
-            'video/vc1',
-            'video/vnd.CCTV',
-            'video/vnd.dece.hd',
-            'video/vnd.dece.mobile',
-            'video/vnd.dece-mp4',
-            'video/vnd.dece.pd',
-            'video/vnd.dece.sd',
-            'video/vnd.dece.video',
-            'video/vnd.directv-mpeg',
-            'video/vnd.directv.mpeg-tts',
-            'video/vnd.dlna.mpeg-tts',
-            'video/vnd.dvb.file',
-            'video/vnd.fvt',
-            'video/vnd.hns.video',
-            'video/vnd.iptvforum.1dparityfec-1010',
-            'video/vnd.iptvforum.1dparityfec-2005',
-            'video/vnd.iptvforum.2dparityfec-1010',
-            'video/vnd.iptvforum.2dparityfec-2005',
-            'video/vnd.iptvforum.ttsavc',
-            'video/vnd.iptvforum.ttsmpeg2',
-            'video/vnd.motorola.video',
-            'video/vnd.motorola.videop',
-            'video/vnd-mpegurl',
-            'video/vnd.ms-playready.media.pyv',
-            'video/vnd.nokia.interleaved-multimedia',
-            'video/vnd.nokia.mp4vr',
-            'video/vnd.nokia.videovoip',
-            'video/vnd.objectvideo',
-            'video/vnd.radgamettools.bink',
-            'video/vnd.radgamettools.smacker',
-            'video/vnd.sealed.mpeg1',
-            'video/vnd.sealed.mpeg4',
-            'video/vnd.sealed-swf',
-            'video/vnd.sealedmedia.softseal-mov',
-            'video/vnd.uvvu-mp4',
-            'video/vnd-vivo',
-            'video/VP8',
-        ]);
+        $fileMimeType = strtolower($fileMimeType);
+        $allowVideoTypes = Configure::read("allow_video_types");
+        return in_array($fileMimeType, $allowVideoTypes);
     }
 
     /**
