9ff02de034c4d80c949ce9f369239c195580f1f4
Joshua McCarthy <mccarthyj@isao.co.jp>
2018-02-28 16:33:40 +0900

    Created conditional if mobile app or desktop

--- app/static_src/js/react/message/actions/detail.js
+++ app/static_src/js/react/message/actions/detail.js
@@ -161,7 +161,12 @@ export function sendMessage() {
           type: FileUploadModule.RESET_STATE
         })
         var ta = document.getElementsByClassName('topicDetail-footer-inputBody')[0];
-        ta.style.height = ""; /* Reset the height*/
+        // Reset the height
+        if (!isMobileApp()) {
+          autosize.update(ta);
+        }else{
+          ta.style.height = ""; 
+        }
       },
       ({response}) => {
         const err_msg = getErrMsg(response);
