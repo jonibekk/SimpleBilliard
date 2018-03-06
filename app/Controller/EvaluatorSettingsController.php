797ab7eeffd864d9480681b3eff191f04ce61b60
masuichig <masuichig@isao.co.jp>
2018-03-06 19:08:54 +0900

    add backend process of /evaluator_settings GL-6615

--- app/Controller/EvaluatorSettingsController.php
+++ app/Controller/EvaluatorSettingsController.php
@@ -1,5 +1,7 @@
 <?php
 App::uses('AppController', 'Controller');
+App::uses('User', 'Model');
+App::import('Service', 'EvaluationService');
 
 /**
  * EvaluatorSettingsController Controller
@@ -15,10 +17,67 @@ class EvaluatorSettingsController extends AppController
     }
 
     /**
-     * TODO: implement here
+     * Evaluation list of
+     * self evaluation and coachee evaluation
      */
     function index()
     {
+        $this->layout = LAYOUT_ONE_COLUMN;
+
+        /** @var  EvaluationService $EvaluationService */
+        $EvaluationService = ClassRegistry::init('EvaluationService');
+
+        $termId = $this->Team->Term->getCurrentTermId();
+        $userId = $this->Auth->user('id');
+
+        $selfEvaluation = $EvaluationService->getEvalStatus($termId, $userId);
+        $evaluateesEvaluation = $EvaluationService->getEvaluateeFromCoachUserId($termId, $userId);
+
+        // Reconstruct evaluation flow data structure
+        $selfEvaluation = $this->reconstructFlows([$selfEvaluation])[0];
+        $evaluateesEvaluation = $this->reconstructFlows($evaluateesEvaluation);
+
+        // Count zero evaluatee users
+        $countOfZeroEvaluateeUsers = 0;
+        foreach ($evaluateesEvaluation as $key => $evaluateeEvaluation) {
+            if (0 === count($evaluateeEvaluation['flow'])) {
+                $countOfZeroEvaluateeUsers++;
+            }
+        }
+
+        $isFixedEvaluationOrder = $this->Team->EvaluationSetting->isFixedEvaluationOrder();
+
+        $this->set('termId', $termId);
+        $this->set('selfEvaluation', $selfEvaluation);
+        $this->set('evaluateesEvaluation', $evaluateesEvaluation);
+        $this->set('isFrozen', false);
+        $this->set('isFixedEvaluationOrder', $isFixedEvaluationOrder);
+        $this->set('countOfZeroEvaluateeUsers', $countOfZeroEvaluateeUsers);
+    }
+
+    /**
+     * Filtering for using to showing only evaluators
+     *
+     * @param array $evaluations
+     *
+     * @return array
+     */
+    private function reconstructFlows(array $evaluations): array
+    {
+        foreach ($evaluations as $key => $evaluation) {
+            $flow = $evaluation['flow'] ?? [];
+            $evaluations[$key]['flow'] = array_values(
+                // leave the leader and evaluator in the flow array
+                // removing "self" and "final evaluator"
+                array_filter($flow, function ($evaluateFlow) {
+                    return in_array($evaluateFlow['evaluate_type'], [
+                        Evaluation::TYPE_EVALUATOR,
+                        Evaluation::TYPE_LEADER,
+                    ]);
+                })
+            );
+        }
+        return $evaluations;
     }
 
     /**
