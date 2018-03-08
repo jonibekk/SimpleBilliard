285370079e5065a1ce64a48b91a6df7c5afe72bb
mukoyamak-isao <mukoyamak@K0419-001-180228.local>
2018-03-08 16:09:17 +0900

    create a utility class to handle text

---
+++ app/Lib/Util/TextUtil.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Class PaymentUtil
+ */
+class TextUtil
+{
+    static function extractAllIdFromMention($text) {
+        preg_match_all('/<@(.*)?>/m', $text, $matches);
+        $result = array();
+        if (count($matches[1]) > 0) {
+	        foreach ($matches[1] as $match) {
+	        	$isUser = strpos($match, 'user') === 0;
+	        	$isCircle = strpos($match, 'circle') === 0;
+	        	$replacement = '';
+	        	if ($isUser) {
+	        		$replacement = 'user_';
+	        	}else if ($isCircle) {
+	        		$replacement = 'circle_';
+	        	}
+	        	$result[$match] = array(
+	        		'id' => str_replace($replacement, '', $match),
+	        		'isUser' => strpos($match, 'user') === 0, 
+	        		'isCircle' => strpos($match, 'circle') === 0
+	        	);
+	        }
+	    }
+        return $result;
+    }
+
+    static function replaceAndAddNameToMention($pattern, $replacement, $subject) {
+        $result = preg_replace('/<@'.$pattern.'>/m', '<@'.$pattern.':'.$replacement.'>', $subject);
+        return $result;
+    }
+
+}
