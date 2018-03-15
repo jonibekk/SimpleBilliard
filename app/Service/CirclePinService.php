6ed51c6944acbd6f6fba865ecb5f6c450080e62a
Seri Jihan Oshima <oshimas@isao.co.jp>
2018-03-15 11:14:35 +0900

    totally renewed circlepin service

--- app/Service/CirclePinService.php
+++ app/Service/CirclePinService.php
@@ -7,157 +7,41 @@ App::uses('CircleMember', 'Model');
 App::uses('AppUtil', 'Util');
 
 /**
- * Class SavedPostService
+ * Class CircleMemberService
  */
 class CirclePinService extends AppService
 {
-    function getDefaultCircle(){
-        $Circle = ClassRegistry::init('Circle');
-        return $Circle->getCirclesDefault();
-    }
-
     /**
-     * Find pinned circles.
-     *
-     * @return array
-     */
-    function getPinned(): array
-    {
-        //Retreive User's Circle List
-        $CirclePin = ClassRegistry::init('CirclePin');
-        $pins = $CirclePin->getPinnedCircles();
-
-        //Retrieve Actual Circle Data
-        $Circle = ClassRegistry::init('Circle');
-        $CircleMember = ClassRegistry::init('CircleMember');
-        $circles = $Circle->getCirclesByIds($CircleMember->getMyCircleList());
-        //$circles = Hash::combine($circles, '{n}.Circle.id', '{n}.Circle');
-
-
-        $pinned = array();
-        $unpinned = array();
-
-        foreach ($pins as $circleId => $pinOrder) {
-            if(array_key_exists($circleId, $circles)){
-                $target = $circles[$circleId];
-                $target['Circle']['pin_order'] = $pinOrder['CirclePin']['pin_order'];
-                $pinned[] = $target;
-            }
-        }
-        foreach ($circles as $key => $value) {
-            if(!array_key_exists($key, $pinned)){
-                $unpinned[] = $value;
-            }
-        }
-        // $pinned = Hash::combine($pinned, '{n}.Circle.id', '{n}.Circle');
-        // $unpinned = Hash::combine($unpinned, '{n}.Circle.id', '{n}.Circle');
-        usort($pinned, function ($item1, $item2) {
-            return $item1['pin_order'] <=> $item2['pin_order'];
-        });
-        usort($pinned, function($a, $b) {
-            return $a['order'] <=> $b['order'];
-        });
-        $results['pinned'] = $pinned;
-        $results['unpinned'] = $unpinned;
-$this->log($pinned);
-        return $results;
-    }
-
-    /**
-     * build circle pin data
-     *
-     * @param  int     $userId
-     * @param  int     $teamId
-     * @param  array   $pinOrderData
-     *
-     * @return array
-     */
-    function buildJoinData(int $userId, int $teamId, array $pinOrderData): array
-    {
-        /** @var CirclePin $CirclePin */
-        $CirclePin = ClassRegistry::init('CirclePin');
-        /** @var CircleMember $CircleMember */
-        $CircleMember = ClassRegistry::init('CircleMember');
-
-        $saveData = [
-            'user_id'               => $userId,
-            'team_id'               => $CircleMember->current_team_id,
-            'circle_id'             => $pinOrderData['circle_id'],
-            'pin_order'             => $pinOrderData['pin_order'],
-            'del_flg'               => false,
-        ];
-        return $saveData;
-    }
-
-    /**
-     * BulkInsert circle pin
-     *
-     * @param int    $userId
-     * @param int    $teamId
-     * @param array  $pinOrderDatas
-     *
+     * 自分が所属しているサークルを抜けるときに更新処理を行うメソッド
      * @return bool
      */
-    function bulkInsert(int $userId, int $teamId, array $pinOrderDatas): bool
+    public function deleteCircleOrder(int $userId, int $teamId, int $circleId): bool
     {
-        /** @var CirclePin $CirclePin */
-        $CirclePin = ClassRegistry::init('CirclePin');
-        $conditions = [
-            'CirclePin.user_id' => $userId,
-        ];
-        try {
-            $CirclePin->begin();
-            if (!$CirclePin->deleteAll($conditions)) {
-                throw new Exception(sprintf("Failed to delete orders of circle pin."));
-            }
-            // build save data
-            $saveData = [];
-            foreach ($pinOrderDatas as $pinOrderData) {
-                $saveData[] = $this->buildJoinData($userId, $teamId, $pinOrderData);
-            }
-            if(!empty($saveData)){
-                // bulk save
-                if (!$CirclePin->bulkInsert($saveData)) {
-                    throw new Exception(sprintf("Failed to add orders to circle pin. data:%s", var_export($saveData, true)));
-                }
-            }
-        } catch (Exception $e) {
+        try{
+            ClassRegistry::init('CirclePin')->deleteId($userId, $teamId, $orders);
+        } catch (RuntimeException $e) {
             $this->log(sprintf("[%s]%s", __METHOD__, $e->getMessage()));
             $this->log($e->getTraceAsString());
-            $CirclePin->rollback();
             return false;
         }
 
-        $CirclePin->commit();
         return true;
     }
 
     /**
-     * Delete all circle pins belonging to a user
-     *
-     * @param int    $userId
-     *
+     * 自分が所属しているサークルのソート順をDBへセットする
      * @return bool
      */
-    function delete(int $userId): bool{
-        /** @var CirclePin $CirclePin */
-        $CirclePin = ClassRegistry::init('CirclePin');
-
-        try {
-            $CirclePin->begin();
-
-            // delete
-            if (!$CirclePin->delete($userId)) {
-                throw new Exception("Failed to delete circle pins.");
-            }
-        } catch (Exception $e) {
+    public function setCircleOrders(int $userId, int $teamId, string $orders): bool
+    {
+        try{
+            ClassRegistry::init('CirclePin')->insertUpdate($userId, $teamId, $orders);
+        } catch (RuntimeException $e) {
             $this->log(sprintf("[%s]%s", __METHOD__, $e->getMessage()));
             $this->log($e->getTraceAsString());
-            $CirclePin->rollback();
             return false;
         }
 
-        $CirclePin->commit();
         return true;
     }
 }
