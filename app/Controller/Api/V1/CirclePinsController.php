ec837a053aef4d8be1d554ba231f36dcca1f559f
Seri Jihan Oshima <oshimas@isao.co.jp>
2018-03-16 16:10:04 +0900

    modified circle pins api

--- app/Controller/Api/V1/CirclePinsController.php
+++ app/Controller/Api/V1/CirclePinsController.php
@@ -1,6 +1,8 @@
 <?php
 App::uses('ApiController', 'Controller/Api');
 App::uses('AppController', 'Controller');
+App::uses('CirclesController', 'Controller');
+App::uses('Circle', 'Model');
 App::import('Service', 'CirclePinService');
 /**
  * Class CirclePinsController
@@ -20,7 +22,7 @@ class CirclePinsController extends ApiController
     /**
      * Update all pin orders relevant to the user using csv.
      * 
-     * usage: PUT:/api/v1/circle_pins/
+     * usage: POST:/api/v1/circle_pins/
      *
      * @return CakeResponse
      */
@@ -31,8 +33,7 @@ class CirclePinsController extends ApiController
         }
 
         if(!array_key_exists('csv', $this->request->data)) {
-            //TODO:fix call procedure use PUT instedad jsut for testing
-            $this->circle_edit_ajax($this->request->data);
+            return $this->_getResponseInternalServerError("Invalid Data");
         }
 
         if (!ClassRegistry::init("CirclePinService")->setCircleOrders($this->Auth->user('id'), $this->current_team_id, $this->request->data['csv'])) {
@@ -40,25 +41,5 @@ class CirclePinsController extends ApiController
         } else {
             //TODO:hamburger update => ClassRegistry::init('AppController')->_setMyCircle();
         }
-    }
-
-    /**
-     * Opens the circle edit modal if the user_id is circle's admin
-     * 
-     * usage: POST:/api/v1/circle_pins/
-     *
-     * @return CakeResponse
-     */
-    public function circle_edit_ajax($data)
-    {
-        if(!isset($data)) {
-            return $this->_getResponseBadFail("No Data");
-        }
-
-        if(!array_key_exists('circle_id', $data)) {
-            return $this->_getResponseBadFail("Missing Data");
-        }
-
-        return ClassRegistry::init("CirclePinService")->isUserCircleAdmin($this->Auth->user('id'), $data['circle_id']);
     }
 }
