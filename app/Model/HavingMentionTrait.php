f2ddc9631ad2a9836894e2577094ac4022622c22
mukoyamak-isao <mukoyamak@isao.co.jp>
2018-03-22 03:34:59 +0000

    remove unnecessary comment

--- app/Model/HavingMentionTrait.php
+++ app/Model/HavingMentionTrait.php
@@ -8,8 +8,6 @@ trait HavingMentionTrait {
             foreach ($results as &$result) {
                 if (isset($result[$this->alias]) && isset($result[$this->alias][$this->bodyProperty])) {
                     $body = $result[$this->alias][$this->bodyProperty];
-                    // I know that we don't use Component in Model 
-                    // but an error occurs when I use Model(s) here like 'Badge' class blah, blah, blah
                     $result[$this->alias][$this->bodyProperty] = MentionComponent::appendName($body);
                 }
             }
