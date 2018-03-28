7014bc488c51e9d352187f4344e85f1f516184f5
Seri Jihan Oshima <oshimas@isao.co.jp>
2018-03-28 09:27:09 +0900

    merged migration files

--- app/Config/Migration/1520971955_add_table_circle_pins_20180314.php
+++ app/Config/Migration/1520971955_add_table_circle_pins_20180314.php
@@ -18,18 +18,16 @@ class AddTableCirclePins20180314 extends CakeMigration {
 			'create_table' => array(
 				'circle_pins' => array(
 					'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
-					'circle_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
 					'team_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
 					'user_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
-					'orders' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
-					'del_flg' => array('type' => 'boolean', 'null' => true, 'default' => null),
+					'circle_orders' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'del_flg' => array('type' => 'boolean', 'null' => true, 'default' => false),
 					'deleted' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true),
 					'created' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true),
 					'modified' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => true),
 					'indexes' => array(
 						'PRIMARY' => array('column' => 'id', 'unique' => 1),
 						'user_id' => array('column' => 'user_id', 'unique' => 0),
-						'circle_id' => array('column' => 'circle_id', 'unique' => 0),
 						'team_id' => array('column' => 'team_id', 'unique' => 0),
 					),
 					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB'),
