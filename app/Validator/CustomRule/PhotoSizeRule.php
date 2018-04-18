2d452b1e311741eab3b75c59812e2beca9a6834d
Stephen Raharja <raharjas@isao.co.jp>
2018-04-18 09:59:14 +0900

    Created test validator for goalous models

---
+++ app/Validator/CustomRule/PhotoSizeRule.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: StephenRaharja
+ * Date: 2018/04/19
+ * Time: 12:16
+ */
+
+namespace Custom\Validation\Rule;
+
+use Respect\Validation\Rules\AbstractRule;
+
+class PhotoSizeRule extends AbstractRule
+{
+    public function validate($input)
+    {
+        // TODO: Implement validate() method.
+    }
+
+}
