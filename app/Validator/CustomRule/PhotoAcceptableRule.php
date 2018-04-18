e8dc72813ca71f011457028ab2f32e7e3b1cc50b
Stephen Raharja <raharjas@isao.co.jp>
2018-04-18 09:59:14 +0900

    Created test validator for goalous models

---
+++ app/Validator/CustomRule/PhotoAcceptableRule.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: StephenRaharja
+ * Date: 2018/04/19
+ * Time: 12:18
+ */
+
+namespace Custom\Validation\Rule;
+
+use Respect\Validation\Rules\AbstractRule;
+
+class PhotoAcceptableRule extends AbstractRule
+{
+    public function validate($input)
+    {
+        // TODO: Implement validate() method.
+    }
+}
