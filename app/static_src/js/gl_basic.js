6b94574e536ec0867eb5b91c0a2edd54d0b91fc3
Seri Jihan Oshima <oshimas@isao.co.jp>
2018-04-24 14:35:58 +0900

    keyboard not hiding on browser/app on android

--- app/static_src/js/gl_basic.js
+++ app/static_src/js/gl_basic.js
@@ -350,14 +350,43 @@ $(function () {
       $("#NavSearchResultsToggle").empty();
       $("#NavSearchResultsToggle").hide();
     });
+    function hideKeyboard() {
+      //this set timeout needed for case when hideKeyborad
+      //is called inside of 'onfocus' event handler
+      setTimeout(function() {
+
+        //creating temp field
+        var field = document.createElement('input');
+        field.setAttribute('type', 'text');
+        //hiding temp field from peoples eyes
+        //-webkit-user-modify is nessesary for Android 4.x
+        field.setAttribute('style', 'position:absolute; top: 0px; opacity: 0; -webkit-user-modify: read-write-plaintext-only; left:0px;');
+        document.body.appendChild(field);
+
+        //adding onfocus event handler for out temp field
+        field.onfocus = function(){
+          //this timeout of 200ms is nessasary for Android 2.3.x
+          setTimeout(function() {
+
+            field.setAttribute('style', 'display:none;');
+            setTimeout(function() {
+              document.body.removeChild(field);
+              document.body.focus();
+            }, 14);
+
+          }, 200);
+        };
+        //focusing it
+        field.focus();
+
+      }, 50);
+    }
     $("#NavSearchInput,#NavSearchInputToggle").off("keyup").on("keyup", function(e) {
       if(!cake.is_mb_app || !cake.is_mb_browser){
         return;
       }
       if(e.keyCode === 13){
-        if(document.activeElement) {
-            document.activeElement.blur();
-        } 
+        hideKeyboard();
       }
     });
     $("#NavSearchHide,#NavSearchHideToggle").off("click").on("click", function() {
