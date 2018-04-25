fd39c5248c8fa2323abde81ab84782730fa1e8dc
Stephen Raharja <stef.stephen.r@gmail.com>
2018-04-25 15:41:47 +0900

    Split test case to 2 methods

--- app/Test/Case/Model/UserTest.php
+++ app/Test/Case/Model/UserTest.php
@@ -1270,10 +1270,18 @@ class UserTest extends GoalousTestCase
         $userValidator = new UserValidator;
 
         $this->assertNotInternalType('array', $userValidator->validate($sampleUser));
+    }
 
+    public function test_NewValidationMethod_failure(){
+        /** @var User $user */
+        $user = ClassRegistry::init('User');
+
+        $sampleUser = $user->getById(1);
+        
         $sampleUser['first_name'] = '129yrb8y))*&)@&$)';
 
-        $this->assertInternalType('array', $userValidator->validate($sampleUser));
+        $userValidator = new UserValidator;
 
+        $this->assertInternalType('array', $userValidator->validate($sampleUser));
     }
 }
