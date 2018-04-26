d1bd3aadeda2d38362d3b08c8ab6f8720056c700
Stephen Raharja <stef.stephen.r@gmail.com>
2018-04-26 16:01:24 +0900

    Improved layout

--- app/static_src/js/view/evaluator_settings/evaluator_settings.js
+++ app/static_src/js/view/evaluator_settings/evaluator_settings.js
@@ -136,7 +136,7 @@ $(document).ready(function () {
 
     function select2Format(state) {
         if (!state.id) return state.text; // optgroup
-        return "<img src='" + state.image + "' width='16' height='16' />" + state.text;
+        return "<img src='" + state.image + "' width='16' height='16' /> " + state.text;
     }
 
     (function () {
