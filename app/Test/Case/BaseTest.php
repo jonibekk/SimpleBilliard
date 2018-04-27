5c5cf23a09d805af07ff98642f836611f805fa27
Stephen Raharja <stef.stephen.r@gmail.com>
2018-04-27 10:51:41 +0900

    Created new validator test directory

--- app/Test/Case/BaseTest.php
+++ app/Test/Case/BaseTest.php
@@ -11,12 +11,13 @@ class BaseTest extends PHPUnit_Framework_TestSuite
             APP_TEST_CASES . DS . 'Console',
             APP_TEST_CASES . DS . 'Lib' . DS . 'Util',
             APP_TEST_CASES . DS . 'Model',
-            APP_TEST_CASES . DS . 'Service'. DS.'Payment1',
+            APP_TEST_CASES . DS . 'Service' . DS . 'Payment1',
         ],
         'CIParallelTests2' => [
             APP_TEST_CASES . DS . 'Service',
-            APP_TEST_CASES . DS . 'Service'. DS.'Payment2',
+            APP_TEST_CASES . DS . 'Service' . DS . 'Payment2',
             APP_TEST_CASES . DS . 'Service' . DS . 'Api',
+            APP_TEST_CASES . DS . 'Validator'
         ],
     ];
 }
