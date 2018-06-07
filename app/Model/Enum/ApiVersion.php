f23db8ea91adb93dbad180108681f617792d62bf
Stephen Raharja <stef.stephen.r@gmail.com>
2018-06-07 10:27:22 +0900

    Created ENUM class for API versions

---
+++ app/Model/Enum/ApiVersion.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Goalous\Model\Enum\ApiVersion;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Created by PhpStorm.
+ * User: StephenRaharja
+ * Date: 2018/06/07
+ * Time: 10:23
+ */
+
+/**
+ * Class ApiVersion
+ * @method static static API_VERSION_2()
+ *
+ * @package Goalous\Model\Enum\ApiVersion
+ */
+class ApiVersion extends Enum
+{
+    const API_VERSION_2 = 2;
+}
