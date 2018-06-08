9911552f2e12d0f9d17a866a1388eb506e323c3d
Seri Jihan Oshima <oshimas@isao.co.jp>
2018-06-08 16:02:59 +0900

    fixed some issue

--- app/static_src/js/gl_basic.js
+++ app/static_src/js/gl_basic.js
@@ -263,6 +263,7 @@ $(function () {
     });
     $(".header-dropdown-add,.header-dropdown-functions,.header-dropdown-notify,.header-dropdown-message,.mb-app-header-dropdown-add,.mb-app-header-dropdown-functions").on("click", function(e) {
         $(".open").removeClass("open");
+        $(".force-open").removeClass("force-open");
         $("#NavSearchResults").empty();
         $("#NavSearchResults").hide();
         $("#NavSearchResultsToggle").empty();
@@ -272,11 +273,8 @@ $(function () {
         $(".header-search-toggle").removeClass("open");
         $(".header-search").removeClass("open");
         $(this).find(".dropdown-menu").toggleClass("open");
+        $(this).find(".dropdown-menu").toggleClass("force-open");
         hideNav();
-    });
-    $(".header-icon-zoom").on("click", function() {
-      $(".force-open").removeClass("force-open");
-      $(this).find(".dropdown-menu").toggleClass("force-open");
     });
     $(document).on("click", function() {
       $(".force-open").removeClass("force-open");
