4c03cde9c7f1ec629571336d7d7d44b87f218ba7
Stephen Raharja <stef.stephen.r@gmail.com>
2018-06-18 15:02:34 +0900

    Created Post Controller

---
+++ app/Controller/Api/PostController.php
@@ -0,0 +1,17 @@
+<?php
+App::uses('BaseApiController', 'Controller/Api');
+
+/**
+ * Created by PhpStorm.
+ * User: StephenRaharja
+ * Date: 2018/06/18
+ * Time: 15:00
+ */
+class PostController extends BaseApiController
+{
+
+    public function post()
+    {
+
+    }
+}
