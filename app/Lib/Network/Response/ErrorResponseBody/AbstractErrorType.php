83c461231527415256722b4a84c0807ecec8e11b
masuichig <masuichig@isao.co.jp>
2018-06-27 11:17:10 +0900

    add comment

--- app/Lib/Network/Response/ErrorResponseBody/AbstractErrorType.php
+++ app/Lib/Network/Response/ErrorResponseBody/AbstractErrorType.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Base class of response error type
+ *
+ * Class AbstractErrorType
+ */
 abstract class AbstractErrorType
 {
     /**
@@ -7,11 +12,20 @@ abstract class AbstractErrorType
      */
     protected $message = '';
 
+    /**
+     * Building a unit of error reason
+     * @see https://confluence.goalous.com/x/gQIQAQ#APIv2ResponseFormat-Errorreasonunittype
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         throw new RuntimeException('need to implement function toArray()');
     }
 
+    /**
+     * @return string
+     */
     public function getMessage(): string
     {
         return $this->message;
