7f2a92d20adcda569af49e4291f49d9e41370e3a
Stephen Raharja <stef.stephen.r@gmail.com>
2018-06-27 17:29:03 +0900

    Added transaction manager

--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -388,10 +388,10 @@ class PostService extends AppService
      * @param int   $teamId
      *
      * @return int Post ID of saved post
+     * @throws Exception
      */
     public function addCirclePost(array $postBody, int $circleId, int $userId, int $teamId): int
     {
-
         /** @var Post $Post */
         $Post = ClassRegistry::init('Post');
         /** @var PostShareCircle $PostShareCircle */
@@ -409,47 +409,55 @@ class PostService extends AppService
             throw new InvalidArgumentException('Error on adding post: Invalid argument');
         }
 
-        $Post->create();
+        try {
+            $this->TransactionManager->begin();
+            $Post->create();
 
-        $postBody['user_id'] = $userId;
-        $postBody['team_id'] = $teamId;
+            $postBody['user_id'] = $userId;
+            $postBody['team_id'] = $teamId;
 
-        if ($postBody['type'] == Post::TYPE_CREATE_CIRCLE) {
-            $postBody['circle_id'] = $circleId;
-        } elseif (empty($postBody['type'])) {
-            $postBody['type'] = Post::TYPE_NORMAL;
-        }
+            if ($postBody['type'] == Post::TYPE_CREATE_CIRCLE) {
+                $postBody['circle_id'] = $circleId;
+            } elseif (empty($postBody['type'])) {
+                $postBody['type'] = Post::TYPE_NORMAL;
+            }
 
-        $savedPost = $Post->save($postBody);
+            $savedPost = $Post->save($postBody);
 
-        if (empty ($savedPost)) {
-            GoalousLog::error('Error on adding post: failed post save', [
-                'users.id'  => $userId,
-                'circle.id' => $circleId,
-                'teams.id'  => $teamId,
-                'postData'  => $postBody,
-            ]);
-            throw new RuntimeException('Error on adding post: failed post save');
-        }
+            if (empty ($savedPost)) {
+                GoalousLog::error('Error on adding post: failed post save', [
+                    'users.id'  => $userId,
+                    'circle.id' => $circleId,
+                    'teams.id'  => $teamId,
+                    'postData'  => $postBody,
+                ]);
+                throw new RuntimeException('Error on adding post: failed post save');
+            }
 
-        $postId = $savedPost['Post']['id'];
+            $postId = $savedPost['Post']['id'];
 
-        // Save share circles
-        if (false === $PostShareCircle->add($postId, [$circleId], $teamId)) {
-            GoalousLog::error($errorMessage = 'failed saving post share circles', [
-                'posts.id'    => $postId,
-                'circles.ids' => $postId,
-                'teams.id'    => $teamId,
-            ]);
-            throw new RuntimeException('Error on adding post: ' . $errorMessage);
-        }
-        // Update unread post numbers if specified sharing circle
-        if (false === $CircleMember->incrementUnreadCount([$circleId], true, $teamId)) {
-            GoalousLog::error($errorMessage = 'failed increment unread count', [
-                'circles.ids' => $postId,
-                'teams.id'    => $teamId,
-            ]);
-            throw new RuntimeException('Error on adding post: ' . $errorMessage);
+            // Save share circles
+            if (false === $PostShareCircle->add($postId, [$circleId], $teamId)) {
+                GoalousLog::error($errorMessage = 'failed saving post share circles', [
+                    'posts.id'    => $postId,
+                    'circles.ids' => $postId,
+                    'teams.id'    => $teamId,
+                ]);
+                throw new RuntimeException('Error on adding post: ' . $errorMessage);
+            }
+            // Update unread post numbers if specified sharing circle
+            if (false === $CircleMember->incrementUnreadCount([$circleId], true, $teamId)) {
+                GoalousLog::error($errorMessage = 'failed increment unread count', [
+                    'circles.ids' => $postId,
+                    'teams.id'    => $teamId,
+                ]);
+                throw new RuntimeException('Error on adding post: ' . $errorMessage);
+            }
+            $this->TransactionManager->begin();
+
+        } catch (Exception $e) {
+            $this->TransactionManager->commit();
+            throw $e;
         }
 
         return $postId;
