aa730d56d4fb16e4710193ce3e9b9b14cd1736a9
masuichig <masuichig@isao.co.jp>
2018-07-27 12:13:23 +0900

    add Language Enum definition

---
+++ app/Enum/Language.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Goalous\Enum;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method static static JA()
+ * @method static static EN()
+ */
+class Language extends Enum
+{
+    const JA = 'ja';
+    const EN = 'en';
+}
