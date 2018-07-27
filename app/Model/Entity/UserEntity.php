18c705c17763a17e11b7175a1fa68bfd041277ae
masuichig <masuichig@isao.co.jp>
2018-07-27 12:16:07 +0900

    add 2 method to UserEntity - UserEntity::getLanguageByIso639_1() - UserEntity::isSetLanguageFromBrowser()

--- app/Model/Entity/UserEntity.php
+++ app/Model/Entity/UserEntity.php
@@ -1,6 +1,8 @@
 <?php
 App::import('Model/Entity', 'BaseEntity');
 
+use Goalous\Enum as Enum;
+
 /**
  * Created by PhpStorm.
  * User: StephenRaharja
@@ -12,5 +14,34 @@ class UserEntity extends BaseEntity
     protected function initializeDefaults()
     {
         parent::initializeDefaults();
+    }
+
+    /**
+     * Return user language configuration
+     * https://en.wikipedia.org/wiki/ISO_639-1
+     *
+     * @return Enum\Language
+     */
+    public function getLanguageByIso639_1(): Enum\Language
+    {
+        // TODO: // UtilLang jpn, eng -> convert -> jp, en
+        switch ($this['language']) {
+            case 'jpn': return Enum\Language::JA();
+            case 'eng': return Enum\Language::EN();
+        }
+        return Enum\Language::JA();
+    }
+
+    /**
+     * Flag that holding get a language setting from browser(= Accept-Language) or not
+     *
+     * The DB table User.auto_language_flg has a comment
+     *   "自動言語設定フラグ(Onの場合はブラウザから言語を取得する)"
+     *
+     * @return bool
+     */
+    public function isSetLanguageFromBrowser(): bool
+    {
+        return boolval($this['auto_language_flg']);
     }
 }
