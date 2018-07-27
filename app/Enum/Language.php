5a4fcf1832a1da9ab19738d1db7437e39328601a
masuichig <masuichig@isao.co.jp>
2018-07-27 15:54:40 +0900

    add comment

--- app/Enum/Language.php
+++ app/Enum/Language.php
@@ -7,6 +7,9 @@ use MyCLabs\Enum\Enum;
 /**
  * @method static static JA()
  * @method static static EN()
+ *
+ * Language code based on ISO 639-1 (2 letters)
+ * @see https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
  */
 class Language extends Enum
 {
