7a1ad17a20a63a1a98534bc0df3bec7829c2e336
masuichig <masuichig@isao.co.jp>
2018-08-01 10:46:10 +0900

    add app/Config/validation_messages.php for Respect/Validation messages

---
+++ app/Config/validation_messages.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This file used on Respect/Validation rule messages
+ * @see /app/Validator/BaseValidator.php::getValidationMessageFromConfig()
+ */
+return [
+    'notEmpty' => __('validation.error.required'),
+    'email'    => __('validation.error.email_format'),
+];
