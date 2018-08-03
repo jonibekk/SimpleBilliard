4c75ffad59c7952119b6277326a63786bfe35920
Stephen <stef.stephen.r@gmail.com>
2018-08-03 10:46:45 +0900

    Fixed environment constants

--- app/Config/extra_defines_default.php
+++ app/Config/extra_defines_default.php
@@ -101,7 +101,7 @@ $defines = [
     'AWS_S3_BUCKET_VIDEO_TRANSCODE_LOCAL_SEPARATOR' => null,
     'JWT_TOKEN_SECRET_KEY_AUTH'                     => 'jwt_secret_key_auth',
     //For local S3 user name
-    'AWS_S3_BUCKET_USERNAME'                        => 'stephen',
+    'AWS_S3_BUCKET_USERNAME'                        => null,
     //For temporary files, such as upload buffering
     'AWS_S3_BUCKET_TMP'                             => 'goalous-local-tmp'
 ];
