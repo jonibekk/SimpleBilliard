c21edf22e9926854daf85f917c44da5afe6f94f4
Stephen <stef.stephen.r@gmail.com>
2018-09-04 12:02:37 +0900

    Add option when environment is docker

--- app/Config/extra_defines_travis.php
+++ app/Config/extra_defines_travis.php
@@ -1,11 +1,16 @@
 <?php
 
 $defines = [
-    'AWS_S3_BUCKET_USERNAME' => 'travis',
-    'REDIS_SESSION_HOST'     => 'localhost',
-    'REDIS_CACHE_HOST'       => 'localhost',
-    'REDIS_HOST'             => 'localhost',
+    'AWS_S3_BUCKET_USERNAME' => 'travis'
 ];
+
+if (empty(getenv('DOCKER_ENV'))) {
+    array_merge($defines, [
+        'REDIS_SESSION_HOST' => 'localhost',
+        'REDIS_CACHE_HOST'   => 'localhost',
+        'REDIS_HOST'         => 'localhost',
+    ]);
+}
 
 foreach ($defines as $k => $v) {
     if (!defined($k)) {
