0bd16e692f663739d2707d3c0b53785ea9db153d
Stephen <stef.stephen.r@gmail.com>
2018-09-04 17:40:55 +0900

    General fixes and improvements

--- app/Config/extra_defines_default.php
+++ app/Config/extra_defines_default.php
@@ -10,9 +10,10 @@
 if (file_exists(APP . 'Config/extra_defines.php')) {
     require_once(APP . 'Config/extra_defines.php');
 }
-if (!empty(getenv('CI_TEST'))){
+if (!empty(getenv('CI_TEST'))) {
     require_once(APP . 'Config/extra_defines_travis.php');
 }
+
 // https://confluence.goalous.com/x/Z4LT
 // $defines will be define(key,value);
 $defines = [
@@ -27,9 +28,9 @@ $defines = [
     'AWS_SECRET_KEY'                                => 'qGbY0i6uK7c+6/RbnqlIEOHFdOBE0vcHz2xyFggT',
     'S3_LOGS_BUCKET'                                => null,
     'S3_ASSETS_BUCKET'                              => 'goalous-local-assets',
-    'REDIS_SESSION_HOST'                            => 'redis',
-    'REDIS_CACHE_HOST'                              => 'redis',
-    'REDIS_HOST'                                    => 'redis',
+    'REDIS_SESSION_HOST'                            => 'localhost',
+    'REDIS_CACHE_HOST'                              => 'localhost',
+    'REDIS_HOST'                                    => 'localhost',
     'SES_FROM_ADDRESS'                              => 'support@goalous.com',
     'SES_FROM_ADDRESS_CONTACT'                      => 'contact@goalous.com',
     'SES_FROM_ADDRESS_NEWS'                         => 'news@goalous.com',
@@ -109,6 +110,13 @@ $defines = [
     //For temporary files, such as upload buffering
     'AWS_S3_BUCKET_TMP'                             => 'goalous-local-tmp'
 ];
+
+//If not on docker, use local redis
+if (!empty(getenv('DOCKER_ENV'))) {
+    $defines['REDIS_SESSION_HOST'] = 'redis';
+    $defines['REDIS_CACHE_HOST'] = 'redis';
+    $defines['REDIS_HOST'] = 'redis';
+}
 
 foreach ($defines as $k => $v) {
     if (!defined($k)) {
--- app/Config/extra_defines_travis.php
+++ app/Config/extra_defines_travis.php
@@ -4,15 +4,6 @@ $defines = [
     'AWS_S3_BUCKET_USERNAME' => 'travis'
 ];
 
-//If not on docker, use local redis
-if (empty(getenv('DOCKER_ENV'))) {
-    $defines = array_merge($defines, [
-        'REDIS_SESSION_HOST' => 'localhost',
-        'REDIS_CACHE_HOST'   => 'localhost',
-        'REDIS_HOST'         => 'localhost',
-    ]);
-}
-
 foreach ($defines as $k => $v) {
     if (!defined($k)) {
         define($k, $v);
