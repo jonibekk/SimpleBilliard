70af45dd6778259d44dd99d44415b26271707ed8
Stephen <stef.stephen.r@gmail.com>
2018-09-04 13:36:56 +0900

    General fixes and improvements

--- app/Config/extra_defines_travis.php
+++ app/Config/extra_defines_travis.php
@@ -4,8 +4,9 @@ $defines = [
     'AWS_S3_BUCKET_USERNAME' => 'travis'
 ];
 
+//If not on docker, use local redis
 if (empty(getenv('DOCKER_ENV'))) {
-    array_merge($defines, [
+    $defines = array_merge($defines, [
         'REDIS_SESSION_HOST' => 'localhost',
         'REDIS_CACHE_HOST'   => 'localhost',
         'REDIS_HOST'         => 'localhost',
