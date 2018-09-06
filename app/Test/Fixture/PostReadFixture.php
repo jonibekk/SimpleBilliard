721abee10bc4c6b5655bccd40e459ec1e5c8b00c
Maruchan <floriachm@isao.co.jp>
2018-09-06 16:10:33 +0900

    change fixture test postread users on the same team

--- app/Test/Fixture/PostReadFixture.php
+++ app/Test/Fixture/PostReadFixture.php
@@ -92,7 +92,7 @@ class PostReadFixture extends CakeTestFixtureEx
         array(
             'id'        => 1,
             'post_id'   => 1,
-            'user_id'   => 1,
+            'user_id'   => 2,
             'team_id'   => 1,
             'del_flg'   => 0,
             'created'   => 1,
@@ -100,8 +100,8 @@ class PostReadFixture extends CakeTestFixtureEx
         ),
         array(
             'id'        => 2,
-            'post_id'   => 1,
-            'user_id'   => 2,
+            'post_id'   => 101,
+            'user_id'   => 4,
             'team_id'   => 1,
             'del_flg'   => 0,
             'created'   => 2,
