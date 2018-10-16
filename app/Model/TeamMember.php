95c31173ba2dcb680c1a20d8669efb14e0848d39
Stephen Raharja <stef.stephen.r@gmail.com>
2018-10-16 15:52:49 +0900

    Fixed SQL condition

--- app/Model/TeamMember.php
+++ app/Model/TeamMember.php
@@ -116,14 +116,14 @@ class TeamMember extends AppModel
                         'TeamMember.status'  => self::USER_STATUS_ACTIVE
                     ],
                     'fields'     => ['TeamMember.team_id', 'Team.name'],
-                    'contain'    => ['Team'],
                     'joins'      => [
                         [
                             'type'       => 'INNER',
                             'table'      => 'teams',
+                            'alias'      => 'Team',
                             'conditions' => [
-                                'teams.id = TeamMember.team_id',
-                                'teams.service_use_status' => [Team::SERVICE_USE_STATUS_FREE_TRIAL, Team::SERVICE_USE_STATUS_PAID, Team::SERVICE_USE_STATUS_READ_ONLY]
+                                'Team.id = TeamMember.team_id',
+                                'Team.service_use_status' => [Team::SERVICE_USE_STATUS_FREE_TRIAL, Team::SERVICE_USE_STATUS_PAID, Team::SERVICE_USE_STATUS_READ_ONLY]
                             ]
                         ]
                     ]
