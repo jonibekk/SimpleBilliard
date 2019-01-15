10c4048b113ba3876ca1716694fb73fae1d53c59
endam <yoshidam2@isao.co.jp>
2019-01-15 16:33:11 +0900

    [GL-7738] Fix design collapse that message form is hidden back of footer on topic detail page

--- app/static_src/js/react/message/constants/Styles.js
+++ app/static_src/js/react/message/constants/Styles.js
@@ -3,11 +3,11 @@ export const PositionIOSApp = Object.freeze({
   HEADER_TOP: 64,
   BODY_TOP: 114,
   BODY_BOTTOM: 61,
-  FOOTER_BOTTOM: 6,
+  FOOTER_BOTTOM: 56,
 })
 export const PositionMobileApp = Object.freeze({
   HEADER_TOP: 45,
   BODY_TOP: 95,
   BODY_BOTTOM: 55,
-  FOOTER_BOTTOM: 0,
+  FOOTER_BOTTOM: 50,
 })
