9eee7972abc663f957d396432a6c502b75743f58
masuichig <masuichig@isao.co.jp>
2019-10-09 17:01:28 +0900

    Create another login form for bulk register /users/agree_and_login

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -13,6 +13,8 @@ App::import('Service', 'TermService');
 App::import('Service', 'TeamMemberService');
 App::import('Service', 'ExperimentService');
 
+use Goalous\Enum as Enum;
+
 /**
  * Users Controller
  *
@@ -42,7 +44,7 @@ class UsersController extends AppController
     public function beforeFilter()
     {
         parent::beforeFilter();
-        $this->Auth->allow('register', 'login', 'verify', 'logout', 'password_reset', 'token_resend', 'sent_mail',
+        $this->Auth->allow('register', 'login', 'agree_and_login', 'verify', 'logout', 'password_reset', 'token_resend', 'sent_mail',
             'accept_invite', 'register_with_invite', 'registration_with_set_password', 'two_fa_auth',
             'two_fa_auth_recovery',
             'add_subscribe_email', 'ajax_validate_email');
@@ -55,6 +57,74 @@ class UsersController extends AppController
             $this->Security->validatePost = false;
             $this->Security->csrfCheck = false;
         }
+    }
+
+    /**
+     * This route `/users/agree_and_login` is for bulk registration.
+     * User will land here from url written in the email sent by bulk registration.
+     * Check the document for spec.
+     * https://isao.esa.io/#path=%2FGoalous%2FSpecification%2FUsers%2FUser%20bulk%20registration%20and%20link%20to%20a%20team
+     *
+     * @return \Cake\Network\Response|CakeResponse|null
+     */
+    public function agree_and_login()
+    {
+        $this->layout = LAYOUT_ONE_COLUMN;
+
+        if ($this->Auth->user()) {
+            return $this->redirect('/');
+        }
+
+        if (!$this->request->is('post')) {
+            return $this->render();
+        }
+
+        //account lock check
+        $ipAddress = $this->request->clientIp();
+        $isAccountLocked = $this->GlRedis->isAccountLocked($this->request->data['User']['email'], $ipAddress);
+        if ($isAccountLocked) {
+            $this->Notification->outError(__("Your account is temporary locked. It will be unlocked after %s minutes.",
+                ACCOUNT_LOCK_TTL / 60));
+            return $this->render();
+        }
+        //メアド、パスの認証(セッションのストアはしていない)
+        $userInfo = $this->Auth->identify($this->request, $this->response);
+        if (!$userInfo) {
+            $this->GlRedis->incrementLoginFailedCount($this->request->data['User']['email'], $ipAddress);
+            $this->Notification->outError(__("Email address or Password is incorrect."));
+            return $this->render();
+        }
+
+        /* @var TermsOfService $TermsOfService */
+        $TermsOfService = ClassRegistry::init('TermsOfService');
+        /** @var User $User */
+        $User = ClassRegistry::init("User");
+
+        $userId = $userInfo['id'];
+        $termsOfService = $TermsOfService->getCurrent();
+        $User->updateAgreedTermsOfServiceId($userId, $termsOfService['id']);
+
+        $this->Session->write('preAuthPost', $this->request->data);
+
+        $is2faAuthEnabled = true;
+        // 2要素認証設定OFFの場合
+        // [Note]
+        // Refer: https://jira.goalous.com/browse/GL-6874
+        if (is_null($userInfo['2fa_secret']) === true) {
+            $is2faAuthEnabled = false;
+        }
+
+        $teamIdSwitch = $this->request->query("team_id") ?? $userInfo['DefaultTeam']['id'];
+        $this->Session->write('invited_team_id', $teamIdSwitch);
+        //２要素設定有効なら
+        if ($is2faAuthEnabled) {
+            $this->Session->write('2fa_secret', $userInfo['2fa_secret']);
+            $this->Session->write('user_id', $userInfo['id']);
+            $this->Session->write('team_id', $teamIdSwitch);
+            return $this->redirect(['action' => 'two_fa_auth']);
+        }
+
+        return $this->_afterAuthSessionStore();
     }
 
     /**
@@ -113,6 +183,17 @@ class UsersController extends AppController
             $this->GlRedis->incrementLoginFailedCount($this->request->data['User']['email'], $ipAddress);
             $this->Notification->outError(__("Email address or Password is incorrect."));
             return $this->render();
+        }
+
+        $activeFlg = (int)$userInfo['active_flg'];
+        $isUserActive = ($activeFlg === Enum\Model\User\ActiveFlg::ON);
+
+        $termOfServiceIdUserAgreed = (int)$userInfo['agreed_terms_of_service_id'];
+        $isNotAgreedToAnyTerm = (0 === $termOfServiceIdUserAgreed);
+
+        if ($isUserActive && $isNotAgreedToAnyTerm) {
+            $this->Notification->outError(__("Please Agree to the term of service."));
+            return $this->redirect('/users/agree_and_login');
         }
 
         $this->Session->write('preAuthPost', $this->request->data);
---
+++ app/Enum/Model/User/ActiveFlg.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Goalous\Enum\Model\User;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Enum of users.active_flg
+ *
+ * @method static static ON()
+ * @method static static OFF()
+ */
+class ActiveFlg extends Enum
+{
+    const ON = 1;
+    const OFF = 0;
+}
