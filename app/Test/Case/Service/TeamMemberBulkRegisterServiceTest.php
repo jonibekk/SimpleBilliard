e4abec06caa16e9d0e830f94c72555923fb6674a
jonghwa.kim <kimuj@isao.co.jp>
2019-10-10 03:40:22 +0900

    fixed test code.

--- app/Test/Case/Service/TeamMemberBulkRegisterServiceTest.php
+++ app/Test/Case/Service/TeamMemberBulkRegisterServiceTest.php
@@ -148,7 +148,7 @@ class TeamMemberBulkRegisterServiceTest extends GoalousTestCase
         $fileName = Hash::get($params, 'file_name', '');
         $dryRun = array_key_exists('dry-run', $params);
 
-        $s3ClientMock = \Mockery::mock(\Aws\S3\S3Client::class);
+        $s3ClientMock = \Mockery::mock(\Aws\S3\S3Client::class)->makePartial();
         $s3ClientMock->shouldReceive('doesBucketExist')->andReturn($doesBucketExist);
         $s3ClientMock->shouldReceive('doesObjectExist')->andReturn($doesObjectExist);
 
