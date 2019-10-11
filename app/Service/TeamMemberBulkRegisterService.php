e686526101b0fcb2c4d279e2000773a772354428
masuichig <masuichig@isao.co.jp>
2019-10-11 13:08:18 +0900

    Add sleep for problem sending email

--- app/Service/TeamMemberBulkRegisterService.php
+++ app/Service/TeamMemberBulkRegisterService.php
@@ -201,6 +201,7 @@ class TeamMemberBulkRegisterService
                 $this->addLog($errorMessage);
             } finally {
                 $this->outputCompleteMessage($index + 1);
+                usleep(500000);
             }
         }
     }
