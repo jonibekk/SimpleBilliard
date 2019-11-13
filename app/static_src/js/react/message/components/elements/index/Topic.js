4c02e89a2403063ff3106e9e3e01c2094d048269
masuichig <masuichig@isao.co.jp>
2019-11-13 15:30:03 +0900

    Display a Reactjs component even there isn't enough data for components

--- app/static_src/js/react/message/components/elements/index/Topic.js
+++ app/static_src/js/react/message/components/elements/index/Topic.js
@@ -26,6 +26,7 @@ class Topic extends React.Component {
 
   render() {
     const topic = this.props.topic
+    const latest_message = topic.latest_message || {};
 
     return (
       <li className="topicList-item" key={ topic.id }>
@@ -33,7 +34,7 @@ class Topic extends React.Component {
               className={`topicList-item-link ${topic.is_unread ? 'is-unread' : ''} ${this.state.is_taped_item ? "is-hover" : ""}`}
               onClick={ this.onClickLinkToDetail.bind(this) }
               onTouchTap={ this.tapLink.bind(this) }>
-          <AvatarsBox users={ topic.users }/>
+          <AvatarsBox users={ topic.users } />
           <div className="topicList-item-main">
             <div className="topicList-item-main-header">
               <div className={`topicList-item-main-header-title oneline-ellipsis ${topic.is_unread ? 'mod-bold' : ''}` }>
@@ -46,7 +47,7 @@ class Topic extends React.Component {
               </div>
             </div>
             <div className="topicList-item-main-body oneline-ellipsis">
-              { topic.latest_message.body }
+              { latest_message.body || '' }
             </div>
             <div className="topicList-item-main-footer">
               {(() => {
@@ -59,7 +60,7 @@ class Topic extends React.Component {
                   return <span><i className="fa fa-check"></i> { topic.read_count }・</span>
                 }
               })()}
-              { topic.latest_message.display_created }
+              { latest_message.display_created || '' }
             </div>
           </div>
         </Link>
