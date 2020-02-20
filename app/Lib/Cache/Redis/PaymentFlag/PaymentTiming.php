0928f4f8b0c43ae09a5c648ab3cb3c67a0314bbc
Ning LI <lin@isao.co.jp>
2020-02-20 22:03:34 +0900

    basic done, need test

--- app/Lib/Cache/Redis/PaymentFlag/PaymentTiming.php
+++ app/Lib/Cache/Redis/PaymentFlag/PaymentTiming.php
@@ -40,4 +40,38 @@ class PaymentTiming
 
     }
 
+    /*
+     * get payment timing day
+     *
+     */
+
+    public function getPaymentTimingDay(int $teamId): int
+    {
+        /* get payment flag */
+        $redisClient = new PaymentFlagClient();
+        $switchFlagKey = new PaymentFlagKey(PaymentFlagKey::SWITCH_FLAG_NAME);
+        $switchStartDateKey = new PaymentFlagKey(PaymentFlagKey::SWITCH_START_DATE_NAME);
+        $switchFlag = $redisClient->read($switchFlagKey);
+        $switchStartDate = $redisClient->read($switchStartDateKey);
+        /** @var Team $Team */
+        $Team = ClassRegistry::init("Team");
+        if (!$Team->isPaidPlan($teamId)) {
+            return true;
+        }
+        /** @var PaymentSetting $PaymentSetting */
+        $PaymentSetting = ClassRegistry::init('PaymentSetting');
+        $paymentSetting = $PaymentSetting->getUnique($teamId);
+        $paymentBaseDay = Hash::get($paymentSetting, 'payment_base_day');
+
+        if (empty($switchFlag) or intval($switchFlag) == 1 and empty($switchStartDate)){
+            $validationErrors = 'Switch Value Error!';
+            throw new Exception(sprintf("Failed to get correct switch value. teamId:%s validationErrors:%s", 
+                $inviteTeamId, var_export($validationErrors, true)));
+        }
+
+        $paymentTimingDay = PaymentFlagClient::getPaymentTimingDay($paymentBaseDay, $switchStartDate);
+        
+        return $paymentTimingDay->getTimestamp();
+
+    }
 }
