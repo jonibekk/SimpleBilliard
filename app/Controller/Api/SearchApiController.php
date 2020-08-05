17fe9d687e32d1ce4b3a1af7b42a9dd331248358
Evan de Graaff <evertjannes.degraaff@colorkrew.com>
2020-08-05 17:23:51 +0900

    Add dummy data for local dev.

--- app/Controller/Api/SearchApiController.php
+++ app/Controller/Api/SearchApiController.php
@@ -52,7 +52,12 @@ class SearchApiController extends BasePagingController
             return ErrorResponse::badRequest();
         }
 
-        $this->searchApiService->search($searchApiRequestDto, $searchApiResponseDto);
+        if ('local' === constant('ENV_NAME')) {
+             $this->_dummyResponse($searchApiRequestDto, $searchApiResponseDto);
+        } else {
+            $this->searchApiService->search($searchApiRequestDto, $searchApiResponseDto);
+        }
+
         $body = $this->searchApiService->dtoToArray($searchApiResponseDto);
 
         return ApiResponse::ok()->withBody($body)->getResponse();
@@ -97,5 +102,116 @@ class SearchApiController extends BasePagingController
         if ($searchApiRequestDto->limit > SearchApiEnum::MAX_LIMIT) {
             $searchApiRequestDto->limit = SearchApiEnum::MAX_LIMIT;
         }
+    }
+
+    /**
+     * @param SearchApiRequestDto $searchApiRequestDto
+     * @param SearchApiResponseDto $searchApiResponseDto
+     *
+     * @return SearchApiResponseDto
+     */
+    private function _dummyResponse(
+        SearchApiRequestDto $searchApiRequestDto,
+        SearchApiResponseDto $searchApiResponseDto
+    ): SearchApiResponseDto
+    {
+        $searchApiResponseDto->actions->totalItemsCount = 1;
+        $searchApiResponseDto->posts->totalItemsCount = 3;
+        $searchApiResponseDto->members->totalItemsCount = 10;
+        $searchApiResponseDto->circles->totalItemsCount = 100;
+
+        // Actions.
+        $count = $this->_dummyItemCount(
+            $searchApiResponseDto->actions->totalItemsCount,
+            $searchApiRequestDto->pn,
+            $searchApiRequestDto->limit
+        );
+
+        for ($i = 0; $i < $count; $i++) {
+            $itemSearchModel = new PostItemSearchDto();
+            $itemSearchModel->id = $searchApiRequestDto->pn * $i + $i;
+            $itemSearchModel->imageUrl = 'https://via.placeholder.com/300/09f/fff.png';
+            $itemSearchModel->type = 1 === mt_rand(0, 1) ? SearchApiEnum::POST_TYPE_COMMENTS : SearchApiEnum::POST_TYPE_POSTS;
+            $itemSearchModel->content = sprintf('This is action %d with a body.', $itemSearchModel->id);
+            $itemSearchModel->dateTime = '2020-07-15 12:00:00';
+            $itemSearchModel->userImageUrl = 'https://via.placeholder.com/300/9fa/fff.png';
+            $itemSearchModel->userName = 'Member '.$itemSearchModel->id;
+
+            $searchApiResponseDto->actions->items[] = $itemSearchModel;
+        }
+
+        // Posts.
+        $count = $this->_dummyItemCount(
+            $searchApiResponseDto->posts->totalItemsCount,
+            $searchApiRequestDto->pn,
+            $searchApiRequestDto->limit
+        );
+
+        for ($i = 0; $i < $count; $i++) {
+            $itemSearchModel = new PostItemSearchDto();
+            $itemSearchModel->id = $searchApiRequestDto->pn * $i + $i;
+            $itemSearchModel->imageUrl = 'https://via.placeholder.com/300/09f/fff.png';
+            $itemSearchModel->type = 1 === mt_rand(0, 1) ? SearchApiEnum::POST_TYPE_COMMENTS : SearchApiEnum::POST_TYPE_POSTS;
+            $itemSearchModel->content = sprintf('This is post %d with a body.', $itemSearchModel->id);
+            $itemSearchModel->dateTime = '2020-07-15 12:00:00';
+            $itemSearchModel->userImageUrl = 'https://via.placeholder.com/300/9fa/fff.png';
+            $itemSearchModel->userName = 'Member '.$itemSearchModel->id;
+
+            $searchApiResponseDto->posts->items[] = $itemSearchModel;
+        }
+
+        // Members.
+        $count = $this->_dummyItemCount(
+            $searchApiResponseDto->members->totalItemsCount,
+            $searchApiRequestDto->pn,
+            $searchApiRequestDto->limit
+        );
+
+        for ($i = 0; $i < $count; $i++) {
+            $itemSearchModel = new DefaultItemSearchDto();
+            $itemSearchModel->id = $searchApiRequestDto->pn * $i + $i;
+            $itemSearchModel->imageUrl = 'https://via.placeholder.com/300/9fa/fff.png';
+            $itemSearchModel->name = 'Member '.$itemSearchModel->id;
+
+            $searchApiResponseDto->members->items[] = $itemSearchModel;
+        }
+
+        // Circles.
+        $count = $this->_dummyItemCount(
+            $searchApiResponseDto->circles->totalItemsCount,
+            $searchApiRequestDto->pn,
+            $searchApiRequestDto->limit
+        );
+
+        for ($i = 0; $i < $count; $i++) {
+            $itemSearchModel = new DefaultItemSearchDto();
+            $itemSearchModel->id = $searchApiRequestDto->pn * $i + $i;
+            $itemSearchModel->imageUrl = 'https://via.placeholder.com/300/9fa/fff.png';
+            $itemSearchModel->name = 'Circles '.$itemSearchModel->id;
+
+            $searchApiResponseDto->circles->items[] = $itemSearchModel;
+        }
+
+        return $searchApiResponseDto;
+    }
+
+    /**
+     * @param int $count
+     * @param int $pn
+     * @param int $limit
+     *
+     * @return int
+     */
+    private function _dummyItemCount($count, $pn, $limit): int
+    {
+        $maxPn = ceil($count / $limit);
+
+        if ($pn < $maxPn) {
+            return $limit;
+        } elseif ($pn === $maxPn) {
+            return $count % $limit;
+        }
+
+        return 0;
     }
 }
