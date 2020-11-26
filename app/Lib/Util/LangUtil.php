e5c111adbb7a9e6d48462c574286f5f5ec38e409
GitJonibek <jonibeknorboev@gmail.com>
2020-11-26 11:47:41 +0900

    Default Language key fix

--- app/Lib/Util/LangUtil.php
+++ app/Lib/Util/LangUtil.php
@@ -7,7 +7,7 @@ use Goalous\Enum as Enum;
  */
 class LangUtil
 {
-    static $defaultLang = 'en';
+    static $defaultLang = 'eng';
     static $ISOMap = [
         Enum\LanguageISO639_2::ENG => Enum\Language::EN,
         Enum\LanguageISO639_2::JPN => Enum\Language::JA,
