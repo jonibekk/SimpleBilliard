c70f539d8bc5bc116198c4e4e95fd63bc96a0bf6
Stephen Raharja <stef.stephen.r@gmail.com>
2020-12-07 13:43:48 +0900

    Change conversion location

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -137,6 +137,7 @@ class AppModel extends Model
         $Session = new CakeSession();
 
         $this->me = $Session->read('Auth.User');
+        $this->me['language'] = LangUtil::convertToISO3($this->me['language']);
         $this->current_team_id = $Session->read('current_team_id');
         $this->my_uid = $Session->read('Auth.User.id');
         if ($Session->read('Auth.User.language')) {
