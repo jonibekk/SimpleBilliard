00e339575c7a5015aafa212bb3380bd11d9e50d6
Ming Xiang Chan <mingxiangc.isao@gmail.com>
2020-12-08 09:42:57 +0900

    get target progress as cumulative percentage

--- app/Service/WatchlistService.php
+++ app/Service/WatchlistService.php
@@ -1,6 +1,7 @@
 
 <?php
 App::import('Service', 'AppService');
+App::import('Service', 'GoalService');
 App::uses('Watchlist', 'Model');
 App::uses('KrWatchlist', 'Model');
 App::import('Model/Entity', 'WatchlistEntity');
@@ -71,5 +72,119 @@ class WatchlistService extends AppService
         }
 
         return $watchlist;
+    }
+
+    public function getWatchlistProgressForGraph(
+        $watchlistId,
+        $graphStartDate,
+        $graphEndDate,
+        $plotEndDate
+    ): array {
+        /** @var GoalService */
+        $GoalService = ClassRegistry::init('GoalService');
+        $targetProgress = $this->findTargetProgress($watchlistId);
+        $progressLogs = $this->findProgressLogsForList($watchlistId);
+        $logsByDate = $this->sumDailyProgressLogs($progressLogs);
+        $xLabels = ['x'];
+        $datapoints = ['data'];
+
+        $TimeEx = new TimeExHelper(new View());
+        $timestamp = strtotime($graphStartDate);
+        $graphEndTimestamp = strtotime($graphEndDate);
+        $cummulativeVal = 0;
+
+        while ($timestamp <= $graphEndTimestamp) {
+            $xLabels[] = $TimeEx->formatDateI18n($timestamp, false);
+
+            $strDate = date('Y-m-d', $timestamp);
+
+            if (array_key_exists($strDate, $logsByDate)) {
+                $cummulativeVal += ($logsByDate[$strDate] / $targetProgress * 100);
+                $datapoints[] = $cummulativeVal;
+            } else if ($timestamp > time()) {
+                $datapoints[] = null;
+            } 
+
+            $timestamp = strtotime('+1 day', $timestamp);
+        }
+
+        $sweetSpot = $GoalService->getSweetSpot($graphStartDate, $graphEndDate);
+
+        return [
+            array_merge(['sweet_spot_top'], $sweetSpot['top']),
+            array_merge(['sweet_spot_bottom'], $sweetSpot['bottom']),
+            $datapoints,
+            $xLabels,
+        ];
+    }
+
+    private function findTargetProgress($watchlistId) {
+        /** @var KrWatchlist */
+        $KrWatchlist = ClassRegistry::init("KrWatchlist");
+        $opts = [
+            'conditions' => ['KrWatchlist.watchlist_id' => $watchlistId],
+            'joins' => [
+                [
+                    'alias' => 'KeyResult',
+                    'table' => 'key_results',
+                    'conditions' => [
+                        'KeyResult.id = KrWatchlist.key_result_id',
+                        'KeyResult.del_flg != 1'
+                    ]
+                ]
+            ],
+            'fields' => [
+                'SUM(COALESCE(KeyResult.target_value,0)) AS target_progress',
+            ]
+        ];
+
+        $row = $KrWatchlist->find('first', $opts);
+        return $row[0]['target_progress'];
+
+    }
+
+    private function findProgressLogsForList($watchlistId) 
+    {
+        /** @var KrWatchlist */
+        $KrWatchlist = ClassRegistry::init("KrWatchlist");
+        $opts = [
+            'conditions' => ['KrWatchlist.watchlist_id' => $watchlistId],
+            'joins' => [
+                [
+                    'alias' => 'KeyResult',
+                    'table' => 'key_results',
+                    'conditions' => [
+                        'KeyResult.id = KrWatchlist.key_result_id',
+                        'KeyResult.del_flg != 1'
+                    ]
+                ],
+                [
+                    'alias' => 'KrProgressLog',
+                    'table' => 'kr_progress_logs',
+                    'conditions' => 'KrProgressLog.key_result_id = KrWatchlist.key_result_id'
+                ],
+            ],
+            'fields' => [
+                'KrProgressLog.change_value',
+                'KrProgressLog.created',
+            ]
+        ];
+
+        $rows = $KrWatchlist->find('all', $opts);
+        return Hash::extract($rows, '{n}.KrProgressLog');
+    }
+
+    private function sumDailyProgressLogs($progressLogs) 
+    {
+        return array_reduce($progressLogs, function($acc, $progressLog) {
+            $logDate = AppUtil::dateYmd($progressLog['created']);
+
+            if (!array_key_exists($logDate, $acc)) {
+                $acc[$logDate] = 0;
+            }
+
+            $acc[$logDate] += $progressLog['change_value'];
+            return $acc;
+        }, []);
     }
 }
