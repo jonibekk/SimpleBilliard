0744cb6b5818f2f7d0f7fc5a65881540614e1d6b
GitJonibek <jonibeknorboev@gmail.com>
2020-12-11 11:23:48 +0900

    Revert "Merge branch 'update-user-settings' of https://github.com/Colorkrew/goalous into update-user-settings"
    This reverts commit e2bc53fed4fc5b5787d9bcd87039f02e5d18f437, reversing
    changes made to 1acc61cae24f2d321d41b3faee946ad6e6778923.

--- app/Lib/Util/LangUtil.php
+++ app/Lib/Util/LangUtil.php
@@ -7,8 +7,9 @@ use Goalous\Enum as Enum;
  */
 class LangUtil
 {
-    static $defaultLang = 'en';
-    static $ISOMap = [
+    static $defaultLang     = 'en';
+    static $defaultLangISO3 = 'eng';
+    static $ISOMap          = [
         Enum\LanguageISO639_2::ENG => Enum\Language::EN,
         Enum\LanguageISO639_2::JPN => Enum\Language::JA,
         Enum\LanguageISO639_2::POR => Enum\Language::PT,
@@ -21,8 +22,41 @@ class LangUtil
      *
      * @return string
      */
-    static function convertISOFrom3to2(string $code) : string
+    public static function convertISOFrom3to2(string $code): string
     {
         return !empty(self::$ISOMap[$code]) ? self::$ISOMap[$code] : self::$defaultLang;
+    }
+
+    /**
+     * Convert Language code from ISO 639-1 (2 characters (en, ja)) to ISO 639-2 (3 characters (eng, jpn))
+     *
+     * @param string $code
+     *
+     * @return string
+     */
+    public static function convertISOFrom2To3(string $code): string
+    {
+        foreach (self::$ISOMap as $key => $value) {
+            if ($value === $code) {
+                return $key;
+            }
+        }
+        return self::$defaultLangISO3;
+    }
+
+    /**
+     * Ensure that language code will alwaye be in ISO 639-2
+     *
+     * @param string $code
+     *
+     * @return string
+     */
+    public static function convertToISO3(string $code): string
+    {
+        if (strlen($code) === 3) {
+            return $code;
+        } else {
+            return self::convertISOFrom2To3($code);
+        }
     }
 }
